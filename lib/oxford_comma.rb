579c5a2c4aef96e279e713deeff67667af70e0e2
onyoo <roberto9m9@gmail.com>
2015-11-11 11:12:15 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,11 @@
 def oxford_comma(arr)
   if arr.length == 1
-    puts "#{arr[0]}"
     arr[0]
   elsif arr.length == 2
     arr.insert(-2, "and")
     arr.join(" ")
   elsif arr.length > 2
-    x = arr[-1].insert(0, "and ")
-    puts "#{x}"
-    puts "#{arr}"
+    arr[-1].insert(0, "and ")
     arr.join(", ")
   end
 end
