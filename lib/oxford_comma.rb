6915b3f49a32985639a9dba540936fd0d3661f70
aaron555 <mahanability@gmail.com>
2015-11-11 23:33:28 +0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.count == 1
+    array[0]
+  elsif array.count == 2
+    array.join (" and ")
+  else
+    first_half = array[0...-1].join ", "
+    second_half = "and #{array.last}"
+    "#{first_half}, #{second_half}"
+  end
 
 end
