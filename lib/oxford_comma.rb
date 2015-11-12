372e25d372cd9e20e64e91141cc32dd33500af2c
Ahertig <alyssa.hertig@gmail.com>
2015-11-11 19:41:15 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size >= 3
+    oxford_comma = ""
 
+    array.each do |element|
+      if element == array[-1]
+        oxford_comma << "and " + element.to_s
+      else
+        oxford_comma << element.to_s + ", "  
+      end
+    end
+
+    return oxford_comma
+  end
+
+  nil
 end
