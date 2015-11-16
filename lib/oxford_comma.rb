3577327161b70651395a9e44ee47efe49d2bcaba
Justin Thomas <jthomas0209@gmail.com>
2015-11-16 09:45:22 -0500

    defined #oxford_comma in oxford_comma.rb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+#input --> array of items
+#output --> string listing the items in sentence form
 def oxford_comma(array)
-
+  output = ""
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    output << array[0..array.length-2].join(", ")
+    output << ", and #{array[array.size-1]}"
+  end
 end
+    
+      
