aa0feb31df446625348d68afade12f29efeeb5c7
surla <josephsurla@gmail.com>
2015-11-16 09:21:52 -0800

    finish method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else array.size >= 3
+    last = "and #{array.pop}"
+    new_array = array << last
+    new_array.join(", ")
+  end 
 end
