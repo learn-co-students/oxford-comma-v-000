fa615bed13013cbc99c277642a8796517dd98afe
marcotsui2003 <marcotsui2003@yahoo.com>
2015-11-19 08:06:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      array.join
+    when 2
+      array. join(" and ")
+    else
+      short_array = array.slice(0..-2)
+      string = short_array.join(", ")
+      string << ", and #{array[-1]}"
+      string
+    end
 end
