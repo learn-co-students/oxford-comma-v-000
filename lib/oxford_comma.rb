f375b14e6951500f5881092cb4cafc2e27200f82
Karuna <imkaruna@gmail.com>
2015-11-30 21:40:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  new_array=[]
+  if array.length == 1
+     array.pop 
+  elsif
+    array.length == 2
+    array.join(" and ")
+    
+  elsif
+    array.length > 2
+    last_item = array.pop    
+    array.join(", ") + ", and "+last_item
+        
+  end
 end
