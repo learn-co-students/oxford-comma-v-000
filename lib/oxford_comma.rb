828d897fa9e0ecaa352bb34e3d85aa75b38d1c9d
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-03 00:58:21 +0000

    solves oxford comma lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.first.to_s 
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last = "and #{array.pop}"
+    array << last
+    array.join(", ")
+  end
 end
