39c2f46e80cb30471d41f32b6bdbf0918a253690
kylegraydev <kylegraydev@gmail.com>
2015-12-15 09:36:52 -0800

    finished

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    add_and = "and #{array.last}"
+    array.pop
+    array.push(add_and)
+    array.join(", ")
+  end
 end
