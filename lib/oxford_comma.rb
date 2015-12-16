578d6d69c610ef0fc8726f9caf6f9fda9af5e818
eric-an <erican@me.com>
2015-12-15 21:57:02 -0800

    convert arrays into Oxford comma delimited strings

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+  if array.count == 1
+    array.join
+
+  elsif array.count == 2
+    array.join(" and ")
+
+  else
+    last_fruit = array.pop
+    last_item = " and " + last_fruit
+
+    list = array.join(", ")
+    list << "," + last_item
+  end
 end
