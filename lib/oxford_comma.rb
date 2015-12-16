52d4272a1bad56a940ddc47e90574d801d79f11b
jsgoldb <jgoldb@yahoo.com>
2015-12-16 16:45:01 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0].to_s
+  else
+    last_word = array.pop
+    new_string = array.join(", ")
+    new_string << "," if array.length > 1
+    new_string << " and #{last_word}"
+  end
 end
