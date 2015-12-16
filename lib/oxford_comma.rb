7c1f25d03727427289182b8e568d211233350573
macgeek30 <mike25ko@gmail.com>
2015-12-16 01:16:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif  array.length == 2
+    array.join (" and ")
+  else
+    i = "and " + array[-1]
+    array.pop
+    array << i
+    array.join(", ")
+  end
 end
