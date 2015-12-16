4ea1db025804163575016300d1a3dad87c42aeeb
pacosta613 <pedro.acosta613@gmail.com>
2015-12-16 14:06:06 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else array.size > 2
+    final = array.pop
+    string = array.join(", ")
+    string << ", and #{final}"
+  end
 end
