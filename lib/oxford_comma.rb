795a3836f8957f2810b9d7acd6f2ebb1d3ed27bf
“empireofryan” <ryan@ryanclifton.com>
2015-12-19 20:07:14 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last_array = "and #{array[-1]}"
+    array.pop
+    array.push(last_array)
+    array.join(", ")
+  end
 end
