80b500b66061f20f3e1d58d8d96ce1abc4e0c47e
rluftw <luxh93@gmail.com>
2015-12-22 16:59:10 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+	string = ""
+	if array.size == 1 
+		array.first
+	elsif array.size == 2
+		array.first + " and " + array.last
+	else 
+		new_array = array
+		last_element = new_array.pop
+		string = new_array.join(", ") + ", and #{last_element}"
+		string
+	end
 end
+
