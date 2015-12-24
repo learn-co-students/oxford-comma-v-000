1aca58d9faa50f12abdf3eb8bfc37ed27ca125fa
Richard Shen <rich.shen@nyu.edu>
2015-12-24 22:17:02 +0000

    Add oxford comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    phrase = "and #{array.last}"
+    array.pop
+    array << phrase
+    array.join(", ")
+  end
 end
