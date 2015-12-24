934c51c3c3db60305c6415b848e723d9c27b1990
corya0687 <corya0687@gmail.com>
2015-12-24 22:48:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  new_string = ""
 
+  if array.size > 1 && array.size != 2
+    last_item= array.pop
+    array.push("and #{last_item}")
+    new_string= array.join(", ")
+
+  elsif array.size == 1
+    new_string= array.join(", ")
+
+  elsif array.size == 2
+    last_item= array.pop
+    array.push(" and #{last_item}")
+    new_string= array.join
+
+  end
+  new_string
 end
