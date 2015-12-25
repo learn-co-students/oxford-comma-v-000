e923d5d4396005105e1dd18c7dfd7e20c05ea575
Brad1974 <danieltbrad@gmail.com>
2015-12-24 20:08:24 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
+newarray=["Jim", "James"]
+
 def oxford_comma(array)
+  if array.length > 2
+    new_last_item = "and "
+    new_last_item << array.last
+    array.pop
+    array << new_last_item
+    array.join(", ")
+  elsif
+    array.length == 2
+    new_last_item = "and "
+    new_last_item << array.last
+    array.pop
+    array << new_last_item
+    array.join(" ")
+  else
+    array.join
+  end
 
 end
+
+oxford_comma(newarray)
