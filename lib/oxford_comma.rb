b4811f578099d191dc1749ded01c046321b9d286
Chris Bruen <bruen.chris@gmail.com>
2015-12-26 11:41:36 -0500

    fix

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,14 @@
 def oxford_comma(array)
-	array[-1] = "and " + array[-1]
-	array = array.join(", ")
-	array
+	if array.size == 1
+		array = array.to_s
+		array
+	elsif array.size == 2
+		array[-1] = "and " + array[-1]
+		array = array.join(" ")
+		array
+	else
+		array[-1] = "and " + array[-1]
+		array = array.join(", ")
+		array
+	end
 end
