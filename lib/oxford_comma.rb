95a0c8eda20466e7afca29167fe56e7adc55aee5
preiser <mattpreiser@gmail.com>
2015-12-27 13:41:53 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+   if array.length == 1
+      return array.first
+   elsif array.length == 2
+      string = "#{array[0]} and #{array[1]}"
+      return string
+   else
+      last = array.pop
+      string = "#{array.join(", ")}, and #{last}"
+      return string
+   end
 end
