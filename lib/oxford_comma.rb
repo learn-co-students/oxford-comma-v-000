72d3afc640d4f944d8cf9cff15b5fd6d1ac88427
fiveinfinity <jlachance1@gmail.com>
2015-12-29 11:20:16 -0600

    add oxford_comma method, tests pass

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    array.join(",")
+  elsif array.length <= 2
+    last_index = array.pop
+    string = array.join(", ")
+    "#{string}" + " and " + last_index
+  else array.length > 2
+    last_index = array.pop
+    string = array.join(", ")
+    "#{string}" + "," + " and " + last_index
+  end
 end
+
