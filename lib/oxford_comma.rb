95da88fc8ac9a78cb837250504409f3e347864c2
John Anthony <askinganthony@gmail.com>
2015-12-30 02:29:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 def oxford_comma(array)
-  oxford_ending=" and #{array.pop()}"
+  if array.length()==1
+    return array[0]
+  end
+  if array.length()==2
+    return "#{array[0]} and #{array[1]}"
+  end
+  oxford_ending=", and #{array.pop()}"
   oxford=array.join(", ")
   oxford+=oxford_ending
-  puts oxford
+  oxford
 end
