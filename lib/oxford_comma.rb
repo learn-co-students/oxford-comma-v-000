dab604aad06373102d4a4fafe9ed3570925f5c01
azheng249 <andy.zheng249@gmail.com>
2016-01-03 20:23:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  formatted_items = ""
+  if array.size == 1
+    formatted_items = array[0]
+  elsif array.size == 2
+    formatted_items = array.join(" and ")
+  elsif array.size > 2
+    formatted_items = array[0..array.size-2].join(", ").concat(", and " + array.last)
+  end
+  formatted_items
 end
