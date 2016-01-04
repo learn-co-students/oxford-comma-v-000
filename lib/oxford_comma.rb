cf3423b40849b7f3a0c9a92b779ffe22b3af7bfe
adamgfraser <adam.fraser@gmail.com>
2016-01-04 15:54:39 -0800

    added oxford comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  size = array.size
+  if size == 1
+    array.join
+  elsif size == 2
+    array.join(" and ")
+  else
+    string = array.each_with_index.map do |element, index|
+      (index + 1 == size)? "and #{element}" : "#{element}, "
+    end
+    string.join
+  end
 end
