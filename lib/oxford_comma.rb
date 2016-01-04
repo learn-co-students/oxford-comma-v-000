d6c88846d6716b8336d0887269eaf7134bb3e508
tttra <tothutra@gmail.com>
2016-01-03 23:57:25 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+  last_item = array.pop
+  array.join(", ") << ", and #{last_item}"
+  elsif array.length == 1
+  array[0]  
+  elsif array.length == 2
+  array.join(" and ")
+  end 
 end
