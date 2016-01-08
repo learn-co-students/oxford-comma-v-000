1e5f96aacf6c76ff5b8ead41743e0b8a7d894867
Chen Yang <yangc5@mail.uc.edu>
2016-01-08 20:48:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size>=3
+    last = array.pop
+    string=array.join(", ")
+    string+=", and #{last}"
+    return string
+  elsif array.size==2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return array[0]
+  end
 end
