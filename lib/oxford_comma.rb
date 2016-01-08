f7782abbfcb8bb87480d47c9b8dde2f6288b58a0
krisnotchris <krishenderson@gmail.com>
2016-01-08 13:00:55 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    return "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.join(", ")
+  end
 end
