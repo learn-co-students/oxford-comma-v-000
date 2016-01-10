c0d0c2c12722182d3d6593a37ad63160d2766049
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-01-09 20:37:49 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    last_element = ", and " + array.pop
+    elements = array.join(", ")
+    result = elements + last_element
+    return result
+  elsif array.length == 2
+    last_element = " and " + array.pop
+    elements = array.join(", ")
+    result = elements + last_element
+    return result
+  else
+    return array.join(", ")
+  end
 end
