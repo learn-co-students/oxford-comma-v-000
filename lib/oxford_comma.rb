e425da1173bdf287ab907cbc42b0d25584eee8f7
michaelsimon <michael.simon@gmail.com>
2016-01-11 22:35:14 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.size > 1
+    string =""
+    if array.size == 2
+      string = "#{array[0]} and #{array[1]}"
+    else
+      array. each do |str|
+        if array.last == str
+          string <<"and #{str}"
+        else
+          string << "#{str}, "
+        end
+      end
+      string
+    end
+  else
+    array[0]
+  end
 end
