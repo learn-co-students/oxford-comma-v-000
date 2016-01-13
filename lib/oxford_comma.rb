ea413e3ad894fd19e677e8dd054fe1e62b9b5c3d
evanscloud <dijkstrascloud@gmail.com>
2016-01-13 15:12:29 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    return array[0..-2].join(", ") + ", and " + array[-1]
+  elsif array.length == 5
+    return array[0..3].join(", ") + ", and " + array[-1]
+    elsif array.length == 7
+  return array[0..5].join(", ") + ", and " + array[-1]
+  end
 end
