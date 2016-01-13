a8c7210d4bcf49c64619a0023f9046cb3ae56497
jenjiyi <jenny.yamada@gmail.com>
2016-01-12 22:29:19 -0800

    done with oxford comma lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.size > 2
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  elsif array.size == 2
+    array.insert(-2, " and " )
+    array.join 
+  else
+    array.join 
+  end
 end
+
