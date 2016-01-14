319b469535b5a5b7a43ec056103e548f7630c604
gormanjp <jpgorman8@gmail.com>
2016-01-13 22:24:53 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+#require 'pry'
 
+def oxford_comma(array)
+	if array.size == 1
+		return array.join
+	elsif array.size == 2
+		return array.join(" and ")
+	else
+		pop = array.pop
+		array = array.join(", ")
+		array << ", and " + pop
+	end
 end
+
+#oxford_comma(["a", "b", "c"])
