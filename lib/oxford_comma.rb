afab337cabccabc231e78594624c3574087db838
aprietof <adrianprieto00@gmail.com>
2016-01-15 17:54:24 -0500

    method passing

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    last =  "and " + array.last
+    array[-1] = last
+    array.join(" ")
+  else  
+    last =  "and " + array.last
+    array[-1] = last
+    array.join(", ")
+  end
 end
