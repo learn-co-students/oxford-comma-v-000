19e5cc9019d78899b2b8baed633cd1859a77d408
simque <simon.duncanson@gmail.com>
2016-01-15 13:25:36 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  elsif array.size > 1
+    "#{array.first} and #{array.last}"
+  else
+    array.first
+  end
 end
