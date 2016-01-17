cd33c6c21d4742fc42af2e6e6e3bb1ef6cbb1f50
lawrend <douglaslawrence314@gmail.com>
2016-01-16 16:41:03 -0800

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string_back = array.join
+  elsif array.length == 2
+    string_back = array.join(" and ")
+  else
+    string_back = array[0..(array.length-2)].join(", ")
+      string_back.concat(", and ")
+      string_back.concat(array[-1])
+  end
 end
