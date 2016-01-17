099dce36e264a38769ac3f1402813dcb71fb7176
smithWEBtek <brad@smithwebtek.com>
2016-01-17 17:31:47 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.count == 1
+    return array.join
+    elsif array.count == 2
+      return array.join(" and ")
+    elsif array.count == 3
+      last = array.last
+      array.pop
+      asdf = array.join(", ")
+      return asdf + ", and #{last}"
+    elsif array.count > 3
+      last = array.last
+      array.pop
+      asdf = array.join(", ")
+      return asdf + ", and #{last}"    
+    else
+  end
 end
