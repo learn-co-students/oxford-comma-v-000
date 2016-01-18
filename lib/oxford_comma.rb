f76cf1a299e0243fe5923c24744dd547f24204d3
mackdalton <mackdalton@gmail.com>
2016-01-18 15:11:23 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+
 def oxford_comma(array)
+  if array.length == 1
+  array.join(" ")
+elsif array.length == 2
+  array.join(" and ")
+else array.length >= 3
+  [array[0...-1].join(", "), array.last].join(", and ")
+    end
+  
 
 end
