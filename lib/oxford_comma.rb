02c914c5ededf8f4ce90cd9dd283299b43878139
LKWLaLa <lkwlala@gmail.com>
2016-01-18 18:14:01 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+     "#{array[0]} and #{array[1]}"
+  else   
+    array.insert(-2, "and")
+    last = array.pop
+    oxford = array.join(", ")
+    oxford << " #{last}"
+  end
 end
+
