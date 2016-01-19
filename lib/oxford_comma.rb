8f2ad47248416f32088a8cfab9973119990acc3c
mhnguyen289 <mhnguyen289@gmail.com>
2016-01-18 19:58:49 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = ", and #{array.pop}"
+    array = array.join(", ")
+    array << last
+  end
 end
