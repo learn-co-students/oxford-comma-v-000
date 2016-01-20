dbc0a9cc803dfa58fafae3d7e1234992509c8828
pickledyamsman <pickledyamsman@gmail.com>
2016-01-20 13:30:37 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  new_string = ""
+  if array.length < 2
+    new_string = array.join
+  elsif array.length < 3
+    new_string = array.join(" and ")
+  else
+    last =", and #{array.pop}"
+    new_string = array.join(", ")
+    new_string << last       
+  end
 end
