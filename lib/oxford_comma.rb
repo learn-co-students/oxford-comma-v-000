aab8351a4448a0c0b709a1e68b9d6b404241f829
zachnewburgh <zach.newburgh@gmail.com>
2016-01-25 10:55:24 -0500

    created strings from array indexes with join method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
+
+# if alone, no join
+# if two " and "
+# if more than two, "," between all and ", and" between last and second last
