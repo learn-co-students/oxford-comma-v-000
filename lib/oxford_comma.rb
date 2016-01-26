b52c7568a725ffb7d22465739665d54b7c5a6ed2
brianmenard <useyourbrian@gmail.com>
2016-01-26 13:44:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.length <=1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_word = array.pop
+    array << "and "
+    string_version = array.join(", ") + last_word
+  end
+
 end
