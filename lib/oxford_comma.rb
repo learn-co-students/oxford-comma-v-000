640b02864a4d3014212a530acb40c290c85d7645
Ann Lee <aplee29@users.noreply.github.com>
2016-02-01 02:46:19 -0500

    define method to convert arrays to strings and add comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.first.to_s
+  elsif array.length == 2
+    "#{array.first} and #{array.last}"
+  else
+    array.last.insert(0, "and ")
+    array.join(", ")
+  end
 end
