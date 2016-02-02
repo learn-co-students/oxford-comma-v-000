3aca3446aa1d7378ee14e350076d4fc333f4e913
jwolfe890 <jwolfe890@gmail.com>
2016-02-01 22:59:24 -0500

    oxford

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+      array.insert(-2, "and")
+      array.map! { |x| 
+        (x != "and" && x != array[-1]) ? x + "," : x }
+      array = array.join(" ")
+  end
 end
+
+
+
