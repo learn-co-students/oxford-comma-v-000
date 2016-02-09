cb30e03665bbea55340d32f1ae2f7d4f01c5ccbd
wsaxe <geosaxe@gmail.com>
2016-02-08 21:55:54 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  last_one = array.pop
+
+  if array.length > 1
+    array.join(", ") << ", and #{last_one}"
+  elsif array.length > 0
+    array.join << " and #{last_one}"
+  else
+    last_one
+  end
+
 end
