31f6bc6c4b93bc9844c501482ab93ad7aafe764b
thelucasmoore <lucasedwardmoore@gmail.com>
2016-02-10 13:40:38 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+	case array.length
+		when 1
+			return array.join
+		when 2
+			return array.join(" and ")
+		when 3
+			array[2] = "and #{array[2]}"
+			array.join(", ")
+		else
+			final = array.length-1
+			array[final] = "and #{array.last}"
+			array.join(", ")
+		end
 end
