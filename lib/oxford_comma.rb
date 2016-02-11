a84f5fef33d9146c50541f7d53d1324edf8a7ee6
Jesse <homestarflyer@gmail.com>
2016-02-11 03:29:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  and_ender = []
+  case array.count
+    when 1
+      array
+    when 2
+      array.join(' and ')
+  else
+    and_ender = array.pop
+    puts "#{array.join(", ")}and #{and_ender}"
 end
