fe115cdc23db660d8098b9b178a7eb088730c25d
hudbuz <hzb2@cornell.edu>
2016-02-13 15:26:21 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  comma = array
+  last = array.last
+  first = comma[0]
+  almost = comma[(comma.size)-1]
+  if array.size == 1
+    return comma.join
+  elsif array.size == 2
+    return comma.join(" and ")
+  elsif array.size > 2
+    lame = comma.pop
+    oxford = comma.join(", ")
+    oxford<<(", and #{lame}")
+    return oxford
+
+  end
 
 end
