9a5a306ac6d692282a8a199d2935d4c9638070fa
tmharkins <tmharkins@gmail.com>
2016-02-15 00:51:25 -0600

    solved oxford lab using .join...

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def oxford_comma(array)
+	ox_string = String.new
 
+	ox_string << array.join if array.size == 1 #for 1 dimensional array
+	ox_string << array.join(" and ") if array.size == 2 #for 2-dimensional array
+
+	i = 0
+	if array.size > 2 #for arrays greater than 2 dimensions
+		while i < array.size - 1
+			ox_string << array[i] + ", "
+			i+=1
+		end
+		ox_string << "and " + array[i]
+	end
+	ox_string
 end
