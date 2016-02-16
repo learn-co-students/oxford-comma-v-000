7e95be4c085e22649965f46bb51189dccc5a305a
ShiLi007 <omgshi888@gmail.com>
2016-02-15 22:41:18 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  return array.join() if array.size ==1
+ if array.size == 2
+    array.join(" and ")
+  elsif array.size >2
+    array[-1].insert(0, "and ")
+    array.join(", ")
 
+      
+end
 end
