a5f15353b0b93d47f53b742a0a3eddb8b76d0683
Brian Holland <brian.holland@proatria.com>
2016-02-16 20:43:51 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if (array.length>1)
+    array[-1] = "and ".concat(array.last)
+  end
+  if (array.length == 2)
+    return array.join(" ")
+  end
+  return array.join(", ")
 end
