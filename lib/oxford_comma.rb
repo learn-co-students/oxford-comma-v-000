7f971639cef49b2f91e09894ce2beebe956bd71b
agdavid <antonio.g.david@gmail.com>
2016-02-18 13:46:11 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.length <=2
+    array.join(" and ")
+  else
+    initial_portion_array = array.first(array.length-1) #shorter array excluding last element
+    initial_portion_array.join(", ") << ", and #{array[-1]}" #convert shorter array to string then shovel last element
+  end
 end
