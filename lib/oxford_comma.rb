8ecc138d241d296cd16db1b91ba2558ff0b24698
jmaerk235 <cmerrick@mail.fresnostate.edu>
2016-02-19 10:43:43 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  last = array.pop # take off the last element for now
+  if array.count > 1 # any array with more than two elements
+    array.join(", ") << ", and #{last}"
+  elsif array.count == 1 # actually 2 elements when we add back what we popped off
+    array.join << " and #{last}"
+  else
+    last
+  end
 end
