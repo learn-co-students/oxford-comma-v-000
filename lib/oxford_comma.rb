c614da40ae18ba89b2729c55dbbd3dc15c90dd05
Chris Haugen <haugenc@gmail.com>
2016-03-02 00:41:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,14 @@
 def oxford_comma(array)
-  
-  if array.count() == 1
-    return array[0]
+
+  case array.count()
+    when 1
+      return array[0]
+    when 2
+      return array.join(" and ")
+    else
+      array.push( "and #{array.pop()}")
+      return array.join(", ")
   end
-  
-  
+
+
 end
