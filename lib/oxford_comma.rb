c26627dd8e6173bbfbcdd230740044dc78941503
esimonian <eli.simonian@gmail.com>
2016-03-02 15:41:37 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else array.length >= 3
+    return "#{array[0...-1].join(', ')}, and #{array.last}"
+  end
+    
+    
 end
