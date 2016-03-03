8d7bd900e26858c655160880c76838e70381cedb
gnfisher <me@gnfisher.com>
2016-03-03 15:28:18 -0300

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count < 2
+    array.join
+  elsif array.count < 3
+    array.join(' and ')
+  else
+    t = array.pop
+    array.join(', ') + ", and #{t}"
+  end
 end
