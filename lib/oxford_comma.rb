587ccd364c8dabccabf6911cc9791539fca6ff0d
aletamarie <aleta.marie3@gmail.com>
2016-03-03 17:32:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+   if
+      array.length == 1
+      array.join()
+   elsif
+      array.length == 2
+      array.join (" and ")
+    else
+      beginning = array[0...-1].join ", "
+      ending = "and #{array.last}"
+      "#{beginning}, #{ending}"
+  end
 end
+
