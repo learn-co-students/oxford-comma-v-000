3323add323441fc208c7d0e2dbcab4125a9357bc
Asher Sullivan <asherfolsomsullivan@gmail.com>
2016-03-04 18:40:52 +0000

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    array[0..-2].join(", ") + ", and " + array[-1]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else array.join(", ")
+  end
 end
