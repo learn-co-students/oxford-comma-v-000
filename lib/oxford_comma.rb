5ce5774e0154a2d99aa5827ebb9ea28d22c51403
aback1227 <aback1227@gmail.com>
2016-03-07 00:20:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    "#{array[0]}"
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    last_word = array.last
+    array.pop
+    string = array.join(", ")
+    string << ", and #{last_word}"
+  end
 end
