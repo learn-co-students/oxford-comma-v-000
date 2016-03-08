b4daf0c465672438731ac1e6ac7f308d4c4d3ed8
jchu4483 <jchu4483@gmail.com>
2016-03-08 15:57:08 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,10 @@ def oxford_comma(array)
     array.join(', ')
   end
 end
+
+=begin
+The key here is the else statement. If the size of the array is greater than 2,
+then I modify the last element of the array using [-1] index and change it so that it
+"and " word right in front of it - it also keeps the existing last element. This 
+makes it so that the last element in the array is literally "and #{element}". Then I use
+.join with a comma argument to format it correctly. 
