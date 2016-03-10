fcaec09491df6c45aae14e5dfc7777e94364c3c8
hangloos <ericloos00@gmail.com>
2016-03-10 16:12:29 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
 
+
+
+
+def oxford_comma(array)
+  
+  if array.length > 2
+    last_element = []
+    last_element = array.pop
+    array.join(", ")  << ", and #{last_element}"
+  elsif array.length > 1
+    array.join(" and ")
+  else 
+    array.join
+  end
 end
