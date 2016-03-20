f0bc48ecfacf3b9878a1e5ee6568c8e54e6c7de7
adriandave13 <adrian@paradoxlogic.net>
2016-03-20 17:23:42 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    array[0..(array.length - 2)].join(", ") << ", and #{array.last}"
+  end
 end
