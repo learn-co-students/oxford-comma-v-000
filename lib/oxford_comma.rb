64dae04456914151dddb45305ad4acdbd3ec368c
yeungn <nin.yeung@gmail.com>
2016-04-04 19:22:46 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  last_array_element = array.last
+  updated_array = array[0...-1]
 
+  if array.length < 3
+    array.join(" and ")
+  else
+    new_array = updated_array.join(", ") << ", and #{last_array_element}"
+  end
 end
