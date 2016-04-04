59feb2c476b64d01c93026dbed8f07cc38f3c04c
yeungn <nin.yeung@gmail.com>
2016-04-04 19:26:41 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,7 @@
 def oxford_comma(array)
-  last_array_element = array.last
-  updated_array = array[0...-1]
-
   if array.length < 3
     array.join(" and ")
   else
-    new_array = updated_array.join(", ") << ", and #{last_array_element}"
+    "#{array[0...-1].join(", ")}, and #{array.last}"
   end
 end
