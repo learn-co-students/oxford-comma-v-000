12f7d5a4082024e33868cc61c75490c96c5f9aee
nOraKat147 <n.mori.iphone@gmail.com>
2016-04-04 22:26:02 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.count > 1
+    last_item = array.pop
+    last_item = "and " + last_item
+    array.push(last_item)
+  end
+  if array.count == 2
+    string = array.join(" ")
+    puts string
+    string
+  else
+    string = array.join(", ")
+    puts string
+    string
+  end
 end
+
+array = ["fiddleheads","okra"]
+
+oxford_comma(array)
