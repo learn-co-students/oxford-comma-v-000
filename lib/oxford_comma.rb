dee71f1714141fad3aa88873ec1d3308f4cef26c
thebrianemory <the@brianemory.com>
2016-04-11 07:58:16 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 2
+    array.join(" and ")
+  elsif array.count > 2
+    array.join(",  ").split("  ").insert(-2, "and").join(" ")
+  else
+    array.join
+  end
 end
