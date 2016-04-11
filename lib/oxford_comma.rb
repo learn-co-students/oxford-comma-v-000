48c3726e806d8d7776a08d4d8bebafa18fca1d4e
tmb41 <tylor.balson@gmail.com>
2016-04-11 10:25:30 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  if array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.size < 2
+    array.join(", ")
+  elsif array.size > 3
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
+      
 end
