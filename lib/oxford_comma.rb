9b20a29007933faf50d90bbec3c10441c84e2d4e
msosborne3 <msosborne@ymail.com>
2016-04-11 20:56:40 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  #converts an array to a string.
+  num = array.count
+
+  if num == 1
+    return array[0]
+  elsif num == 2
+    array.join(" and ")
+  elsif num > 2
+    array[0..num-2].join(", ") << ", and " << array[num-1]
+  end
 
 end
