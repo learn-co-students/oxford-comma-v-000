d9738ec058a8597bfe0be98e8028f294f21a20e9
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2016-04-20 02:56:42 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,11 @@ def oxford_comma(array)
     if array.size == 2
        return array.join(" and ")
     elsif array.size >= 3
-      new_last = "and " + array.last
-      array.pop
-      array << new_last
-      return array.join(", ")
-   else
+        new_last = "and " + array.last
+        array.pop
+        array << new_last
+       return array.join(", ")
+    else
    	  array[0]
     end
     
