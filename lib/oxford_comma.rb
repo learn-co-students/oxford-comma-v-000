dcd513a620ee9db998b31d06283fe4943415b6ca
Dax Blemberg <dax.blem@gmail.com>
2016-04-22 22:03:54 -0400

    finished

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    list = ""
+    until array.size == 1
+      new_item = array.first
+      list += "#{new_item}, "
+      array.shift
+    end
+    array.unshift(list).join("and ")
+  end
 end
