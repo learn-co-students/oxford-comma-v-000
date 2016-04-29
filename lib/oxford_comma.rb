3329947a06197d89ef4e175097761459ac2855ab
Justin Hall <hall.justin.d@gmail.com>
2016-04-28 21:31:14 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 2
+    array.join(" and ")
+  elsif array.count > 2
+    last_word = array.last
+    array.pop
+    array.join(", ") + ", and " + last_word
+  else
+    array.join
+  end
 end
