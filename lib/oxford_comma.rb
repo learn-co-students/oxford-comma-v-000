499460a6342e29b0ed31237ec7a2e65ecf75f302
Being Young <beingy@gmail.com>
2016-05-01 17:07:05 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.first.to_s
+  elsif array.length == 2
+    array * (" and ")
+  elsif array.length == 3
+    array[0] + ", " + array [1] + ", and " + array[2]
+  elsif array.length > 3
+    array[-1] = "and " + array[-1]
+    array * (", ")
+  end
 end
