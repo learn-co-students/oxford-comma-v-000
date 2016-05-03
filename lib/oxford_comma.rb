fd0d475e995b4d8f82298a288a755b425b75d22b
brad72287 <brad72287@gmail.com>
2016-05-03 12:09:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+if array.count == 1
+  return array.join
+end
+  if array.count == 2
+    return array.join(" and ")
+  end
 
+  if array.count > 2
+    array[-1] = "and "+array.last
+    return array.join(", ")
+  end
 end
