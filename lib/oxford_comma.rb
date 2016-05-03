d26138e383b47f787fa15cc6550c1f5340343e6c
iandevivi <iandevivi@gmail.com>
2016-05-03 17:12:57 -0400

    completed oxford comma lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.count == 1
+    array[0]
+  elsif array.count == 2 
+    array.join(" and ")
+  elsif array.count > 2
+    array[0..-2].join(", ") + ", and " + array[-1]    
+  end
 end
