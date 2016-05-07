257137c69da52431edfe528b5bd207db7a081426
hilaryml <hilyttle@gmail.com>
2016-05-07 14:50:03 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.length == 1
+		array.join
+	elsif array.length == 2
+		array.join(" and ")
+	elsif array.length >= 3
+		#use .pop to remove last element add "and" and then shovel it back on the end and .join with comma separation
+		array << "and #{array.pop}"
+		array.join(", ")
+	end
 end
