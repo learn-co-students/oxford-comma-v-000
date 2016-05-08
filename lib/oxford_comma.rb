1f316f0d7b19bfd9e415688a7d866feaf3c478bc
SnowboardTechie <bryan.thompson518@gmail.com>
2016-05-08 07:51:35 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count > 2
+    ending = array.pop
+    phrase = array.join(", ")
+    phrase + ", and #{ending}"
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    array.join(", ")
+  end
 end
