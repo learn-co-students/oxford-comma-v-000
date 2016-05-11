27b41e934778165288470d9c19e44623a45dc46d
Anita Ford <anitamford@gmail.com>
2016-05-10 23:12:02 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    return [array[0...-1].join(", "), array.last].join(", and ")
+  elsif array.size == 2
+    return array.join " and "
+  else return array.join(", ")
+  end
 end
