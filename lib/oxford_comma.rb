ece1a486118e2a77f314640adebbae0674e72630
Emmanuel Owusu <e_owusu@live.com>
2016-05-15 14:44:22 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,47 @@
-def oxford_comma(array)
+cities = []
+=begin
+def oxford_comma(cities)
+  if cities.length < 3
+    cities.join(" and ")
+  elsif
+    cities.length == 3
+    cities.insert(1, ", ")
+    cities.insert(3, ", ")
+    cities.insert(4, "and ")
+    cities.join()
+  elsif
+    cities.length > 3
+    cities.insert(1, ", ")
+    cities.insert(3, ", ")
+    cities.insert(5, ", ")
+    cities.insert(7, ", ")
+    cities.insert(-2, "and ")
+    cities.join()
+  end
+end
+=end
 
+=begin
+def oxford_comma(cities)
+  cities.each_with_index do |places, index|
+    if index < cities.length - 2
+      cities << ", "
+    elsif index < cities.length - 1
+      cities <<" and"
+    end
+  end
+end
+=end
+
+def oxford_comma(cities)
+  cities.each_with_index do |places, index|
+    if cities.length == 2
+      return cities.join(" and ")
+    elsif index < cities.length - 2
+      places << ", "
+    elsif index < cities.length - 1
+      places <<", and "
+    end
+  end
+  cities.join()
 end
