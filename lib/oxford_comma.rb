11aa2e3bb259977e5de132e8f193cec32f5f483c
bmolina-nyc <bruce.d.molina@gmail.com>
2016-05-15 10:36:21 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  array << "and #{array.pop}" if array.length > 1 
+
+    case array.length 
+    when 1
+      array.join
+    when 2
+      array.join(" ")
+    else
+      array.join(", ")
+    end
 
 end
