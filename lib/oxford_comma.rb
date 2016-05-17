8ed91da1254dfea1270d143711567c602198b60d
vicision <duncan.j.mclachlan@gmail.com>
2016-05-16 22:51:16 -0400

    Done: passing all tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    list = array[0...-1].join(", ")
+    list << ", and #{array.last}"
+    list
+  end
 end
