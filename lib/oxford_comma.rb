7a64b50c4f8a948d2997d20d88845fe0fe9c6edb
Shoko Furushita <shktkt219@gmail.com>
2016-05-17 22:32:14 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array.join}"
+  elsif array.length == 2
+    "#{array.join(" and ")}"
+  elsif array.length == 3
+    array << "and #{array.last}"
+    array.delete_at(2)
+    "#{array.join(", ")}"
+  else
+    array << "and #{array.last}"
+    array.delete_at(array.length-2)
+    "#{array.join(", ")}"
+  end
 end
