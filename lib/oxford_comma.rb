27aae8c403b379c951caaaa6e70d1a7283f9dbd1
Lenn Hypolite <lenntriniman@hotmail.com>
2016-05-21 14:26:15 -0400

    initial commit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
 def oxford_comma(array)
+  # join method turns the array into a strings
+  # and the comma in brackets seperates each string
+  if array.size == 1
+        array.join
 
+  elsif array.size == 2
+        array.join(' and ')
+
+  elsif array.size == 3
+    last = array.pop
+    array.join(','' ') +',' + ' and ' + last
+
+  elsif array.size > 3
+    last = array.pop
+    array.join(','+' ') + ',' ' and ' + last
+  end
 end
+# array holds values
+# each value is a string seperated by commas
+# some strings are longer than others
+# remove the strings from out of the array
+# return string values together in a line seperated by a comma
+# converted = []
+#   array.each do | element |
+#   converted << "#{element}"
+# end
