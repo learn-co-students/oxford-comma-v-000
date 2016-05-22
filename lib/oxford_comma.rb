13596361022bba74027d54110d245933694176fb
alice <github email address>
2016-05-22 16:10:38 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  if array.count == 2
+    array.join(" and ")
 
+  elsif array.count >= 3
+    oxford = ""
+    array.each_index do |word|
+      word == array.count - 1 ? oxford << "and #{array[word]}" : oxford << "#{array[word]}, "
+    end
+    oxford
+
+  else array.count == 1
+    array.join(", ")
+  end
 end
