f6003cdf851841b7082da59bfba502368d13ab1f
jack woodward <jackwoodwardschool@gmail.com>
2016-05-24 23:01:13 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+    if array.count == 1
+    result = array.join
+elsif array.count == 2
+    array.join(" and ")
+elsif array.count == 3
+    array.insert(1, ", ")
+    array.insert(3, ", ")
+    array.insert(4, "and ")
+    array.join("")
+    else
+    array[0..-2].join(", ") + ", and " + array[-1]
+
+end
 
 end
