d3fda24abb0494a442c153aa95e2d19a7c790193
Joseph Lim <joseph.d.lim@gmail.com>
2016-05-27 11:14:24 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 0
+    "Empty!"
+  elsif array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join" and "
+  elsif array.size > 2
+    "#{array[0..-2].join(", ")}, and #{array.last}"
+  end
 end
