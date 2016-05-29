55ba78dba2a9c68bd102c056185b8a96fb869d26
Adam Cooper <amcooper@gmail.com>
2016-05-29 00:03:58 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  array_size = array.size
+  result = ""
+  if array_size == 1
+    result = array[0]
+  elsif array_size == 2
+    result = "#{array[0]} and #{array[1]}"
+  else
+    while array.size > 1
+      result = result + array.shift + ", "
+    end
+    result = "#{result}and #{array[0]}"
+  end
+  result
 end
