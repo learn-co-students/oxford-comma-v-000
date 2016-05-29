be9bb86bd78ddf54295099ccbac47edab67dc209
interestinall <justin.lefurjah@gmail.com>
2016-05-29 11:51:43 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  return array[0].to_s if array.length == 1
+  if array.length == 2 
+    return array[0].to_s + " and " + array[1].to_s
+  elsif array.length == 3
+    return array[0].to_s + ", " + array[1].to_s + ", and " + array[2].to_s
+  elsif array.length > 3
+    return array[0..array.length-2].join(", ") + ", and " + array[array.length - 1]
+  end
+    
+    
+
 end
