892c761fedc97f4d1979d8b7efadddd4d2437116
andrewstone8 <andrewstone8@gmail.com>
2016-06-08 20:31:11 +0900

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.insert(-2," and ").join
+  else
+    array.map { |x| x + ", "}.insert(-2, "and ").join.chomp(", ")
+  end
 end
