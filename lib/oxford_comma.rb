8528210236c5bb0816711c6edd48791ae6f6fbe3
Traci7822 <tracir@gmail.com>
2016-06-15 19:25:48 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last_item = array.pop
+    array.insert(-1, "and")
+    fruit_list = array.join(", ")
+    return "#{fruit_list} #{last_item}"
+  end
 end
