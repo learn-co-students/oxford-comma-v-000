548172ce7647947b2ef6c2bf6a12ab53390636f1
David Kennell <dkennell998@hotmail.com>
2016-06-17 15:50:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  list = ""
+  if array.size == 1
+    array.first
+  elsif array.size ==2
+    array.first + " and " + array[1]
+  else
+    array.each_with_index.map do |word|
+      if word != array.last
+        list += word + ", "
+      else
+        list += "and " + word
+      end
+    end
+    list
+end
 end
