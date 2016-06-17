04e150e92563cef62fec2b1a282984046de7ea4a
Scott Savage <snsavage@gmail.com>
2016-06-16 21:14:01 -0400

    Passing tests for oxford_comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_element = array.pop
+    array << "and #{last_element}"
+    array.join(", ")
+  end
 end
