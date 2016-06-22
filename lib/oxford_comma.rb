7224fdd8e8178fae09e6ef93a4e0d577f10d5173
Howard DeVennish <howarddevennish@gmail.com>
2016-06-22 01:03:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
+
 def oxford_comma(array)
   if array.length == 1
     return array.join
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
-    last = array.pop
-    last.insert(0, "and ")
-    array << last
+    array[-1].insert(0, "and ")
   end
   array.join(", ")
 
