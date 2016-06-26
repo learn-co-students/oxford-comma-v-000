1ac521729363dcf82f9ad76160f80b5fbb96fd73
Binny Lewis <binnylewis@gmail.com>
2016-06-26 08:25:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0]
+  elsif array.count == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    lastitem = array.last
+    lastitem = "and #{lastitem}"
+    array.pop
+    array << lastitem
+    array.join(", ")
+  end
 end
