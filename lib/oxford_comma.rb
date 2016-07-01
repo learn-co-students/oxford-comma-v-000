5fe8bec16a5a57a5b805822823dbeeedc71b394a
Juan Gongora <gongora.animations@gmail.com>
2016-07-01 18:29:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array*","
+  elsif array.size == 2
+    array.join(" and ")
+  else array.size == 3
+    [array[0...-1].join(", "), array.last].join(", and ")
+  end
 end
