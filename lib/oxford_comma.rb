8c3250ed6e93aad995c75e165d363420e3319b60
vanita eckert <vanita112003@gmail.com>
2016-07-05 20:13:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+  array.join
+
+  elsif array.length == 2
+array.join(' and ')
+
+elsif array.length == 3
+array[-1].prepend('and ')
+array.join(', ')
+
+else
+array[-1].prepend('and ')
+array.join(', ')
+
+end
 end
