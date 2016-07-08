eb2604ef096df432f5594ada007f058d845766ee
arturusfury <bri.reynolds@gmail.com>
2016-07-08 15:29:47 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  line = ''
 
+  if array.size == 1
+    line = array.first
+  elsif array.size == 2
+    line = "#{array[0]} and #{array[1]}"
+  else
+    until array.size == 1
+      line += "#{array.shift}, "
+    end
+
+    line += "and #{array[0]}"
+  end
+
+  line
 end
