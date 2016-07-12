927f6ec44d4b67005e83a75a0f702a3e80e8a210
Frederico  Morato <freddymorato@gmail.com>
2016-07-12 22:00:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+	if array.length == 1
+		array.join
+	elsif array.length == 2
+		array.join (" and ")
+	elsif array.length > 2
+ 		string = array[0, array.length-1].join(", ")
+  		string << ", and #{array[array.length-1]}"
+  	end
 end
