96df5d8906874e633a11338ab68b6c237fc3dd6d
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-07-25 04:14:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.length == 2
+   string = array.join(" and ")
+  elsif array.length == 3
+    string = array[0..1].join(", ")
+    string += ", and "
+    string += array[2]
+  else
+    string = array.join(", ")
 
+  end
+   string
 end
+
+
+ #use -1 for arrays and make 1 string for last array with and then 2nd string wiht .join(comas)#
