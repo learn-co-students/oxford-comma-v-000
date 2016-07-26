c0c87940919b08a08930a8e964af0a7da2eafb33
William Jones <enquiries@forestfed.com>
2016-07-26 17:36:19 +0000

    use array and string methods

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  line_str = ""
+  array.each_with_index do |name, index|
+    if index > 0
+      if index == array.size - 1
+        if array.size == 2
+          line_str.insert(-1, " and ")
+        else
+          line_str.insert(-1, ", and ")
+        end
+      else
+        line_str.insert(-1, ", ")
+      end
+    end
+    line_str.insert(-1, "#{name}")
+  end
+  line_str
 end
