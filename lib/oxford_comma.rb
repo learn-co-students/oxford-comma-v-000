06e3178dbaed8b7a7417a2a61abac6a64e53f9e5
Kenlyn Terai <kkterai@gmail.com>
2016-08-05 03:40:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length ==1
+    array.join
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else array.length > 2
+    with_commas = array[0..-2]*", " + ", and " + array[-1]
+  end
 end
