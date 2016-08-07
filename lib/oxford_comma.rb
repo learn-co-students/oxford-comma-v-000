c3142b12d198094c55fd24389bbf8cf33ec8309a
alispace <alispace410@gmail.com>
2016-08-07 12:38:00 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")
+	if array.length > 2
+		array.last.insert(0, "and ")
+		array.join(", ")
+	elsif array.length == 2
+		return "#{array[0]} and #{array[1]}"
+	else
+		array.join
+    end
 end
