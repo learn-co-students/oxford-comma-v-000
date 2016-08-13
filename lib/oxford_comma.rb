658361dd9995d30d633400749cd4cfe05c5e7260
Andrea Toston <tostonar@gmail.com>
2016-08-13 07:40:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 1
+    array.join(" and ")
+  else
+    last_item = array.pop
+
+    first_items = array.join(", ")
+    "#{first_items}, and #{last_item}"
+  end
 
 end
