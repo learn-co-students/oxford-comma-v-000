6ec7cd26fc3066ec5f8ae06e12b175712d73a536
Justin Acker <justin@justinacker.com>
2016-08-17 17:48:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    output = array.join
+    return output
+  elsif array.length == 2
+    output = array.join(' and ')
+    return output
+  elsif array.length >= 3
+    output = String.new
+    array.each_with_index do |word, index|
+      if index == (array.length - 1)
+        output += "and "
+        output << word
+      else
+        output << word
+        output += ", "
+      end
+    end
+    return output
+  end
 end
+
+#oxford_comma(["kiwi","durian"])
