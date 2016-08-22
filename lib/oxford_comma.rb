a4637ba46bae21f444eb57bfb5c0648ac29ecfd0
Kenneth Holmquist <madifana@yahoo.com>
2016-08-22 02:50:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+   if array.length == 1
+     output = array.join
+     return output
+   elsif array.length == 2
+     output = array.join(" and ")
+     return output
+   elsif array.length >= 3
+     output = String.new
+     array.each_with_index do |oxford, index|
+       if index == (array.length - 1)
+         output += "and "
+         output << oxford
+       else
+         output << oxford
+         output += ", "
+       end
+     end
+     return output
+   end
 end
