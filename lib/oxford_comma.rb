d5c82cd638e6a4db8bf35f7623be2c88ac6f142e
Samuel Lair <lair001@gmail.com>
2016-08-27 05:53:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array[0] + " and " + array[1]
+  elsif array.size > 2
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  else
+    return "I am Error!"
+  end
 end
