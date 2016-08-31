00d136296e073bece887aac06e2ee381bc15c9ee
Benny Hudson Hung <wakenightryu@gmail.com>
2016-08-31 04:01:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  new_array = String.new
+  new_array << "#{array.last}"
+  if array.size <= 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array.join(", ")
+    array.pop
+    array << "and"
+    puts array + new_array
+  end
 end
