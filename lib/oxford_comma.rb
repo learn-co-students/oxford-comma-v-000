a4fd1367fe5e2162c94b32e071cccf9d2dffea80
Ryan Evans <ryanwevans@icloud.com>
2016-09-05 21:02:01 -0700

    convert array to string

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  arrayString = ""
+  numberOfElements = array.length
 
+  if numberOfElements == 1
+    array.each {|element| arrayString += "#{element}"}
+  elsif numberOfElements == 2
+    arrayString += "#{array.join(" and ")}"
+  else numberOfElements > 2
+    lastElement = array.pop
+    arrayString += "#{array.join(", ")}, and #{lastElement}"
+  end
+  arrayString
 end
