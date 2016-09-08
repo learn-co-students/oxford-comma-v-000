ee899306822b41dea68318230a92d23978f7dfbf
Janine Rosen <janinemrosen@gmail.com>
2016-09-08 22:16:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+# require 'pry'
 def oxford_comma(array)
-
+  if array.length == 1
+    result = array[0]
+  elsif array.length == 2
+    result = array.join(" and ")
+  else array.length >= 3
+    result = array[-1].insert(0, "and ")
+    array.join(", ")
+  end
 end
