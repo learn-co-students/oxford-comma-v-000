d3ef423053e9d65d39e20828d9dfc0e610952972
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2016-09-10 03:06:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[0...-1].join(", ") + ", " + "and " + array[-1]
+  else
+    array.join
+  end
 end
