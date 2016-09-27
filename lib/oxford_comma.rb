54ca2755ce1dcf650254c65ea6909e83b98f41c7
treverandersen <trever.r.andersen@gmail.com>
2016-09-27 12:21:20 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-
+	return array.join if array.nil? or array.length <= 1
+	return array.join(" and ") if array.length == 2
+	array[0..-2].join(", ") + ", and " + array[-1]
 end
+
