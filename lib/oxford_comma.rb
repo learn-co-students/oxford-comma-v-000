a238b50c23fad28644835b01bd982f2be2fa0125
Jenna Leopold <jleopold424@gmail.com>
2016-09-28 20:39:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last_element = array.pop
+    string = array.join(", ")
+    string << ", and #{last_element}"
+  end
 end
