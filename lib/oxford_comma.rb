f8fe80152e74dccb95b376a2e377e29a6e8bcb8e
danielvachalek <daniel.vachalek@gmail.com>
2016-10-01 16:04:28 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    array.push("and #{array.pop}").join(", ")
+    
+    end
 end
