1a4fbb6ce23549e1ca3e775b75cc204cea3f857c
Ray Mulligan <rmullig2@optonline.net>
2016-10-11 16:26:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    output = ""
+    while array.size > 1
+      output += "#{array.shift}, "
+    end
+    return output += "and #{array[0]}"
+  end
 end
