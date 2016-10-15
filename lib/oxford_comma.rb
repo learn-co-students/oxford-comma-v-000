c7795f41fe8151c6663fe5ef079ab3741528fed7
Alexis Roman <alexismmroman21@gmail.com>
2016-10-15 19:16:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  array.collect do |arr|
+    if arr == array[-1]
+      array.push(" and #{arr}")
+    elsif arr== array[0]
+      array.push("#{arr},")
+    else
+      array.push( " #{arr},")
+    end
+
+  end.join("")
 end
