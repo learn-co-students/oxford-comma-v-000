f1e80b20428ea84c0ffb5d58d21a0a6e3c7e31ca
Jon Fougner <jon.fougner@gmail.com>
2016-10-21 00:12:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  last_element = array.pop
+  oxford_string = ""
+  if array.size == 1
+     oxford_string = array[0] + " and "
+  elsif array.size > 1
+    array.each do |element|
+      oxford_string += element +", "
+    end
+    oxford_string += "and "
+  end
+  oxford_string += last_element
 end
