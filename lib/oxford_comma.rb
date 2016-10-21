1f588c4aa71b7412a331d224e108031be9d3597c
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2016-10-21 02:04:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+    if array.length>2
+    array[array.length-1]="and "+array[array.length-1]
+    return array.join(", ")
+elsif array.length == 2
+    return array.join(" and ")
+end
+array[0]
+
 
 end
