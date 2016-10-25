a5674b51139b8358fdfc197b308a6e908c0277f2
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-10-25 05:06:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+if (array.length == 1)
+ return array[0]
+ elsif (array.length == 2)
+   ["kiwi","durian"].join(" and ")
+ elsif(array.length ==3)
+   array[0].join(array[1])
 
+end
 end
