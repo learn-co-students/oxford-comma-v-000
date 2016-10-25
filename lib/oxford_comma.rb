198bf20d4c0a5f4a787a7d23cc1180e004b0f044
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-10-25 18:46:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,21 @@
 def oxford_comma(array)
-if (array.length == 1)
+  if (array.length == 1)
  return array[0]
  elsif (array.length == 2)
    ["kiwi","durian"].join(" and ")
  elsif(array.length ==3)
-   array[0].join(array[1])
-
+   array[0]<<",";
+   array[1]<< ", and"
+   array.join(" ")
+elsif (array.length>3)
+array.each do |ele|
+if(array.index(ele) == array.length-1)
+  ele<<""
+elsif(array.index(ele) == array.length-2)
+  ele << ", and"
+else
+  ele<<","
+end
+end.join(" ")
 end
 end
