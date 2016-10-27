5af150a89bf329ab17c868ebe71f3776f38972fd
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-27 09:49:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+   n=array.length()-1;
+   r=""
+   if n==1
+     return "#{array[0]} and #{array[1]}"
+   elsif n==0
+     return array[0]
+   end
+   for i in 0..n
+     r+=array[i]
+     r+=", " if i<n
+     r+="and " if i==n-1
+   end
+   return r
 end
