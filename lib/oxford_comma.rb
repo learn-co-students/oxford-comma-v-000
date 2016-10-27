6487eb527ad44e6933f1b56261a88cf8415ce5e2
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-10-27 04:03:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  if(array.size == 1)
+    array.join(" ")
+  elsif (array.size == 2)
+    array.join(" and ")
+  else
+    oxford_array = Array.new
+    array.each_with_index do |item, index|
+      if(index < array.size - 1)
+        oxford_array << (item + ",")
+      elsif (index == (array.size - 1) )
+        oxford_array.insert(index, "and")
+        oxford_array << item
+      end
+    end
 
+    return oxford_array.join(" ")
+  end
 end
