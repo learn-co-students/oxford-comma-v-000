9334d4990f5e2141ab05a70bd8b8fa8a1986e454
Keegan Leitz <kjleitz@gmail.com>
2016-10-31 14:12:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array[0]
+  when 2
+    array.join(" and ")
+  else
+    last = array.pop
+    array << "and #{last}"
+    array.join(", ")
+  end
 end
