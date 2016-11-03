635fe40c4abdd58c5510eccceb88233b2f2fdf81
Dequan Xiao <splscoupon@gmail.com>
2016-11-03 13:56:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  mystring=""
+  enddigits = array[-1].length + 3
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    array.each do |word|
+      mystring = mystring + "#{word}, "
+    end
+  end
+  no_end_comma = mystring[0..-(enddigits)]
+  no_end_comma + "and #{array[-1]}"
 end
