b957668a592f40945b1f17e9b458ce9f4ec35aa5
John Pfingst <jpfingst@outlook.com>
2016-11-07 19:32:14 +0000

    comma format array elements

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  result = ''
+  case array.size
+    when 1
+      result = array[0]
+    when 2
+      result = "#{array[0]} and #{array[1]}"
+    when 3
+      result = "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    array[0..-2].each_with_index {|value,  index| result += "#{value}, "}
+    result += "and #{array[-1]}"
+  end
+  result
 end
