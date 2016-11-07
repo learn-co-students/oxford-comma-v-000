73aa130e44842b4e9cff2ab3c4b49964c218dd7b
John Pfingst <jpfingst@outlook.com>
2016-11-07 19:49:19 +0000

    condense the code a bit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,10 @@
 def oxford_comma(array)
-  result = ''
   case array.size
-    when 1
-      result = array[0]
-    when 2
-      result = "#{array[0]} and #{array[1]}"
+    when 1 then array[0]
+    when 2 then "#{array[0]} and #{array[1]}"
   else
-    array[0..-2].each_with_index {|value,  index| result += "#{value}, "}
+    result = ''
+    array[0..-2].each_with_index {|value, index| result += "#{value}, "}
     result += "and #{array[-1]}"
   end
-  result
 end
