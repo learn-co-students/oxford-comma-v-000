9229c7a19608befdf0b9828c5684e9435540088b
Tayari Appiah <cjhhh.ta@gmail.com>
2016-11-13 21:24:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
+=begin
 def oxford_comma(array)
+if array.length ==  1
+  array.join()
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length > 3
+  array.join(", ").insert(13, " and")
+else
+  array[-1].insert(0, "and ")
+ end
+ array.join(", ")
+end
+=end
 
+
+
+def oxford_comma(array)
+if array.length ==  1
+  array.join()
+elsif array.length == 2
+  array.join(" and ")
+else
+  array[-1].insert(0, "and ")
+   array.join(", ")
+ end
 end
