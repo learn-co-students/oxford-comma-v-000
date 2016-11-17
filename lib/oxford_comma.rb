07543f13404c7d8558402d4bd2447ebd3ed0dcfb
Daniel Warren <warrend555@gmail.com>
2016-11-17 15:40:08 -0500

    refactors #oxford_comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,32 +1,8 @@
 def oxford_comma(array)
-  if array.count == 1
-    array.join
-  elsif array.count == 2
-    oxfordize(array)
-    array.join(" ")
-  else
-    oxfordize(array)
-    array.join(", ")
+  if array.count == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.count > 2
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
-
-def oxfordize(array)
-  array << "and #{array.last}"
-  array.delete_at(-2)
-end
-
-# def oxford_comma(array)
-#   last_element = "and #{array.last}"
-#   words = array.count 
-#   if words == 1
-#     array.join
-#   elsif words == 2
-#     array.pop
-#     array << last_element
-#     array.join(" ")
-#   else
-#     array.pop
-#     array << last_element
-#     array.join(", ")
-#   end
-# end
