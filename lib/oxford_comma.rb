8504eb75ce20291f3db6e7004e5c7ff883aabe35
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-18 01:49:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  n = array.length
+    if n == 1
+      array.join
+    elsif n == 2
+      array.join(" and ")
+    else
+      x = array[n - 1]
+      array.pop
+      new_string = array.join(", ")
+      new_string = new_string + ", and " + x
+      return new_string
+    end
 end
