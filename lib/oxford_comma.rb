0da0026b9308ceeff33ef4dacd08809cbd7f3846
Michael Burak <michaelrburak@gmail.com>
2016-11-20 04:22:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  while (array.length == 1)
+    return array.join
+  end
+  while (array.length == 2)
+    return array.join(" and ")
+  end
+  while (array.length == 3)
+    array[2] = "and #{array[2]}"
+    return array.join(", ")
+  end
+  while (array.length > 3)
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+end
 end
