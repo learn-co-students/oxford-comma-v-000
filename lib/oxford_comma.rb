0d706fe57acd82dd7d9d94136a48da1d6ad94760
Yamini Kumawat <yaminikumawat94@gmail.com>
2016-12-04 23:32:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.size == 1
+    array = array.join
+  elsif array.size == 2
+    array = array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 
 end
