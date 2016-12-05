4e14e547c00b944d80660ffdeabe6fe93639ff69
Alyssa Sidebottom <alyssa.m.sidebottom@gmail.com>
2016-12-05 18:23:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  for i in array
+    if i == array.last and array.length > 2
+      i = "and " + i
+      array.pop
+      array.push(i)
+    end
+  end
+  if array.length == 2
+    string = array.join(" and ")
+  else
+    string = array.join(", ")
+  end
+  return string
 end
