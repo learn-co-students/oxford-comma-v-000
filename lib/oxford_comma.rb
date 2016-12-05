cb5904038a9c291bcdcaf5d8516ce124a0cb1ddd
Krishel Lasam <krishel.lasam@gmail.com>
2016-12-05 00:20:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  words = array.length
+  if words == 1
+    return array[0]
+  elsif words == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif words == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last_element = array.pop
+    return "#{array.join(", ")}, and #{last_element}"
+  end
 end
