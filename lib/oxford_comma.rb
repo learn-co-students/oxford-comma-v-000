4ed34f5aa14e2b7cea2acf660fdca2611171fc3b
Adam Lucia <lucia.adam@gmail.com>
2016-12-06 00:01:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  case array.count
+  when 1 then array[0]
+  when 2 then array[0] + " and " + array[1]
+  else
+    last = array.pop
+    array.join(", ") + ", and " + last
+  end
 end
