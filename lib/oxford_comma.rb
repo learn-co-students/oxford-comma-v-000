4127c764eeec93bef90e092fc385ffff96786270
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-12-09 21:10:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.empty? || array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    [array[0...-1].join(", "), array.last].join(", and ")
+  end
 end
+
+puts oxford_comma(["as", "sd"])
