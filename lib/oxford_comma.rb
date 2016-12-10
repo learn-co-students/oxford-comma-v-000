f5f974452407abf9fb23cfe13e249e54b4d26059
charlie lee <ctlee9096@gmail.com>
2016-12-10 23:13:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,38 +3,32 @@ def oxford_comma(array)
   array_count=array.count
   array2=[]
   if array_count == 1
-  array.each do|value|
-    array2 << ("#{value}")
-    end
-    return array2.join(" ")
-  elsif array_count == 2
     array.each do|value|
       array2 << ("#{value}")
-    end
-        return array2.join(" and ")
-  elsif array_count == 3
-    array.each do|value|
-      array2 << ("#{value}")
-    end
-    array2.insert(2, "and ")
-    array2.insert(1,", ")
-    array2.insert(3,", ")
-    array2.join
- elsif
-    array.each do|value|
-     array2 << ("#{value}" ", ")
-    end
-    array2.insert(-2, "and ")
-    #binding.pry
-    array3 = []
-    #binding.pry
-    array3 = array2.pop
-    #array3.join
-    #puts array3
-    binding.pry
-      #binding.pry
-      #return array2.join
+      end
+      return array2.join(" ")
+    elsif array_count == 2
+      array.each do|value|
+        array2 << ("#{value}")
+      end
+      return array2.join(" and ")
+    elsif array_count == 3
+      array.each do|value|
+        array2 << ("#{value}")
+      end
+      array2.insert(2, "and ")
+      array2.insert(1,", ")
+      array2.insert(3,", ")
+      array2.join
+   elsif
+      array.each do|value|
+       array2 << ("#{value}" ", ")
+      end
+      array2.insert(-2, "and ")
+      array3 = []
+      array3 = array2.pop
+      binding.pry
       return array2.join
-      #binding.pry
+        #binding.pry
   end
 end
