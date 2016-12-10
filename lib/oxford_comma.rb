4dd51aced3b062641d8723944386320ed3141536
charlie lee <ctlee9096@gmail.com>
2016-12-10 18:34:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,59 @@
+require 'pry'
 def oxford_comma(array)
-
+  array_count=array.count
+  array2=[]
+  if array_count == 1
+  array.each do|value|
+    array2 << ("#{value}")
+    #binding.pry
+    #puts array2.join(" ")
+    #array3 = array2.delete
+    #binding.pry
+            end
+  #if array1 == 2
+  #array.each do|value|
+          #
+    return array2.join(" ")
+    #binding.pry
+  end
+  if array_count == 2
+    array.each do|value|
+      array2 << ("#{value}")
+      #binding.pry
+      #puts array2.join(" ")
+      #array3 = array2.delete
+      #binding.pry
+              end
+    #if array1 == 2
+    #array.each do|value|
+            #
+      return array2.join(" and ")
+      binding.pry
+  end
+  if array_count == 3
+    array.each do|value|
+      array2 << ("#{value}")
+      #binding.pry
+      #puts array2.join(" ")
+      #array3 = array2.delete
+      #binding.pry
+              end
+    #if array1 == 2
+    #array.each do|value|
+            #
+      #return array2.join(" and ")
+      #binding.pry
+      array3 = []
+      array3 << "#{array[0]}"
+      array3.join(" , ")
+      binding.pry
+      #binding.pry
+      array4 = []
+      array4 << "#{array2[1]},#{array[2]}"
+      #binding.pry
+      array4.join(" and ")
+      binding.pry
+      return array4.unshift(array3)
+      binding.pry
+  end
 end
