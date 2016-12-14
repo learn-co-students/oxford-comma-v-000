a3a44afd711bb7f1490022cbec760a5d9238f81e
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2016-12-14 03:49:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  case array.size
+  when  1
+    array.join (" ")
+  when 2
+    array.join(" and ")
+  when 3
+    fruit = array.pop
+     array.join(", ") << ", and #{fruit}"
+  else
+    fruit = array.pop
+    array.join(", ") << ", and #{fruit}"
+  end
 end
