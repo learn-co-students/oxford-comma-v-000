625a30f948bb5c651ac99d04f5581f75b746a0ee
Daniel Goldberg <danpaulgo@aol.com>
2016-12-15 05:13:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array[0]
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else
+    output = ""
+    array.each_with_index do | item, index |
+      if index == array.size - 1
+        output += "and #{item}"
+      else
+        output += "#{item}, "
+      end
+    end
+    output
+  end
 end
