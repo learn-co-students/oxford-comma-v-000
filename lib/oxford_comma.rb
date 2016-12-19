1e5aeacb33dc97fe7fd75b699e944f2ae67ed4e8
ryanpmcmahon <ryan@mcmahon.work>
2016-12-18 19:25:54 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  len = array.length
+  case len
+    when 1
+      str = array.join
+    when 2
+      str = array.join(" and ")
+    else
+      str = ""
+      arr_copy = array.dup
+      while arr_copy.length > 1
+        str << arr_copy.shift + ", "
+      end
+      str << "and #{arr_copy.shift}"
+  end
+  str
 end
