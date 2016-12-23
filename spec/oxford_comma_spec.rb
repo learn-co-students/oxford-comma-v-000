67affb8c3f8dffcadc1c629cf7b53ff03e4c1413
Velin Mihaylov <velinfilevmihaylov@gmail.com>
2016-12-23 01:43:01 +0000

    Done.

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 describe "#oxford_comma" do
   it 'returns a string without any additional fomatting when given a 1-element array' do
     expect(oxford_comma(["kiwi"])).to eq("kiwi")
