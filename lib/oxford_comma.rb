e26c2a61b35ae2563b11d750ebea2ec3355deeb1
Jonathan Foster <redcabinny@gmail.com>
2017-01-04 02:37:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
-def oxford_comma(array)
+#def oxford_comma(array)
+#  statement = ""
+#  array.each do |item|
+#    statement += item
+#      if item == array[array.length - 2] && array.length > 2
+#        statement += ", and "
+#      elsif item == array[array.length - 2] && array.length == 2
+#        statement += " and "
+#      else
+#        statement += ", "
+#      end
+#  end
+#  statement.slice(0, statement.length - 2)
+#end
 
+def oxford_comma(array)
+statement = array.join(", ")
+  if array.length == 1
+    statement
+  elsif array.length == 2
+    statement.sub(", ", " and ")
+  else
+    array_last = array.pop
+    array.join(", ") + ", and #{array_last}"
+  end
 end
