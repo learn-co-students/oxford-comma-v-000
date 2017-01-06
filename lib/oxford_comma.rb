01d5513d6ca2664bfb0278715f959820ed99db0b
Scott Young <young.scottw@gmail.com>
2017-01-06 06:15:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1
-    string = array[0]
-  elsif array.length == 2
-    string = "#{array[0]} and #{array[1]}"
-  elsif array.length == 3
-    string = "#{array[0]}, #{array[1]}, and #{array[2]}"
-  else
-    string = ""
-    array.each { |word|
-      string = string + ", " + word
-    }
-    string.insert(string.rindex(", ")+1, " and")
-    string[0]=''
-    string[0]=''
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
-  return string
+  array.join(", ")
 end
