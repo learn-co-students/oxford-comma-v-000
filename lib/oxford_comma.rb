fc8688dd14a1011c12208dcc4be15433842175d1
Allison Chesky <afc340@nyu.edu>
2017-01-23 23:06:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0]
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    string = array[0..-2].join(", ") + ", and " + array[-1]
+    string
+  end
 end
