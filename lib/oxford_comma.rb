8a001a43c50b7118f3c12bc6a71485daa4cfb6fb
Isis Tejeda <isis@isistejeda.com>
2017-01-31 12:09:42 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
 
+if (array.length == 2)
+  return array.join(" and ")
+end
+if (array.length >= 3)
+  return array[0..-2].join(", ") + ", and " + array[-1]
+end
+
+array.join(", ")
 end
