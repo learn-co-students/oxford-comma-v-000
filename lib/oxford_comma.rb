d33af275b76d1d4e810beff0a6353db6a7262c66
Hui Jiang <hui1021@yahoo.com>
2017-02-04 03:40:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  last_word = array.pop
+  if array.length == 0
+    return last_word
+  else
+    second_last_word = array.pop
+    if array.length == 0
+      return "#{second_last_word} and #{last_word}"
+    else
+      string =array.join(", ")
+      return "#{string}, #{second_last_word}, and #{last_word}"
+    end
+  end
 end
