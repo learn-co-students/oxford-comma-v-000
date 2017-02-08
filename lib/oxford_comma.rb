22bf5a797aaa0edb4686dbf501304a6ae30de7e2
Trai Compton <tlcompton2546@outlook.com>
2017-02-08 22:55:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    first_items = array.join(", ")
+    first_items << ", and "
+    first_items << last_item
+  end
 end
