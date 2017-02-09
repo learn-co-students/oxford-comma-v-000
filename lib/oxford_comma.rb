d6100d07455919ec853ea567f6dec42a8941a916
krystlebarnes <krystlecbarnes@gmail.com>
2017-02-08 21:45:12 -0800

    changed last elsif to else in oxford_comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size > 2
+  else array.size > 2
     last_item = array.pop
     string = array.join(", ")
     string << ", and #{last_item}"
