728911a4096f8a698e7876c4ec5ec9970dee3e94
krystlebarnes <krystlecbarnes@gmail.com>
2017-02-08 21:40:02 -0800

    add oxford_comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    last_item = array.pop
+    string = array.join(", ")
+    string << ", and #{last_item}"
+  end
 end
