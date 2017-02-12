3912abe5b583a8bb096f26cebae81b0927408010
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-02-12 05:13:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  size = array.length + 1
+  if array.length == 1
+    return array[0]
+
+
+  elsif array.length == 2
+    two = array.join(" and ")
+    return two
+  else
+    last = ", and " + array.pop
+    comma = array.join(", ")
+    comma << last
+
+    return comma
+  end
 
 end
