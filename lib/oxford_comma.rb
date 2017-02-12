d7ead6713ba72fbc6915b74350575a6d14d47a42
Villy Siu <villysiu@gmail.com>
2017-02-12 05:04:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    array_last = array.pop
+    str = array.join(", ")
+    str += ", and #{array_last}"
+  elsif array.size == 2
+    str = array.join(" and ")
+  else
+    str = array.join
+  end
+  return str
 end
