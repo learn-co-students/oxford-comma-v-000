0db6691c58ba0c4b56ac7d60a6bf6884719b426a
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-02-21 23:07:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count < 2  # if only one element in array
+    array.join  # convert array to string
+  elsif array.count == 2  # if two elements in array join with "and"
+    array.join(" and ")  # include spaces before and after (i.e. " and ")
+  else array.count >= 3  # if three or greater elements in array
+    array[-1] = "and " + array[-1]  # convert last element(index[-1]) to include "and"
+    array.join(", ")  # join elements with comma(,) and convert to string
+  end
 end
