1e1773260021f7801e586a54e89e59c7f8b2b213
bortch89 <bortch@gmail.com>
2017-02-23 19:00:12 +0200

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    last = array.pop
+    array.join(", ") << ", and #{last}"
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
