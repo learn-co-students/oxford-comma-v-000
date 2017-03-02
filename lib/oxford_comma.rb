c342234c97ad5c9f9b25b54db34877add2d56e92
thesilentc <thesilentc@me.com>
2017-03-02 11:38:30 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,24 @@
 
 def oxford_comma(array)
   if array.count == 1
- 	  array.join(", ")
-   elsif array.count == 2
-     array.join(" and ")
-   elsif array.count == 3
-     array[0..1].join(", ") + array[2] = ", and #{array[2]}"
-   elsif array.count > 3
-     array[-1] = "and #{array[-1]}"
-     array.join(", ")
-  end
+ # 	  array.join(", ")
+  #  elsif array.count == 2
+  #    array.join(" and ")
+  #  elsif array.count == 3
+  #    array[0..1].join(", ") + array[2] = ", and #{array[2]}"
+  #  elsif array.count > 3
+  #    array[-1] = "and #{array[-1]}"
+  #    array.join(", ")
+  # end
+  return array
 end
+
+
+# def oxford_comma(array)
+#   if array.length == 2
+#     return "#{array[0]} and #{array[1]}"
+#   elsif 2 < array.length
+#     array[-1].insert(0, "and ")
+#   end
+#   array.join(", ")
+# end
