991fa2e802fdbf7ba4fa51e2b1efc88ab4f5adea
Rachel Walwood <walwoodr@gmail.com>
2017-03-08 20:16:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   else
-    array[array.size-1] = "and #{array[array.size-1]}"
+    array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
 end
