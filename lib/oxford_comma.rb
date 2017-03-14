42d47337d5493ff11c003df10f6fe392ee92462a
Laamia Islam <laamia.islam@gmail.com>
2017-03-14 17:37:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  last = array.size-1
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size > 2
+    string = array[0,last].join(", ")
+    return string << ", and #{array[last]}"
+  else
+    return "Nothing to join here!"
+  end
 end
