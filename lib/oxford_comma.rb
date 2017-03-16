72b2e9c0356c0bd7f76be34716a81c9e2857849c
antley chu <antley.chu@gmail.com>
2017-03-16 21:55:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
 
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last_fruit= array.pop
+    fruit_string = array.join (", ")
+    fruit_string + ", and #{last_fruit}"
+  end
 end
