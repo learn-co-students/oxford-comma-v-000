8cef4bcfcddafed11d232b58ac96e9a1547c8726
Liliana Alonzo <lillyneveu@gmail.com>
2017-03-16 06:09:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    [array[0...-1].join(", "), array.last].join(", and ")
+  end
 end
+
+#in Ruby on Rails the same can be accomplished with to_sentence
