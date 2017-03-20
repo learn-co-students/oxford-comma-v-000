bea9405bfbff7abc8615b1ebe672f90d6e00997c
Harleigh Abel <harleighabel@gmail.com>
2017-03-20 06:01:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    array.join
+  elsif array.length == 2
+    string = array.join (" and ")
+  else
+  last_word = array.pop
+  string = array.join (", ")
+  string << ", and " + last_word
+  end
 end
