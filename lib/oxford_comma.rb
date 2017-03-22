0a4396bd4d8daae3719ba7a9367fdbf641ae97c0
William Barela <william.barela.dev@gmail.com>
2017-03-22 16:23:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last_one  = "and #{array.pop}"
+    array << last_one
+    array.join(", ")
+  end
 end
