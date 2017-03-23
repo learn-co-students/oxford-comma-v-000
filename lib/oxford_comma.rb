c5afdb03c33b1b80a2e1ca8b12d25c1f50daa26f
Zenia Villa <zeniavilla@hotmail.com>
2017-03-22 17:01:54 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length == 1
+        array.join
+    else
+        size = array.length - 2
+        array[0..size].join(", ") + ", and " + array[array.length - 1]
+    end
 end
