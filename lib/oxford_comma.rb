1a839a330619a9e6bb4239fbbe2e65a7d4880316
Andrew Bonner <andrewbonner92@gmail.com>
2017-03-24 14:42:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,13 +11,13 @@ def oxford_comma(array)
 end
 
 # Another solution
-#def oxford_comma(array)
-#  if array.length == 2
-#    array.join(" and ")
-#  elsif array.length > 2
-# !!By declaring the array index outside of insert, .insert instead refers
-# to the the 0 index of the sting element in that last place of the array.
-#    array[-1].insert(0, "and ")
-#  end
-#  array.join(", ")
-#end
+def oxford_comma_2(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    # By declaring the array index outside of insert, .insert instead refers
+    # to the the 0 index of the sting element in that last place of the array.
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
+end
