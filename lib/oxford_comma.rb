3f87240e15027db4191dc78b6dcea81f3c1b50e4
Michael Mirzakandov <mikemirzak@aol.com>
2017-03-28 03:15:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size == 1
+    array[0]
+  else
+    line = ""
+  for i in 0..array.length-2
+    line << "#{array[i]}, "
+  end
+  line << "and #{array[array.length-1]}"
+ end
 end
