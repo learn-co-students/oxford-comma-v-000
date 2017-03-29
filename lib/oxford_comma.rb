fbf717b3a68d98d7dd791ffd5a0ccc39dfee780c
Katie Smith <catesmith0@gmail.com>
2017-03-29 00:45:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    array[0..-2].join(", ") + ", and #{array[-1]}"
+  else
+    array.join
+  end
 end
