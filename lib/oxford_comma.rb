9ca5939159576f5bb12938f032aaec5b73a04680
Danielle Croxton <danikrist@gmail.com>
2017-03-30 18:25:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    "#{array.first} and #{array.last}"
+  else array.count >= 3
+    last_element = array.pop
+    "#{array.join(", ")}, and #{last_element}"
+  end
 end
+
+
+# "'#{['12','34','35','231'].join("','")}'"
