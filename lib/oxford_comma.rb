e095678f351821c12b106d033e3a520e944e4d6c
Ryan Do <falcsmike7@gmail.com>
2017-04-02 16:55:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  x = array.size
+  case x
+  when 1
+    array[0]
+  when 2
+    array.join(" and ")
+  else
+    array[0...-1].join(", ") + ", and " + array.last
+  end
 end
