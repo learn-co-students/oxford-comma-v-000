f9dcc7ff1af184176bdac786a3c45f5fcb613321
David Mars <david.w.mars@my.occc.edu>
2017-04-02 02:16:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+#   ~~~HAVICK WAS HERE~~~
 
+def oxford_comma(array) #method for converting array to string and inserting commas
+  if array.length == 1
+    return array[0]
+  elsif array.length <=2
+    array.join(" and ")
+    elsif array.length > 2
+    array[-1] = "and #{array[-1]}"#Convert to string/Oxford Comma between elements
+    array.join(", ")
+else
+  return nil
+end
 end
