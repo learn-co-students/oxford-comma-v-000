08f132d9b74a9e35c41e5977296ceb3cd8e97708
Michael Ries <michael.ries@gmail.com>
2017-04-03 18:09:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array[0]
+  when 2
+    return array[0]+" and "+array[1] if array.size==2
+  else
+    last = array.pop
+    array.join(", ") + ", and #{last}"
+  end
 end
