bb25a98dfcbfdb13d4a0d9017366068ceeb49c62
Zain <zain@halai.us>
2017-04-06 18:21:51 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+	if array.length == 1
+		return array.join
+	elsif array.length == 2
+		return array.join(" and ")
+	end
 
+	array.map.with_index do |fruit, idx|
+		( idx != array.length - 1 ) ? "#{fruit}, " : "and #{fruit}"
+	end.join	
 end
