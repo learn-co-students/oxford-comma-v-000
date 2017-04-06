12748433d219c402d52f9b83cb11ac5397d9ea43
Zain <zain@halai.us>
2017-04-06 18:36:33 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-	if array.length == 1
-		return array.join
-	elsif array.length == 2
+	if array.length == 2
 		return array.join(" and ")
+	elsif array.length > 2
+		array.last.insert(0, "and ")
 	end
 
-	array.map.with_index do |fruit, idx|
-		( idx != array.length - 1 ) ? "#{fruit}, " : "and #{fruit}"
-	end.join	
+	array.join(", ")
 end
