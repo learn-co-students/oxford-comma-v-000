4475e64b43a0da543ba6feb53cf35a7cd2e4da0a
Martyn Conkling <haoleboy3000@yahoo.com>
2017-04-07 10:14:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+      string = array.join
+      return string
+    elsif array.length == 2
+      string = array.join(" and ")
+      return string
+    elsif array.length >= 3
+      last_element = array.pop
+      string = array.join(", ")
+      string << ", and "
+      string << last_element
+      return string
+    end
+  end
