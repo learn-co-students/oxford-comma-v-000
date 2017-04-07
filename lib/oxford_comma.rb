36d3bb5839813a19de552ad0479f70bf1be5af95
jose cobos <luiscobosj@gmail.com>
2017-04-07 23:58:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+    
+  elsif array.count == 2
+  array.join(" and ")
+  
+elsif array.count == 3
+  
+  array[array.count - 1] = "and #{array.last}"
+  array.join(", ")
+  
+else
+  array[array.count - 1] = "and #{array.last}"
+  array.join(", ")
+  
+  end
 end
+
