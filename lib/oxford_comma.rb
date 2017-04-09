21982204e8a538b8c9dae5b031da0e95573f032c
Julia Thompson <thompson.julia@gmail.com>
2017-04-09 17:33:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array.slice(0,array.size - 1).join(", ") << ", and #{array.last}"
+  end
 end
