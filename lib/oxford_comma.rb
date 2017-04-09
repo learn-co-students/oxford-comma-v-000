67c559b983883c8d7329d5385fdbb6fca080ad24
John Blank <veloblank@gmail.com>
2017-04-09 00:30:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(array, *p)
+  if array.length > 2
+    new_array = array.pop
+    original_string = array.join( ", ")
+    original_string << ", and "
+    original_string << new_array
+  else
+    array.join
+    array.join( " and ")
+  end
 end
