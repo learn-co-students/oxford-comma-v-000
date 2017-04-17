e713c72474bb3438b20d2b1fb52550c61de32b2c
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-17 21:14:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
 def oxford_comma(array)
-  for array.length == 1
+  if array.length == 1
     array.join(" , ")
 
   elsif array.length == 2
-    array.join(" , and ")
+    array.join("  and ")
 
   elsif array.length == 3
-    array.join
 
-    else
 
   end
 end
