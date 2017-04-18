32a41b8c2a7feeb34965788ce50390e7ccd6816c
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-18 00:55:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,17 +2,13 @@ def oxford_comma(array)
   if array.length == 1
     array.join(" , ")
 
-  elsif array.length == 2
-    array.join(" and ")
+    elsif array.length == 2
+      array.join(" and ")
 
-  elsif array.length >= 3
-    array.each_with_index do |array, index|
-      if index.odd?
-          array.insert(array.length - 1, ", ")
-      else
-
-      end
-    # array = array.join(", ")
-    new_string.insert(new_string.length - 1, "and ")
+    elsif array.length == 3
+        array.insert(array.length - 1, ", and ")
+        array.insert(array.length - 3, ", ")
+        array.join()
   end
+
 end
