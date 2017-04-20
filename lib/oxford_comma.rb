ef58f9de7bc1f5c0ad0c8c0ccef42e769dbd324a
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-04-20 20:39:13 +0000

    Solution

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    return array.join(" and ")
+  elsif array.size > 1
+    array_1 = []
+    array_1 << array[-1]
+    array.pop
+    array = array.join(", ")
+    array << ", and "
+    array << array_1.join
+  else
+    return array.join
+  end
 end
