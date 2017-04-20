79ab4bbf5304f94ffc1ad4887f173c8ad7ce2a75
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-04-20 20:45:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,7 @@ def oxford_comma(array)
   if array.size == 2
     return array.join(" and ")
   elsif array.size > 1
-    array_1 = []
-    array_1 << array[-1]
-    array.pop
-    array = array.join(", ")
-    array << ", and "
-    array << array_1.join
+    array[-1].insert(0, "and ")
   else
     return array.join
   end
