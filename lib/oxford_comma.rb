6147beabc1e5eec08f07957952bd490ec17a77b5
Mollie Stein <mollie.stein@gmail.com>
2017-04-30 14:41:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length > 1
+    array.last.prepend("and ")
+  end
+  if array.length > 2
+    string = array.join(", ")
+  else
+    string = array.join(" ")
+  end
+  string
 end
