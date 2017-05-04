0412d2edcf13cefc3b4188b29d1e444b9242e48b
curlywallst <nnoyes@comcast.net>
2017-05-04 11:13:21 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  new_string=String.new
+  array.each do |item|
+    if array.count == 2
+      item != array.last ? new_string = new_string + "#{item} and " : new_string = new_string + "#{item}"
+    elsif array.count == 1
+      new_string= "#{item}"
+    else item != array.last ? new_string = new_string + "#{item}, " : new_string = new_string + "and #{item}"
+    end
+  end
+  new_string
 end
