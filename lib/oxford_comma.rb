ea4acd892a041a3a0252d7a13932e9bfe31e7cc7
Casey Tyack <kazenorite@gmail.com>
2017-05-04 20:30:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  if (array.length == 1)
+    return array[0]
+  elsif (array.length == 2)
+    return "#{array[0]} and #{array[1]}"
+  else
+    i = 0
+    while (i < (array.length - 1))
+      array[i] << ", "
+      i += 1
+    end
+    j = 0
+    array[-2] << "and "
+    aString = ""
+    while (j < array.length)
+      aString << array[j]
+      j += 1
+    end
+    return aString
+  end
 end
