a176503e54b007c67eeaa589cbe5b4ab88cc48f8
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-05-08 19:36:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  return array[0] if array.size == 1
+  return array.join(" and ") if array.size == 2
+  oxford = ""
+  word_count = 0
+  array.each do |word|
+    word_count += 1
+    if(word_count == array.size)
+      oxford += "and #{word}"
+    else
+      oxford += "#{word}, "
+    end
+  end
+  return oxford
 end
