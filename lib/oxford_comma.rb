d23c172abcfcf185d655fcc513a51f0fec2710df
kvn a <koh.armada@gmail.com>
2017-05-08 21:53:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size >= 3
+    last_index = "and " + array.pop
+    array = array << last_index
+    return array.join(", ")
+  end
 end
