8c32c2b372b47fbdd7b9a453b11936360be944bb
Sullivan Nolan <nolakat@gmail.com>
2017-05-09 03:27:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+   array_length = array.length
+
+ if array_length < 2 
+  return array[0]
+elsif array_length == 2
+  return array.join(" and ")
+else
+  new_string= Array.new
+  new_string = array[0..(array.length - 2)].join(", ")
+  new_string << ", and " + array[-1].to_s
+  
+  return new_string
+  
+end
+  
 
 end
