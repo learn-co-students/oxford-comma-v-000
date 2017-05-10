f4f8b550282a43aefb52606aabe78b1cec3f2a24
Jenni Macklin <jennimacklin@hotmail.com>
2017-05-10 21:57:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if (array.length == 1)
+    return "#{array.first}"
+  elsif (array.length == 2)
+    return "#{array.first} and #{array.last}"
+  else
+    last_item = array.pop
+    return "#{array.join(", ")}, and #{last_item}"
+  end
 end
