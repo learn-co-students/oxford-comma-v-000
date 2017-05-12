96d838747765b2590f9113844ec84e448511da9a
Derek  Pelt <derekpelt@gmail.com>
2017-05-12 00:10:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.count == 1 
+    array.join("-")
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count == 3
+    new = [] 
+    new = array.insert(array.length-1, "and " + array[-1])
+    new.pop 
+    new.join(", ")
+  else 
+    new = [] 
+    new = array.insert(array.length-1, "and " + array[-1])
+    new.pop 
+    new.join(", ")
+    
+    
+  end 
 end
