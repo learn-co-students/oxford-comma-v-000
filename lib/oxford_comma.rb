b8392c8eea48337a5a1d5586670a24b266d2b4df
Sam Marshall <samuel.r.marshall@gmail.com>
2017-05-12 04:51:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.first
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    array[array.size-2]=[array[array.size-2],array[array.size-1]].join(", and ")
+    array.delete_at(array.size-1)
+    return array.join(", ")
+  end
 end
