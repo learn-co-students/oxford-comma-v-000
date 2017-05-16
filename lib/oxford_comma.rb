aafff009267088ed97d7b7ce3e18c1c937c7b4af
Noah Summers <summers.noah@gmail.com>
2017-05-15 22:21:17 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.size
+    when 0
+      return nil
+    when 1
+      return "#{array.first}"
+    when 2
+      return "#{array.first} and #{array.last}"
+    else
+      return "#{array[0..-2].join(", ")}, and #{array.last}"
+    end
+    
 end
