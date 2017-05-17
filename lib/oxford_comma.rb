0e5013d26084acac57893096d60bd51fab509abc
gb23 <gregbenj@hotmail.com>
2017-05-17 14:53:07 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array.first
+    elsif array.size == 2
+        array.join(" and ")
+    else     #["a", "b", "c"]
+        last = array.pop #["a", "b"]
+        string_list = array.join(", ") #"a, b"
+        string_list << ", and " << last
+    end
 end
