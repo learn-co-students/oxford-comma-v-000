b65c81bbae0ad70d8d4415300ac73958f1de160e
Jelo Yambao <jelolearns@gmail.com>
2017-05-20 23:22:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,8 @@
 def oxford_comma(array)
-  string = ""
-  array.each_with_index do |fruit, index|
-    if array.length == 2
-      string = array.join(" and ")
-    elsif index == 0
-      string << "#{fruit}"
-    elsif index == array.length - 1 && array.length > 2
-      string << ", and #{fruit}"
-    else
-      string << + ", #{fruit}"
-    end
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
   end
-  string
+  array.join(", ")
 end
