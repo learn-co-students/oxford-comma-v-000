d2d7f14e35a494d4fc86b8c8b7a4dd034b24588d
Emily Howard <howard.emilya@gmail.com>
2017-05-23 21:18:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    array[-1].prepend("and ")
+    array.join(", ")
+  end
 end
