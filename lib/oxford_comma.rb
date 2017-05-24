b11d8102e7a280fb5c41d8159cbcde20eced57f5
Mason Ellwood <masonellwood@gmail.com>
2017-05-24 17:38:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array.join
+  elsif array.count == 2
+    return array.join(" and ")
+  else
+    # last_value = array[-1]
+    return array[0..-2].join(", ") + ', and ' + array[-1]
+  end
 end
