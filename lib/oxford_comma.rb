7f4433025b475d9f2daff58ebdddf6a31bdf48f2
kyleblee <hello.kylelee@gmail.com>
2017-05-25 14:42:44 -0400

    lab complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_word = array.pop
+    partial_list = array.join(", ")
+    partial_list << ", and #{last_word}"
+    partial_list
+  end
 end
