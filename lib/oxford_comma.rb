c5ac8f2896bb03a185bdbfdc8e7ff19704ef87a2
Nicholas D'Amico <nickalan82@icloud.com>
2017-05-25 18:47:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+  last_item = array.pop
+  return "#{array.join(", ")}, and #{last_item}"
+  end
 end
