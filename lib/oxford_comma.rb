4c578a22ccb13dfcf9d9f1bde035b74e24a3bbb4
Luisa Scavo <luisascavo@gmail.com>
2017-05-26 20:35:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0].to_s
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    array.last.replace("and #{array.last}")
+    array.join(", ")
+  end
 end
