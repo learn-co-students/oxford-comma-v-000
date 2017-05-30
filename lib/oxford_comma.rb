d63753fd1a3eb9a16b29cd8fbb101735691767db
Naomi Dennis <njdennis94@gmail.com>
2017-05-30 23:09:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    array[array.size - 1] = "and " + array[array.size-1]
+    array.join(", ")
+  end
 end
