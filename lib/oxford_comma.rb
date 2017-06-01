2f9977e435f3ad986bd8178b4e0059f43c96c05f
Tyler Gotto <tylergotto17@gmail.com>
2017-06-01 09:54:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        return array.join
+    elsif array.size == 2
+        return array.join(" and ")
+    else
+        final_item = array.pop
+        new_array = array.join(", ")
+        new_array += ", and #{final_item}"
+    end
 end
