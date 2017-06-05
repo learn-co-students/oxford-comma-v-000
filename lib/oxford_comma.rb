f7c5f5ca795a0edc0f9e83517276e831495a7d34
Brian Bytnar <bytnarb2@gmail.com>
2017-06-05 21:26:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    list = array.join(" and ")
+  else
+    last_item = ", and " + array.pop
+    list = array.join(", ")
+    entire_list = list << last_item
+  end
 end
