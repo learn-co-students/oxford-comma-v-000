311ff64e63add5650b8183a8d69b72b1ec17c0f0
Denise Dearman <denisedearman@Patricks-MacBook-Pro.local>
2017-06-07 14:52:46 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  else
+    array.slice(0, array.length-1).join(', ') + (array.length === 2? '' : ',') + ' and ' + array[array.length-1]
+  end
 end
