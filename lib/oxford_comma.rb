18ac83054e190a4cdd49a11f3c7c914486a5a660
jamiegiuliano <jamiepgiuliano@gmail.com>
2017-06-08 22:54:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+    if array.size < 3
+        array.join(" and ")
+    else
+        removed_element = array.pop
+       return "#{array.join(", " )}, and #{removed_element}"
+    end
 end
