54fed583e16d190d45cea710c7d94188bce0a429
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2017-06-13 20:36:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  # last = array.pop
+  if array.length == 1
+    array.join (", ")
+  elsif array.length == 2
+    array.join (" and ")
+  else
+    [array[0..-2].join(", "), array.last].join (", and ")
+    # new_array = ", and " + array.last
+    # array + comma_array + new_array
+ end
+  # new_array
 
+  # array.to_
 end
+
+
+#need to use the .split method to have the comma inserted,
+#then another method to get the " and " before the last item
