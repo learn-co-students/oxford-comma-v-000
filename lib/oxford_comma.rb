aca1e10b4e8ff20049b10a2ebdc8079ddcdc8ae6
Sydney Chun <schun08@gmail.com>
2017-06-13 19:45:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    str = ""
+    array.each_with_index do |item, index|
+      if index == array.length-1
+        str += "and #{item}"
+      else
+        str += "#{item}, "
+      end
+    end
+    return str
+  end
 end
+
+#is there a cleaner way to do this?
