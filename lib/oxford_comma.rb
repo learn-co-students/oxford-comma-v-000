f0731357259e2754c5a600a0dd2f10c3dd88f0cc
David Holton <it.arcanum@gmail.com>
2017-06-13 09:44:46 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+	if array.size == 1
+		return array.join
+	elsif array.size == 2
+		return array.join(" and ")
+	else
+		lastItem = array.pop
+		listItems = array.join(", ")  # to me, this is easier to read.
+		#listItems = array * ", "  # does the same as above.
+		return listItems + ", and " + lastItem
+	end
 end
