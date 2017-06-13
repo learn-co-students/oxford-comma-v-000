cb98ff07a1447668944e75975cc6340918b76370
Shangni Hu <shangnihu@gmail.com>
2017-06-13 04:00:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.count > 1
+    joined = array.join(",")
+    items = joined.split(",")
+    count = items.count
+    last = items.pop
+    string_items = items.join(", ")
+    if count > 2
+      string_items = string_items + ", and #{last}"
+      return string_items
+    else
+      string_items = string_items + " and #{last}"
+      return string_items
+    end
+  else
+    return array[0]
+  end
 end
