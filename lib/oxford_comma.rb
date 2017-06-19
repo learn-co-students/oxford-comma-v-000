a83adf4bc7de5e0241fe078f6dc0d0cb508b3671
Robin Doirin <robindoirin@gmail.com>
2017-06-19 17:33:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ case
+ when array.size == 1
+   array[0]
+ when array.size == 2
+   array[0..1].join(" and ")
+ when array.size >= 3
+   array[-1] = "and #{array[-1]}"
+   array.join(", ")
+ end
 end
