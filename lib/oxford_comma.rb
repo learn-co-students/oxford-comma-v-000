d27c0bb23a54b429cf6da638ee7e022c8b272b7b
Erica Plofsky <ericaplofsky@gmail.com>
2017-06-23 00:10:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  if array.length > 2
+    string = array.join(", ")
+    string = string.gsub(array.last, "and")
+    string << " #{array.last}"
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.join
+  end
+end 
 
-end
+
+#def oxford_comma(array)
+#if array.length > 2
+#  array[-1].insert(0, "and ")
+#elsif array.length == 2
+#     array.join(" and ")
+#end
+#array.join(", ")
+#end
