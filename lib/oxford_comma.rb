77dc3a3b969958223423dba9dab69dfa60725ff4
Sarah Schaller <sarcsch@aol.com>
2017-06-27 20:46:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size === 1
+    return array.join
+  elsif array.size === 2
+    return array.join(" and ")
+  elsif array.size >= 3
+    last_element = array[-1]
+    array.pop
+    array << "and #{last_element}"
+    return array.join(", ")
+  end
 end
