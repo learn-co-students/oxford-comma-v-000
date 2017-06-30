625f4fd574b06a33004da9c2bfa49a58c7eac498
Sam Crowder <samecrowder@gmail.com>
2017-06-30 15:13:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  oxford_sentence = ""
 
+  if array.length == 2
+    oxford_sentence = "#{array[0]} and #{array[1]}"
+  else
+  count = 1
+    array.each do |item|
+      oxford_sentence << item
+      if count != array.length
+        oxford_sentence << ", "
+      end
+      if count == array.length - 1
+        oxford_sentence << "and "
+      end
+      count += 1
+    end
+  end
+  oxford_sentence
 end
