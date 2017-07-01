56980ee087c67ceec8349d0e526064bb36f6103f
nathalie garcia <nathalie.scruz@gmail.com>
2017-07-01 19:16:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+     array.join
+  elsif array.length == 2
+     array.join(" and ")
+   else
+     last_value = ", and " + array.pop
+    array.join(", ") << last_value
+  end
 end
