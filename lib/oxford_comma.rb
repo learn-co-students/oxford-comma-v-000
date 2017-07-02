7cdbf165d331c534738da9f9688d744729c8ee9a
Mars Howard <marsbhoward@gmail.com>
2017-07-02 21:29:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size ==2
+    array.join(" and ")
+  else
+    counter = 1
+    array.each do |noun|
+      if counter == array.size-1
+        noun << (", and ")
+      elsif counter < array.size-1
+        noun << (", " )
+      end
+      counter = counter + 1
+    end
+    array.join
+  end
 end
