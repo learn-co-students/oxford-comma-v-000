a4bc6828a3f848b98643b042b62827234efc7701
Sydney Chun <schun08@gmail.com>
2017-07-02 23:19:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,32 @@
+# first attempt (6/13/17)
+# def oxford_comma(array)
+#   if array.length == 1
+#     return array.join
+#   elsif array.length == 2
+#     return array.join(" and ")
+#   else
+#     str = ""
+#     array.each_with_index do |item, index|
+#       if index == array.length-1
+#         str += "and #{item}"
+#       else
+#         str += "#{item}, "
+#       end
+#     end
+#     return str
+#   end
+# end
+#
+# #is there a cleaner way to do this?
+# second attempt (7/2/17)
 def oxford_comma(array)
   if array.length == 1
-    return array.join
+    array.join('').strip
   elsif array.length == 2
-    return array.join(" and ")
+    array.join(' and ').strip
   else
-    str = ""
-    array.each_with_index do |item, index|
-      if index == array.length-1
-        str += "and #{item}"
-      else
-        str += "#{item}, "
-      end
-    end
-    return str
+    last = array.pop
+    array.push("and #{last}")
+    array.join(', ')
   end
 end
-
-#is there a cleaner way to do this?
