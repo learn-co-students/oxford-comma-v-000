f227798e94be5017f7c27b180eaa94a13e70e638
Dennis Nastasi <dcnastasi@gmail.com>
2017-07-04 19:56:57 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    result = "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+  end
 end
+
+array = %w(peanut butter jelly)
+
+# binding.pry
