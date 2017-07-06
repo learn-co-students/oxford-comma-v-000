722804695156298d29abb1c29c1f7892fbe08fd4
Aellon Krider <aellonkrider@gmail.com>
2017-07-06 23:50:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  when 3
+    array[0..1].join(", ") + ", and " + array[-1]
+  else
+    array[0..array.count-2].join(", ") + ", and " + array[-1]
+  end
 end
