7490b72f36f5bca343a0ad1dacfdcadd215c3658
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-07-07 15:44:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if (array.size == 1)
+    array[0]
+  elsif (array.size == 2)
+    array.join(" and ")
+  elsif (array.size == 3)
+    array = "#{array[0]}, #{array[1]}, and #{array[2]}"
+    array
+  elsif (array.size > 3)
+    array[array.length-1] = "and " + "#{array[array.length-1]}"
+    array.join(", ")
+  end
 end
