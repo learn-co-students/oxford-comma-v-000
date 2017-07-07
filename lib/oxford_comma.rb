bdfcf0b7b3bf34830e73b2f004ce5efea64d6737
Elizabeth Kuok <elizabethkuok@gmail.com>
2017-07-07 19:06:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size ==2
+    array.join (" and ")
+  else
+    message = array.pop
+    string = array.join(", ")
+    string << ", and #{message}"
+  end
 end
