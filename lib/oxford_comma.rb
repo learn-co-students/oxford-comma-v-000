9d794012ec11847982f1c483e7000c8480ae78bc
Femi F <ofpupp@princeton.edu>
2017-07-09 18:01:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else array.size >= 3
+     prototype = array
+     word = prototype.pop
+     prototype2 = prototype.join(", ")
+     prototype2 = prototype2 + ", and #{word}"
+     return prototype2
+  end
 end
