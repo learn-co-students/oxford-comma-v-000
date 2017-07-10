e6fd7fbf007a89f6652bb5812995b3fdf8172c4b
Aron Roitblat <roitblatari@gmail.com>
2017-07-10 18:26:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,30 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    # array ["john", "bob"]
+    # "john and bob"
+    # "bob".join(" and ")
+    # array.join(" and ")
+    # "john, mary, and bob"
+    array.join(" and ")
+  elsif array.size > 2
+    # so the last element should be "and element"
+    array[array.size - 1] = "and #{array[array.size - 1]}"
+    # the array will need ', ' to divide all elements
+    array.join(", ")
+  else
+    array[0]
+  end
 end
+#
+# def oxford_comma(array)
+#     sentence = array.join(", ")
+#     s_a = sentence.split("")
+#     if(s_a.count{|x| x == ","} == 1
+# end
+
+
+
+
+# def oxford_comma(array)
+#     [array[0...-1].join(", "), array.last].join(", and ")
+# end
