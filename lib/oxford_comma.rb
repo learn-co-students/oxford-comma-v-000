a5f9f3f1a8e5cf7bc2f0f82d2f67c57481107c27
Steven Lennis <steven.lennis@icloud.com>
2017-07-11 21:57:28 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
 def oxford_comma(array)
+  word_count = array.count
+  oxford = ""
+  last_item = ""
+  comma = ", "
+  seperator = " and "
+  final_seperator = ", and "
+  case word_count
 
+  when 1
+    array.join
+
+  when 2
+    array.join(seperator)
+
+  when 3
+    last_item << array.pop
+    oxford << array.join(comma)
+    oxford << final_seperator
+    oxford << last_item
+
+  else
+    last_item << array.pop
+    oxford << array.join(comma)
+    oxford << final_seperator
+    oxford << last_item
+  end
 end
