80ade766928e1f66921a4ecc6bad1eb169a2954e
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-07-14 19:44:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  length = array.length
 
+  if length == 1
+    array.join
+
+  elsif length == 2
+    array.join(" and ")
+
+  else
+    oxford_string = array.join(", ")
+    oxford_array = oxford_string.split(", ")
+    oxford_array[-1].prepend("and ")
+    oxford_array.join(", ")
+  end
 end
