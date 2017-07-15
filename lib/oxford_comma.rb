a91d82983a9e6318d2bd887dac50cdea7c79b852
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-07-15 13:24:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  c=0
+  strin=""
+  flag=false
+while (c<array.length)
+  if (array.length==1)
+    return strin+="#{array.shift()}"
+  elsif(array.length==2)
+     if (flag==true)
+       strin+="#{array.shift()}, and "
+     else
+        strin+="#{array.shift()} and "
+      end
+  else
+    flag=true
+    strin+="#{array.shift()}, "
+  end
+end
 end
