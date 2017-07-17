cfdccbd2279cf5bf16e090c8030b846ec094fc0f
Joanna Sese <jmgsese@gmail.com>
2017-07-17 19:45:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    "#{array.join(", ")}, and #{last_item}"
+  end
 end
