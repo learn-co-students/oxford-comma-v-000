dcca7d78e8fdfcf6d1c231dfa47ccf4abbc5bd78
Byungwook Yoon <byung90@gmail.com>
2017-07-21 17:48:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
-
+  value = ""
+  case array.size
+  when 1
+    value << "#{array[0]}"
+  when 2
+    array.each_with_index do |item, index|
+      if index == array.size-1
+        value << "and #{item}"
+      else
+        value << "#{item} "
+      end
+    end
+  else
+    array.each_with_index do |item, index|
+      if index == array.size-1
+        value << "and #{item}"
+      else
+        value << "#{item}, "
+      end
+    end
+  end
+  value
 end
