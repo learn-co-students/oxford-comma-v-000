9432e25f3ef3a4fdd9927f20f261f42f9c76d820
lauraszyms <lauraszyms@gmail.com>
2017-07-22 14:46:26 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.size < 2
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    last_item = array.pop
+    return "#{array.join(", ")}, and #{last_item}"
 
+  end
 end
