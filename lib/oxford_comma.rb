52f20f9f3efbd0c9fe4575b8407b7ab0923dd895
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-07-23 06:58:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-  if array.length == 1
-    return array.join
-  elsif array.length == 2
+  if array.length == 2
     return array.join(" and ")
-  elsif array.length >= 3
-    last_elem = array.pop()
-    array << "and #{last_elem}"
-    return array.join(", ")
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
   end
+
+  return array.join(", ")
 end
