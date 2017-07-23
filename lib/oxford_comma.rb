7f894e83b4226baa80cf023ce17a9f6c100411a3
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-07-23 06:46:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    last_elem = array.pop()
+    array << "and #{last_elem}"
+    return array.join(", ")
+  end
 end
