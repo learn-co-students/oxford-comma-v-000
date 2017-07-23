6a9386d36a879fc59a2366f9b1fb1c5e2dad0429
Mariel F <marielbeth@gmail.com>
2017-07-23 17:41:12 +0000

    created oxford comma list formatter

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  end
+  oxford_array = (array[0...-1]).to_a
+  oxford_array << "and #{array[-1]}"
+  oxford_array.join(", ")
 end
