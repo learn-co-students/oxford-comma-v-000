069a1a44690650842e5b8086bd33722d608c7785
Vicky Lau <vlau.nyc@gmail.com>
2017-07-28 19:31:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_array = ", and #{array.pop}"
+    array.join(", ") + last_array
+  end
 end
+
+#  array[0, array.length-1].join(", ") << ", and #{array.last}"
+
+#  last_array = array.pop
+#  "#{array.join(", ")}, and #{last_array}"
+# end
