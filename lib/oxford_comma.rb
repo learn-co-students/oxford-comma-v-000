d21a08bb451ef99653e4231a54cda3d460514ef5
Veronica Miller <vmcmiller@gmail.com>
2017-07-29 21:38:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    "#{array[0]}"
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    list = array.join(", ")
+    list << ", and #{last_item}"
+  end
 end
