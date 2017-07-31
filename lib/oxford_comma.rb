a19ee336f33ce34724379270e0a20523e9ac631b
Valerie Corvington Farrell <vcorvington@gmail.com>
2017-07-31 03:11:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,25 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-
   elsif array.length > 2
-  #  last_elem = array.length - 1
-  #  array[last_elem] = "and " + array[last_elem]
-     last_elem = array.length - 1
-     last_elem = "and " + last_elem
-     new_str = array.join(", ")
-
+    last_elem = array.length - 1
+    array[last_elem] = "and " + array[last_elem]
+    new_str = array.join(", ")
   end
 end
-
-
-# elsif array.length > 2
-# array.pop(2) = last_2_elem
-# last_2_elem.join(", and ") = last_2_items
-#
-# array.join(" , ") = new_list
-#
-# puts "#{new_list} + #{last_2_items}"
-#
-#  end
-#  end
