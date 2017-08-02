092eeca8b42ae2903bbcbd00df7f57dafb23a6e6
Sophia H <shhubeny@gmail.com>
2017-08-02 02:38:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 2
+    array.join(" and ")
+  elsif array.count >= 3
+    "#{array[0..-2].join(', ')}, and #{array.last}"
+  else
+    array.join
+  end
 end
