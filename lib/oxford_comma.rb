c06ce1e8816dec6733c7c15b033bc600025651af
Juliana baggio <baggiojuliana@gmail.com>
2017-08-03 20:00:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,9 @@
 
 def oxford_comma(array)
-  if array.size == 1
-    array.join
-  elsif array.size == 2
-    array.join(" and ")
-  else
-    last_element = array.pop
-    add_end = "and #{last_element}"
-    array.push(add_end)
-    array.join(", ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
