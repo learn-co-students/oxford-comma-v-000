c7a83a3f4e513bb479f3372160f4507c751a2000
Aaron Samuel <aasamuelj@gmail.com>
2017-08-03 05:55:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    first_array = []
+    first_array.push(array[0...-1].join(", "))
+    first_array.push(array[-1])
+    first_array.join(", and ")
+  elsif array.size > 3
+    first_array = []
+    first_array.push(array[0...-1].join(", "))
+    first_array.push(array[-1])
+    first_array.join(", and ")
+  end
 end
