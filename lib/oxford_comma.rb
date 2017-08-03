6c62564c5e7d65683907a7a1b5eeb682fb78dbfa
Juliana baggio <baggiojuliana@gmail.com>
2017-08-03 19:59:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    last_element = array.pop
+    add_end = "and #{last_element}"
+    array.push(add_end)
+    array.join(", ")
+  end
 end
