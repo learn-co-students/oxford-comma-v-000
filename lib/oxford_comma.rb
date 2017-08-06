37f8dcb8c5ced707bf4f5741163d24c8b748703c
Guesly  Germain <germain1211@outlook.com>
2017-08-06 00:09:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length <= 2
      array.join(" and ")
-   elsif array.length >= 3
+   else array.length >= 3
      array[array.length - 1] = "and #{array[array.length - 1]}"
      array.join(", ")
   end
