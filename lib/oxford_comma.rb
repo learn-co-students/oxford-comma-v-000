9e3686bbbe338054e4db76bfdd874eeafa49d4d6
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2017-08-08 02:36:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    lastElem = array.last.length
+    str = array.join(", ")
+    str.insert((str.length - lastElem), "and ")
+  end
 end
