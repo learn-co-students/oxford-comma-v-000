daf2b32e890fb68430094be3f5d51aec4f8d2e7e
Mary Benitez <meebenitez@gmail.com>
2017-08-09 07:02:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,16 @@
-def oxford_comma(array)
-  if array.length == 1
-    string = array.join
-  elsif array.length == 2
-    string = array.join(" and ")
+def oxford_comma(names_array)
+  if names_array.length == 1
+    string = names_array.join
+  elsif names_array.length == 2
+    string = names_array.join(" and ")
   else
-    no_comma = array.length - 1
-    new_array = []
-    array.each do |name|
-      new_array << name + ","
-    end
-    new_array[no_comma] = array[no_comma]
-    array[no_comma]
-    new_array.insert(no_comma, "and")
-    string = new_array.join(" ")
+     i = 0
+     while i < names_array.length - 1
+       names_array[i] = names_array[i] + ","
+       i += 1
+     end
+    names_array.insert(names_array.length - 1, "and")
+    string = names_array.join(" ")
   end
-  return string
+  string
 end
