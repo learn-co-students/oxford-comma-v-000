ea0db3a2e3c60ed3441c147311850d73a74a4b8c
Alexandra <superbiscuit@gmail.com>
2017-08-13 15:50:19 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+	res = ''
+		if array.size == 1
+			return array[0]
+		end
+		if array.size == 2
+			return "#{array[0]} and #{array[1]}"
+		end  
+	array.each do |word|
+		if word == array.last
+			res << "and #{word}"
+		else
+			res << "#{word}, "
+		end 
+	end
+	res  
 end
