23d5b92c90ec17ade974bbcf433832102c2dfcb3
Mary Obray <maryobray@gmail.com>
2017-08-14 05:57:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  comma_string = ""
+  index = 0
+  array.each do |word|
+    if index == 0
+      comma_string << word
+    elsif index == array.length-1
+      if array.length == 2
+        comma_string << (" and " + word)
+      else
+        comma_string << (", and " + word)
+      end
+    else
+      comma_string << (", " + word)
+    end
+    index += 1
+  end
+  comma_string
 end
