92a3714d4d55847a273892e940171e5f98fac16c
kylek <kbelway29@gmail.com>
2017-08-14 14:12:54 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join()
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    last = array[-1]
+    array.pop()
+    return array.join(", ") + ", and " + last
+  end
 end
