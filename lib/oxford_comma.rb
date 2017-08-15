0760787d18b2b8a41eb72342594da8a4a248f071
Annette Maza <nettymaza@gmail.com>
2017-08-15 22:49:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
-require 'rails'
+# require 'rails'
 
 def oxford_comma(array)
-  array.to_sentence
-  # if array.length == 2
-  #   return "#{array[0]} and #{array[1]}"
-  # elsif array.length > 2
-  #   array[-1].insert(0, "and ")
-  # else
-  #   [array].join(", ")
-  # end
+  # array.to_sentence
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ") #I DON'T UNDERSTAND THIS
+  end
+  array.join(", ")
 end
