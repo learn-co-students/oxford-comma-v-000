e57401dd48f51f350361b21aa74c1480f8e02823
Sergio Correa <correa.sergio@gmail.com>
2017-08-16 19:14:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  if array.count > 2
+    last_element = array.pop
+    array << "and "
+    string = array.join(", ")
+    string << "#{last_element}"
+  else
+    array.join(" and ")
+  end
+end
 
+# ALTERNATIVE METHOD #
+def oxford_comma(array)
+  if array.count > 2
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  else
+  array.join(" and ")
+  end
 end
