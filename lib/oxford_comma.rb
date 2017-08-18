cabb9a40a95f77106db1f9fbfb3841fc75ba2f40
Stephen Decker <sdeckerm@gmail.com>
2017-08-18 19:01:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
-
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
+  if array.size == 1
+      array.join
+  elsif array.size == 2
+      array.join(" and ")
+  elsif array.size == 3
+      array.insert(-2, ", and ").insert(1, ", ").join
+  elsif array.size > 3
+      last = array.pop
+      array.join(", ") + ", and " + last.to_s
   end
-  array.join(", ")    
 end
