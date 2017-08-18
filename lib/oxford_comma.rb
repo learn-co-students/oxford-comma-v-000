da49e7791e9d77fbabed438acbca880cd4369e50
Robert Laws <roblaws@me.com>
2017-08-18 13:47:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  result = ""
+  last_el = ""
+  len = array.size
 
+  case len
+  when 1
+    result = array.join
+  when 2
+    result = array.join(" and ")
+  else
+    last_el = array.pop
+    result = array.join(", ")
+    result << ", and " + last_el
+  end
+  result
 end
