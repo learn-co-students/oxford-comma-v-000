ff0d56c2b5bd931a2e4a6e2915e31db266038c73
brandon wirz <brandonwirz2@brandons-MacBook-Pro.local>
2017-08-18 12:04:11 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  #if less than or equal to 2 coverts array to a string adds "and" between strings
+  if array.length <= 2
+    array.join(" and ")
+  else
+    # converts an array to a string and adds a comma/space between strings",
+    # then adds ", and" between last word in the string"
+    "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+  end
 end
