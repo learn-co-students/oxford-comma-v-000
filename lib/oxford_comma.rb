e55568bfcedbb32e381c1b26b1c9358bae5ac442
Kathleen Connell <kathleenconnell11@gmail.com>
2017-08-19 19:35:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+    #=>returns array at index 0 and array at index 1, to give you "string and string"
+  elsif array.size > 2
+    array[-1].insert(0, "and ")
+    #=>takes the last item of the array and inserts an "and" right before it
+    #=>the 0 indicates the index relative to array[-1], so that is the array elements
+    #=> right before the last element
+  end
+ array.join(", ")
 end
