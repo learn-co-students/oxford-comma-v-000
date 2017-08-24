77bea7e030aee6adabe2aeb22c89e32b589c468f
Alyssa Ragusa <alyssa.ragusa07@yahoo.com>
2017-08-24 03:09:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size >= 3
+    last_word = "and #{array[-1]}"
+    array.pop
+    array << (last_word)
+  end
+  return array.join(", ")
 end
