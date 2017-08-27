d15388a21c9b6b9d1012f46cff7b4172190587fe
matos89 <matos89@gmail.com>
2017-08-26 20:51:31 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.count <= 2
+    array.join(" and ")
+  elsif array.count >= 3
+    last_element_in_array = array.pop
+    string = array.join(", ")
 
+    string << ", and "
+    string << last_element_in_array
+  end
 end
