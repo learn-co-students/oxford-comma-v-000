9bd297c909b3ff929c57e9f1559cf6da8d9d8ffe
camneu37 <neuner.camille@gmail.com>
2017-08-27 18:39:25 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count > 2
+    last_item = array[-1]
+    array.pop
+    string = array.join(", ")
+    string << ", and #{last_item}"
+  end
 end
