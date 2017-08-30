04ff7f8974479ebe4ce5120165e688b4305458c5
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-08-30 20:42:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  new_string = ""
+  count = array.length
 
+  array.each_with_index do |element, index|
+    new_string += element
+    if index < count - 2
+      new_string += ", "
+    elsif index == count - 2 && count > 2
+      new_string += ", and "
+    elsif index == count - 2 && count == 2
+      new_string += " and "
+    end
+  end
+
+  new_string
 end
