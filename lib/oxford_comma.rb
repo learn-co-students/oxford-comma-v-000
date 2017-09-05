1365d8924b55daff05bf3713ea483dcf0ebec35f
Sophie Chang <soapycee@icloud.com>
2017-09-05 20:49:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  if array.count == 1
+    array.join
+  elsif array.count == 2
 
+  array.join(" and ")
+elsif array.count == 3
+  array.join(", ").insert(14, "and ")
+elsif array.count > 3
+  result = "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+  # placement = array.length - 1
+  # array.each do |place|
+  # array.join(", ").insert(array[0..-2], "and ")
+end
 end
