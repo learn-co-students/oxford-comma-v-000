06864d74b8183c1bfead2c3af463117e61eb2eae
Sophie Chang <soapycee@icloud.com>
2017-09-05 21:28:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,9 +7,17 @@ def oxford_comma(array)
 elsif array.count == 3
   array.join(", ").insert(14, "and ")
 elsif array.count > 3
-  result = "#{array[0, array.length-1].join(", ")}, and #{array.last}"
-  # placement = array.length - 1
-  # array.each do |place|
-  # array.join(", ").insert(array[0..-2], "and ")
+  # "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+  list = ""
+
+  array.each do |a|
+    unless a == array.last
+      list << "#{a}, "
+    else
+      list << "and #{a}"
+    end
+  end
+
+  return list
 end
 end
