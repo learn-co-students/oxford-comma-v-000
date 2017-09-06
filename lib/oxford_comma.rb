8b9cf77b7320c625738732a80619de620f2b5ce1
Chris Nady <nady.christopher@gmail.com>
2017-09-06 14:42:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  new_string = String.new
+  array.each_with_index do |i,index|
+    if index==0
+      new_string<<i
+    elsif (i==array.last && array.length ==2)
+      new_string<<" and #{i}"
+    elsif i==array.last
+      new_string<<", and #{i}"
+    else
+      new_string<<", #{i}"
+    end
+  end
+  new_string
 end
