38a084b6364db4bd80910b20c10b41a6e12a1586
Ally Kadel <akadel01@gmail.com>
2017-09-08 17:15:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    front_elements = array[0..-2].join(", ")
+    front_elements << (", and " + array[-1])
+  end
 end
+
+binding.pry
