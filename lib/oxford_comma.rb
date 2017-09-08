3a3ee59c36a4cb84f19d37e5de281e78e4fcfb3b
Cristina Collazos <clcgalex@gmail.com>
2017-09-08 18:39:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    last_one = array.pop
+    array.join(", ") << ", and #{last_one}"
+  end
 end
