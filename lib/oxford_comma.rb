91de9ab89264b9613c68d2d60ae179a75f966fb7
ff18qd <ff18qd@gmail.com>
2017-09-10 01:21:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size ==2
+    last = array.pop
+    last = " and " + last
+    array<<last
+    array.join
+  else
+    last = array.pop
+    last = "and " + last
+    array<<last
+    array.join(", ")
+  end
 end
