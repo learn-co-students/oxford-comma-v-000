fdec9c4b91d124f5648b3da751568e944760a01c
Jeremy <emata@rapidratings.com>
2017-09-10 13:47:26 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,14 @@
-
-  
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")    
-end  
-  
 
+if array.length>2
+
+  array[-1].insert(0, "and ")
+  array.join(", ")
+elsif array.length == 1
+  array.join()
+else
+  array.join(' and ')
+end
+
+
+end
