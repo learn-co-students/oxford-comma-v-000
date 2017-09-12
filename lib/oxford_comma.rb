69623ce30f1a0fb1c62725d977ee328253720c06
Kristen Peck <kpeck71@gmail.com>
2017-09-12 23:24:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  #array.join(", ")
+  #array.join(" and ")
+  if array.length > 2
+    array.insert(-2,"and")
+    string = array.join(", ")
+    string.sub("and,","and")
+  elsif array.length == 2
+    array.join(" and ")
+  else array.length < 2
+      array.join(", ")
+  end
 
 end
