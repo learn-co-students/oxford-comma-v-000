13c57b65bfffbdbfb8381688a546fa27f03ccc38
rjcolmed <rjcolmed@gmail.com>
2017-09-13 14:18:39 +0000

    add refactored solution

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,8 @@
 def oxford_comma(arr)
-  if arr.length == 0
-    "No array..."
-  elsif arr.length == 1
-    arr[0]
-  elsif arr.length == 2
-    "#{arr[0]} and #{arr[1]}"
-  else
-    arr[0..-2].join(", ") + ", and " + arr[-1]
-  end
+  new_string = case arr.size
+               when 0 then "No array..."
+               when 1 then arr[0]
+               when 2 then "#{arr[0]} and #{arr[1]}"
+               else arr[0..-2].join(", ") + ", and " + arr[-1]
+               end
 end
