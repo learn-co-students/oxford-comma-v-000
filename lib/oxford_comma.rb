db86f2991c83d24581a2cfdc048483209b289217
rjcolmed <rjcolmed@gmail.com>
2017-09-13 14:08:21 +0000

    add initial solution, #oxford_comma, using if/elsif/else

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.length == 0
+    "No array..."
+  elsif arr.length == 1
+    arr[0]
+  elsif arr.length == 2
+    "#{arr[0]} and #{arr[1]}"
+  else
+    arr[0..-2].join(", ") + ", and " + arr[-1]
+  end
 end
