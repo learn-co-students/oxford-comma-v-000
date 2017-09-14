2f7d573ff90dbdaa8c70db7da10601e421969f5e
Yosa Yon <yosa.yon@gmail.com>
2017-09-14 20:12:45 +0000

    solved oxford lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.length === 1
+    return array.join
+  end
+  if array.length === 2
+    return array.join(" and ")
+  end
+  if array.length === 3
+    new_array = array.slice(0,array.length-1).join(", ")
+    last_value = array.pop()
+    return new_array << ", and "<< last_value
+  end
+  if array.length > 3
+    new_array = array.slice(0, array.length-1).join(", ")
+    last_value = array.pop()
+    return new_array << ", and " << last_value
+  end
 end
