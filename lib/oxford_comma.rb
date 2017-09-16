634a8693fade23f539a08d75f697d56d7025a176
Nathan  Visak <nathan.visak@gmail.com>
2017-09-16 20:46:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array[array.length - 1] = "and " + "#{array.last}"
+    array.join(", ")
+  elsif array.length > 4
+    array[array.length - 1] = "and " + "#{array.last}"
+    array.join(", ")
+  end
 end
