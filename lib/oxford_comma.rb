eee10d4de2a4f9418e407a7ce92d769c07e333c8
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-18 17:42:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1 then array.join
+  when 2 then array.join(" and ")
+else
+  last_word = array.pop
+  array << "and " + "#{last_word}"
+  array.join(", ")
+end
 end
