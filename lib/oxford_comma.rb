043f2dc3e7a5aa736320db59831589298908adb5
Emily Morgan <emorgan05@gmail.com>
2017-09-18 17:14:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last_word = array.last
+    changed_array = array[0...-1]
+    string = changed_array.join(", ")
+    string << ", and #{last_word}"
+  end
 end
