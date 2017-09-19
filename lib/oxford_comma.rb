877f3d1064a8487c8b17c502a4dd6db82180eebe
John Mooney <jrmint46@gmail.com>
2017-09-19 19:38:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    newarray = array.each_with_index.map do |x, y|
+      if array[y+1] == nil
+        x.prepend("and ")
+      else
+        x << ", "
+      end
+    end
+    return newarray.join
+  end
 end
