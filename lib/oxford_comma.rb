b45917461f42254c4aaa79005594250ed23f4a51
Alberto Mayanga <amayanga90@yahoo.com>
2017-09-20 23:19:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join()
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+  last_element = "and " + array[-1]
+  array[-1] = last_element
+  array.join(", ")
+  end
 end
