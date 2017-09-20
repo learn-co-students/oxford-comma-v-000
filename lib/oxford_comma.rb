a971742fe5add8df69f30ed3cf9c3d452225c58b
Adam Tropp <adt6261@gmail.com>
2017-09-20 00:55:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,41 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size == 3
+    return "#{array[0]}" + ", " + "#{array[1]}" + ", and " + "#{array[2]}"
+  else
+    my_string = ""
+    array.each_with_index {|element, index|
+      if index < (array.size - 2)
+        my_string << "#{element}, "
+      elsif index == (array.size - 2)
+        my_string << "#{element}, and "
+      else
+        my_string << "#{element}"
+      end
+    }
+    return my_string
+  end
 end
+
+
+
+
+
+
+
+
+    #  if array[element.index] < (array.size -2)
+      #  my_string <<("#{element}, ")
+    #  elsif array[element.index] == (array.size-2)
+    #    my_string <<("#{element}, and ")
+    #  else
+      #  my_string << ("#{element}")
+    #  end
+    #end
+    #return my_string
+  #end
+#end
