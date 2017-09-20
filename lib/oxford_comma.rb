545e454bda7bb24478b72d53002ed0cb1ad4ba6c
dsbondin <dsbondin@gmail.com>
2017-09-20 00:36:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    last = array[-1]
+    array.pop
+    string = array.join(", ")
+    string = string + ", and " + last
+  end
 end
