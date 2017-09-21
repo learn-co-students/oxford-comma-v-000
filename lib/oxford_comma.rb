0f4cda25e3345b39a3b05189cae33b782aa818c1
Frank McCallion <keith@sensortech.com>
2017-09-21 12:30:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
 
+  array_to_string = array.join(" and ")
+
+  count = array.size
+
+  case count
+
+    when 1
+      array_to_string = array.join
+    when 2
+      array_to_string = array.join(" and ")
+    else
+      last_item = array.pop
+      array_to_string = array.join(", ")
+      array_to_string << ", and " + last_item
+
+  end
+
+  return array_to_string
+
 end
