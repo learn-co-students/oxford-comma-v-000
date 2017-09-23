6f078e1d1c6794e65020a0554cd51c698d690b2e
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 03:46:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  #string with no additional formatting, 1-element array
+  if array.length == 1
+    array.join
+  end
+  #add "and" between elements in 2-element array
+if array.length == 2
+return "#{array[0]} and #{array[1]}"
+  #and 'and' before last element if array > three elements
+elsif array.length > 2
+array[-1].insert(0, "and " )
 end
+array.join(', ')
+end
+
+#array to string
