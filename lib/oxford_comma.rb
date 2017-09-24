1948d391e785b0907acadf1ffa6757714249d9cd
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-24 17:05:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+veggies = ["fiddleheads", "okra", "kohlrabi", "broccoli"]
 
+def oxford_comma(array)
+  if array.length <= 2
+    return array.join(" and ")
+
+#UNNECESSARY ADDITION. CHANGE array.lenght == 2 to array.length <= 2
+  # elsif array.length == 1
+  #   puts array,join
+  #   return array.join
+
+  else
+    last_string = ", and " + array.pop
+    full_string = array.join(", ") + last_string
+    return full_string
+  end
 end
+
+oxford_comma(veggies)
