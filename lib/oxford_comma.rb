d499599d190ea7a396cf4661db56b09681a83ffd
hailey endres <hailey.endres@gmail.com>
2017-09-26 20:11:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  elsif array.length == 3
+    last= array.last.to_s
+    last.prepend("and ")
+    array.join(", ")
+  else
+    last= array.last.to_s
+    last.prepend("and ")
+    array.join(", ")
+  end
 end
