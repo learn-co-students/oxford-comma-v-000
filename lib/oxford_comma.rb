f4f5b429e0dac5b3c596938e7ff4c60ac56f2562
John Tuttle <thejwt@gmail.com>
2017-09-26 00:39:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  response = ""
+  array.size == 2 ? response = array.join(" and "):response += ""
+  if array.size > 2
+    array[array.length-1] = "and #{array[array.length-1]}"
+    response = array.join(", ")
+  elsif array.size < 2
+    response = array.join
+  end
+  response
 end
