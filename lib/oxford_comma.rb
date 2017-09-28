914f4be75cf9aa30407189e4bc0505aa4fed2d1f
Trisha Garrett <trisha.garrett@gmail.com>
2017-09-28 21:15:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  if array.length > 1
+  if array.length == 2
     return array[0..-2].join(', ') + " and " + array[-1]
+  elsif array.length >=3
+    return array[0..-2].join(', ') + ", and " + array[-1]  
   else
     return array.join
 end
