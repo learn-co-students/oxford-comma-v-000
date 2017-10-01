46c32bae0f0a00d27590cfba13a20089294a2f4d
Araba Taylor <araba.taylor20@gmail.com>
2017-10-01 19:35:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+array = [1,2,3]
 
+def oxford_comma(array)
+  if array.length==1
+    array.join
+  elsif array.length==2
+    array.join(" and ")
+  elsif array.length==3 || array.length>3
+    new_array = []
+    new_array<<array.pop
+    new_array.unshift(", and ")
+    array.join(", ") + new_array.join
+  end
 end
