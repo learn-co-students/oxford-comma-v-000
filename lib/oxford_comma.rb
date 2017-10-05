b2daf886af7bd9c452fe2fc9f4c87e8863ac1eb7
Anthony Harvey <anthonygharvey@gmail.com>
2017-10-05 03:06:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    (array.first 2).join(", ") << ", and " << array.pop
+  else
+    (array.first array.size - 1).join(", " ) << ", and " << array.pop
+  end
 end
