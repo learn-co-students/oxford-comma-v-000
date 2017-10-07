82686726417a9873739998717d77eb2eb259d5e9
Richard Hanolt <rhanolt@gmail.com>
2017-10-07 01:06:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
+  if array.size == 1
+    new_array = array.join
 
+  end
+  if array.size == 2
+    new_array = array.join(" and ")
+  end
+  if array.size == 3
+    temporary_array = array[2]
+    array.pop
+    array.push("and ")
+    new_array = array.join(", ")
+    new_array << temporary_array
+  end
+  if array.size >= 4
+    temporary_array = array[array.size - 1]
+    array.pop
+    array.push("and ")
+    new_array = array.join(", ")
+    new_array << temporary_array
+  end
+  return new_array
 end
