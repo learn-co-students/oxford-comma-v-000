a48a07cf7e6a06af0c5ede13d82f98982effac23
Richard Dotson <rich@cityofla.net>
2017-10-12 01:20:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+#  "#{array[0]}, #{array[1]}, and #{array[2]}" works but too literal, not abstract enough
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  elsif array.length > 3
+    #array[-2].insert(-2, "and ") #this line tipped me off to solution
+    array[-1].insert(0, "and ")
+      array.join (", ")
+end
 end
