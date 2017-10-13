9e5532bf3875d7886813712fb332ee4d8f21bcb1
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-10-13 17:58:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array[0]
+  when 2
+    return array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
