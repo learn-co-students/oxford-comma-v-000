c7b978895949f5c82ff05040e0ac6ecdb704afcc
Matthew Crooks <mfcrooks@gmail.com>
2017-10-17 00:00:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array.join
+  elsif array.count == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    array[-1].insert(0,"and ") #index of -1 counts backwards from end of index_
+  end
+  array.join(", ")
 end
