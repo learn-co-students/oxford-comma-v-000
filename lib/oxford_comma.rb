ecbbe54f65254529ed428962fa4c3cc684dc6824
Julianne Kim <juliannne@gmail.com>
2017-10-18 20:36:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  str = ""
+  if array.length <= 2
+    array.join(" and ")
+  elsif array.length > 2
+    array[0...-1].each do |x|
+      str << x + ", "
+    end
+    str + "and " + array[-1]
+  end
 end
