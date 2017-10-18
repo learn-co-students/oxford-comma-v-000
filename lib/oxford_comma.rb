0945f4d139d06ede7671e5ab78a485756c549498
Jessica Bensch <jessica.bensch11@gmail.com>
2017-10-18 03:03:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  string = ""
+  array.each do |word|
+    if array.count == 1
+      string << "#{word.to_s}"
+    elsif array.count == 2
+      if word == array[0]
+        string << "#{word.to_s}"
+      else
+        string << " and #{word.to_s}"
+      end
+    else
+      if word == array[0]
+        string << "#{word.to_s},"
+      elsif word != array[array.count - 1]
+        string << " #{word.to_s},"
+      else
+        string << " and #{word.to_s}"
+     end
+    end
+  end
+  return string
 end
