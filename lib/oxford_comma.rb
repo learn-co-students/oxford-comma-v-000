4197e5ae52dac96a965771dbfab109b8dc2b3da1
Richard Chang <rchang9999@yahoo.com>
2017-10-20 00:39:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+if array.length == 1
+  array.join("")
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length >=3
+  array[0..array.length-2].join(", ") + ", and " + array[array.length-1].to_s
+end
 
 end
