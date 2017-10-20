2237acba096fada60910a5434a4d3d0bf7a3f467
Felix Lemerle <felix@lemerle.fr>
2017-10-20 20:42:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  string = array.shift.to_s
+  if array.length == 1
+    string << " and " + array.shift.to_s
+  elsif array.length > 1
+    while array.length > 1 do
+      string << ", " + array.shift.to_s
+    end
+    string << ", and " + array.shift.to_s
+  end
+  string
 end
