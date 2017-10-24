b33b88530d0033da8b39c77fe97c2067a19f4c70
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-24 19:17:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = array.slice!(array.length-1)
+    array.push("and #{last}")
+    array.join(", ")
+  end
 end
