daca69b37a652290453378712a6ec0f49ca38532
Shibani Mookerjee <shibani.mookerjee@gmail.com>
2017-10-25 20:04:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  str = ""
+  if array.size < 3
+    str = array.join(" and ")
+  else
+    array.each_with_index do |el, index|
+      if index == array.size - 1
+        str += "and #{el}"
+      else
+        str += "#{el}, "
+      end
+    end
+  end
+  return str
 end
