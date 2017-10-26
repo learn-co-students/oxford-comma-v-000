b2c7d5d4a0596d7bcd2c078598c356fad9002efa
Nolan Hughes <nhughes987@gmail.com>
2017-10-26 20:26:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+ if array.length < 3
+   array.join(" and ")
+ else
+   last_index = array.last
+   array.pop
+   array << "and #{last_index}"
+   array.join(", ")
+ end
 end
