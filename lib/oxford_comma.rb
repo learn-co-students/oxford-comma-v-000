bf49fb835d9039c782be150d29a7a968998dc572
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 16:49:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  string = ""
+  array.each {|item|
+    if array.size == 1
+      string = item
+    elsif array.size == 2 && item.index == 0
+      string << item
+    elsif item == array.last
+      string << "and #{item}"
+    else
+      string << "#{item}, "
+    end
+  }
+  string
 end
+
+array = ['sloth', 'wrath']
+
+puts oxford_comma(array)
