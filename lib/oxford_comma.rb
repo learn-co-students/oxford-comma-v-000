105cfaf7e3aeead2c621adf4d07c299e51f78958
Yina Zhu <yinazee@gmail.com>
2017-10-31 04:34:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require "pry"
 
+
+def oxford_comma(array)
+  # binding.pry
+if array.size == 1
+  return array[0]
+elsif array.size == 2
+  return array.join(" and ")
+else
+  # we want to add 'and' to the last element
+  array[array.size - 1] = "and #{array[array.size - 1]}"
+
+  return array.join(", ")
 end
+ end
