ff70f3746555a404442477e49ed88fe0c5fee149
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-01 20:11:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+    last_element = array.pop #.pop gets rid of the last element of the array and returns that last element so we can reuse it again
+    return array.join(", ") << (", and ") << last_element
+  else
+    return array.join
+  end
 end
