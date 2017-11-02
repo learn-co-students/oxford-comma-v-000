c36225e6568a5b43a0906bb6297a92da200b4209
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 19:33:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    count = 0
+    new_string = ""
+    while count < array.length - 1
+      new_string << "#{array[count]}, "
+      count += 1
+    end
+    new_string << "and #{array[array.length-1]}"
+    new_string
+  end
 end
