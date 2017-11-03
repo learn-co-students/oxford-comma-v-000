44ffc9c442329cf4d211ebcaed501132cf9234a3
Nolan Byrnes <nolanehs@gmail.com>
2017-11-03 20:26:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+if array.size == 1
+  return array.join
+end
+if array.size == 2
+  return array.join(" and ")
+end
+if array.size >= 3
+  last = array.pop
+return array.join(", ") << ", and " << last
+end
+
+
 
 end
