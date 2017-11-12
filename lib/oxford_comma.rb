a21d6e2907924a87882f1a475c194988a1921798
Bryan Hooper <bryan@lindsayfarm.net>
2017-11-11 19:59:25 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
 
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    last = array.pop
+    string = array.join(", ")
+    string << ", and "
+    string << last
+  end
+
+  
 end
+
