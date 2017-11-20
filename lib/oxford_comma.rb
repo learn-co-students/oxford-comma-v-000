d13d1d95e833135f7e04bb3aedd589520bf34184
John O'Connor <oconnojb@gmail.com>
2017-11-20 18:48:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.size >= 3
+    array[-1]= "and #{array[-1]}"
+    return array*", "
+  end
 end
