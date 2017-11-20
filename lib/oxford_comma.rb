7b9e3c6ed5c4ceaa5822b858434d1fbac3ffd4e4
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 14:50:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size < 3
+    array.join (" and ")
+  else
+    new_last_item = ["and ", array.last].join
+    array.pop
+    array << new_last_item
+    array.join(", ")
+  end
 end
