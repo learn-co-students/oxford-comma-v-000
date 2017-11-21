27d93dd7001b2050dc92301915d55754dab41437
Jason Vance <jbvance@gmail.com>
2017-11-21 19:35:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.size >= 3
+    tmp = ""
+    array.each_with_index do |el, index|
+      tmp += index == array.size - 1 ? "and #{el}" : el + ", "
+    end
+    return tmp
+  end
 end
