8cc54e691ba70d2ce896f4d38b37dff95d244dc6
Chris Gullickson <cgullickson5@gmail.com>
2017-11-21 17:40:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    y = array.pop
+    x = "and #{y}"
+    array.push(x)
+    array.join(", ")
+  elsif array.size == 2
+    y = array.pop
+    x = "and #{y}"
+    array.push(x)
+    array.join(" ")
+  else 
+    array.join
+  end
 end
