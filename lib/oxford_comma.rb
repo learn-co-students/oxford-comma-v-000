b43d04aa74790bee09d4aff5c92737931b0e0efc
Raiet Intraub <raietsaeed@gmail.com>
2017-11-21 20:54:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  tempString = ""
+  if array.length == 1
+    return array.join
+  else
+    array.each_with_index do |element, index|
+      if index == array.length - 1
+        tempString += "and " + element
+      else
+        if array.length == 2
+          tempString += element + " "
+        else
+          tempString += element + ", "
+        end
+      end
+    end
+    return tempString
+  end
 end
