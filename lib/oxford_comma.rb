ba7b8f2294c105d2c55e43ec3b87841b1c74570d
Liz Burton <liz.burton147@gmail.com>
2017-11-22 16:59:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    array.each_with_index do |element, index|
+      if index < array.size - 2
+        array[index] << ", "
+      elsif index == array.size - 2
+        array[index] << ", and "
+      end
+    end
+  elsif array.size == 2
+    array[0] << " and "
+  end
+  value = array.join
 end
