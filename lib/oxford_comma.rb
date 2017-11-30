9e90a931512844b4fa34a74fa8dde38c4d892018
James Ferguson <ncjamesferguson@gmail.com>
2017-11-30 22:56:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
 
+  if array.length
+
+    case array.length
+      when 1
+        array.first
+      when 2
+        array.join(" and ")
+      else
+        closer = array.pop
+        array.join(", ") + ", and #{closer}"
+    end
+
+  end
+
 end
