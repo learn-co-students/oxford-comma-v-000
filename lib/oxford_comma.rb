d50a2b686331cd8db2980305d95799e178c49918
Brittany Morris <brittmmendez@gmail.com>
2017-11-30 17:43:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+array=["a", "b", "c","t"]
 
+def oxford_comma(array)
+  if  array.size==2
+    array.join(" and ")
+  elsif array.size>2
+    "#{array[0..-2].join(", ")}, and #{array.last}"
+  else
+    array.join
+  end
 end
