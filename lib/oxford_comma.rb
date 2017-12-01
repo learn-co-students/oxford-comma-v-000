28d6e3e8d3102c2287fbf94d4bce9e512c91bef4
Annalisa Graziano <arosegraz@gmail.com>
2017-12-01 21:58:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string = array.join
+  elsif array.length == 2
+    string = array.join(" and ")
+  elsif array.length >= 3
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
 end
