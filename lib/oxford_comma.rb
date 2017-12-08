5f5eb34a4f86de85aa38e1a740adeb1629ac94cf
Lena Sawyer <lenasawyer93@gmail.com>
2017-12-08 23:06:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    list = []
+    list << array[0...-1].join(", ")
+    list<< ", and #{array.last}"
+    list.join
+  end
 end
