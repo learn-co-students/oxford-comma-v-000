e6d98d3b85f8cd1b0f42209771d5bbd11e249bf1
jtorre20 <jtorres1989@gmail.com>
2017-12-08 11:16:38 -0500

    used conditional logic to format arrays

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def oxford_comma(array)
-
+   
+    if array.length == 1
+         x = array.join
+         return x
+    elsif array.length == 2
+        x = array.join(" and ")
+        x
+    elsif array.length >= 3
+        x = array[0..-2].join(", ") + ", and " + array.last
+        x
+    end
+  
 end
