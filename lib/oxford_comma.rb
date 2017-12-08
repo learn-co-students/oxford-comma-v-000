adeaa558886aef776818fdcf83587b39610a9767
jtorre20 <jtorres1989@gmail.com>
2017-12-08 12:12:00 -0500

    added case statement that is not working, corrected elsif to else

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,39 @@
 require 'pry'
+
 def oxford_comma(array)
    
     if array.length == 1
          x = array.join
          return x
+binding.pry
     elsif array.length == 2
         x = array.join(" and ")
         x
-    elsif array.length >= 3
+    else array.length >= 3
         x = array[0..-2].join(", ") + ", and " + array.last
         x
     end
   
 end
+
+
+=begin
+def oxford_comma(array)
+    array = []
+
+    case array
+
+    when array.length == 1
+        x = array.join
+        return x
+
+    when array.length == 2
+        x = array.join(" and ")
+        x
+    when array.length >= 3
+        x = array[0..-2].join(", ") + ", and " + array.last
+        x
+
+    end
+end
+=end
