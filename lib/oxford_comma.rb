5017d2fac2f2ea796cd4d9839ff2ea674a185b91
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2017-12-08 17:11:51 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1
+    array.join(', ')
+elsif array.length == 2
+    array.join(' and ')
+elsif array.length >= 3
+    string = array.slice!(-1)
+    "#{array.join(', ')}, and #{string}"
 end
+end
+
