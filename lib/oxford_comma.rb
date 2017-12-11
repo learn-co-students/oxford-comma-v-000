f601147e862151f1c88b5d88611581ab3b18eddd
Man Leung <lepusleung@gmail.com>
2017-12-11 17:11:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if array.length > 2
+    array.insert(array.length-1, "and " + array.pop)
+    array.join(", ")
+  elsif array.length == 2
+    array.insert(1, "and")
+    array.join(" ")
+  else
+    array.join("")
+  end
 end
