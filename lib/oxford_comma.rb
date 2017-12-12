2ad0ca5554e13bf7a4efce9889bcded81701dbf0
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-12 14:12:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  if array.length > 2
+  array.insert(-2, 'and')
+    x = array.length - 2
+    y = 0
+    while y < x do
+      array[y] << ","
+      y += 1
+    end
+    coma = array.join(" ")
+    coma
 
+  elsif array.length == 2
+    array.insert(1, 'and')
+      coma = array.join(" ")
+      coma
+
+  else
+    array[0]
+  end
 end
