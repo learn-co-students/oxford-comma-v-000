5089b0cb01a8fa9c7b4be806ba79ee5e1046515d
rh24 <ms.rebeccahong@gmail.com>
2017-12-12 18:55:41 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.length == 1
+    string += array.join
+  elsif array.length == 2
+    array.insert(array.length-1, "and")
+    string += array.join(" ")
+  elsif array.length > 2
+    array.insert(array.length-1, "and")
+    string += array.join(", ")
+    string.sub! "and,", "and"
+  end
+  string
 end
