db066b36ecaaf6d1f754dc83f70b5ddc18649d05
funemployeddad <funemployeddad@gmail.com>
2017-12-13 15:59:51 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,5 @@
 def oxford_comma(array)
-    if array.length <=2
-      array.join(" and ")
-    else
-      array.to_s
-    end
+    return nil if array.nil?
+    return array.join(" and ") if array.length <= 2
+    return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 2
 end
