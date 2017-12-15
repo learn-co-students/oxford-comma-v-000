2958a9326a446ba600b3681305bab2a04c306013
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-15 01:53:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,28 @@
 require 'pry'
 
 def oxford_comma(arr)
-  new = "and #{arr.last}"
-    if arr.length <= 1
-      arr[0]
-    elsif arr.length <=2
-      arr_edit = arr.slice(0, arr.length - 1).join(", ")
-      "#{arr_edit} #{new}"
-    else
-      arr_edit = [arr.tap(&:pop)].join(", ")
-      "#{arr_edit}, #{new}"
-  end
+#   new = "and #{arr.last}"
+#     if arr.length <= 1
+#       arr[0]
+#     elsif arr.length <=2
+#       arr_edit = arr.slice(0, arr.length - 1).join(", ")
+#       "#{arr_edit} #{new}"
+#     else
+#       # arr_edit = arr.slice(0, arr.length - 2).join(", ")
+#       arr_edit = [arr.tap(&:pop)].join(", ")
+#       "#{arr_edit}, #{new}"
+#   end
+# end
+
+new = "and #{arr.last}"
+  # if arr.length <= 1
+  #   arr[0]
+  # elsif arr.length <=2
+  #   arr_edit = arr.slice(0, arr.length - 1).join(", ")
+  #   "#{arr_edit} #{new}"
+until arr.length > 3
+    # arr_edit = arr.slice(0, arr.length - 2).join(", ")
+    arr_edit = [arr.tap(&:pop)].join(", ")
+    "#{arr_edit}, #{new}"
+end
 end
