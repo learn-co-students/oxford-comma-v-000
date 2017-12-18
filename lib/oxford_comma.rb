e59be064350afbcad904ff0a0d47dab597784e6f
Laura <laurao148@gmail.com>
2017-12-18 20:33:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
-    array[0]
+    array.join
   elsif array.length == 2
-    [array].join(" and ")
-  elsif array.length == 3
-    "#{array[0]}, #{array[1]}, and #{array[2]}"
-  elsif array.length == 4
-    "#{array[0]}, #{array[1]}, #{array[2]}, and #{array[3]}"
-  elsif array.length == 5
-    "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, and #{array[4]}"
-  elsif array.length == 6
-    "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, and #{array[5]}"
-  elsif array.length == 7
-    "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, #{array[5]}, and #{array[6]}"
+    array.join(" and ")
+  elsif array.length >= 3
+    "#{array[0..-2].join(", ")}, and #{array[-1]}"
   end
 end
