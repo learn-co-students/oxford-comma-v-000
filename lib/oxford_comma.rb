9401106ed77a947b31bd6eecf52835aec8f992d5
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-18 18:56:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  length = array.length
 
+  case length
+  when 1
+      array.join
+      #even tho array is only 1 element long, we still need join to convert it to a string
+    when 2
+      array.join(" and ")
+
+      #the case for 3 or 3 or more is essentially the same. Just code it to be dynamic enough to run on 3 or 100 element arrays.
+    else
+      string_1=array[0..length-2].join(", ")
+      string_1 + ", and #{array[length-1]}" #syntax to concatenate strings together
+  end
 end
