d54facd369ce070446885d04f050e36110c33fb5
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-18 19:25:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -14,3 +14,16 @@ def oxford_comma(array)
       string_1 + ", and #{array[length-1]}" #syntax to concatenate strings together
   end
 end
+
+
+#here is the formal solution..which only briefly uses join. rather it uses string
+#concatenation, and insert
+
+#def oxford_comma(array)
+#  if array.length == 2
+#    return "#{array[0]} and #{array[1]}"
+#  elsif 2 < array.length
+#    array[-1].insert(0, "and ")
+#  end
+#  array.join(", ")
+#end
