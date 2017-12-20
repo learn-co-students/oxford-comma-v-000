5ab6a4f73080107fdb22f0e7c8bf7923707898e5
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-20 21:14:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,13 @@
-require 'pry'
 
 def oxford_comma(arr)
   new = "and #{arr.last}"
-    if arr.length <= 1
-      arr[0]
-    elsif arr.length <=2
-      arr_edit = arr.slice(0, arr.length - 1).join(", ")
-      "#{arr_edit} #{new}"
-    else
-      # arr_edit = arr.slice(0, arr.length - 2).join(", ")
-      arr_edit = [arr.tap(&:pop)].join(", ")
-      "#{arr_edit}, #{new}"
+  if arr.length <= 1
+    arr[0]
+  elsif arr.length <=2
+    arr_edit = arr.slice(0, arr.length - 1).join(", ")
+    "#{arr_edit} #{new}"
+  else
+    arr_edit = [arr.tap(&:pop)].join(", ")
+    "#{arr_edit}, #{new}"
   end
 end
