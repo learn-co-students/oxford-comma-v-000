a9091641dba7495285806f5c97dfb5ffca2ab95c
EPIC448 <samrey2018@gmail.com>
2017-12-22 20:33:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry' # require 'pry' and put binding.pry anywhere we want to filter what going on in the code.#
 
+def oxford_comma(array)
+  #covert array to string#
+    #if x == 1 element #
+    #  puts  .........
+        if array.length == 1
+         return array[0]
+       elsif array.length == 2
+         return array.join (" and ")
+       elsif array.length >=3
+         array[-1] = "and #{array[-1]}" #change the last item in the array to have the "and" as part of the string
+         return array.join (", ")
+        #  binding.pry
+       end
 end
