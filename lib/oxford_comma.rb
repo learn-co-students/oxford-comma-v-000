9c40b27d714a983fc314ffd015182ffa5125e9ac
TravisA <github email address>
2017-12-22 19:32:01 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.size > 2
+		last = array.pop
+		combined_array = array.join(", ")
+		return "#{combined_array}, and #{last}"
+	elsif array.size == 2
+		return array.join(" and ")
+	elsif array.size == 1
+		return array.join
+	end
 end
