2f2d8070e4dcc32a61f9be507b12b645d55fd761
kanndide <kanndide@gmail.com>
2017-12-27 14:23:06 +0300

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array[1] == nil
+        return "#{array[0]}"
+    elsif array[2] == nil
+        array.join" and "
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join", "
+    end
 end
