906e6aa945af9d760c6395c53459be424441fbfa
John Daise <jdaise@gmail.com>
2018-01-03 02:00:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count > 2
+    a = ", and "<< array[-1]
+    array.pop
+    array.join(", ")<< a
+  end
 end
