d88847a67a50e4718b375cc60eecc0395f62cf6f
Kyle Horsch <krhorsch@yahoo.com>
2018-01-04 22:28:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+if array.count == 1
+  array.join(", ")
 
+elsif array.count == 2
+  array.join(" and ")
+
+else
+  new_array = array.insert(-2, "and")
+  string = new_array[0..-2].join(", ")
+  string << " #{new_array[-1]}"
+
+end
 end
