420946887de4c65417a2f8d27fc6cc05274ee32f
Brad Hambrice <hambrice@uab.edu>
2018-01-04 23:30:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  
+ if array.length == 1 
+   array.join
+   elsif array.length == 2 
+   array.join(" and ")
+   elsif array.length > 2 
+   last=array.pop
+   new=array.join(", ")
+   new << ", and #{last}"
+ end
 end
