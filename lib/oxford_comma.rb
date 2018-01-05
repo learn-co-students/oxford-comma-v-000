c689a5a2b6a1ea000e32a0a1471e6e4a8797c6ea
Helen Du <helendu.614@gmail.com>
2018-01-05 18:55:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+array = ["fiddleheads","okra","kohlrabi", "cabbage"]
 def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ") # -1 means last array insert adds
+    #"and" just before the last element
+  end
+  array.join(", ") #convers to string and add comma between elements
 
 end
