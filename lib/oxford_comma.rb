178320f2f53cdf9efe79ac8a232b8a4954094c65
Sam Kalum <stkalum@gmail.com>
2018-01-05 15:07:12 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  last_elem = array.pop
 
+  oxed_string = array.join(", ")
+
+  if array.length > 1
+    oxed_string << ","
+  end
+  if array.length >= 1
+    oxed_string << " and "
+  end
+
+  oxed_string << last_elem
 end
