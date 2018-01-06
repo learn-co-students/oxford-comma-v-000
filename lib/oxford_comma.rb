049703fe5b97d01bf1b3795af0f8dc786f4c0127
Alli Tzadok <allisontzadok@gmail.com>
2018-01-06 23:41:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array == nil
+    return nil
+  elsif array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else array.length > 2
+    return array[0..-2].join(", ")+", and "+ array[-1]
+  end
 end
