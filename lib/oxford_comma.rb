cb563854d7d9bb1989a8190fab0ab521cb1a635e
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-01-08 00:31:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  last_item = array.pop
+  if array.count == 0
+    return last_item
+  elsif array.count == 1
+    return "#{array[0]} and #{last_item}"
+  else
+    return "#{array.join(", ")}, and #{last_item}"
+  end
 end
