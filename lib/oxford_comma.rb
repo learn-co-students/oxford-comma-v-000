2c437a52786aa2f2158a5db1a379b77deec8be43
jljardon <github email address>
2018-01-11 22:03:37 -0500

    added oxfor comma rules

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+   if array.size == 1
+      array.join
+   elsif array.size == 2
+      array.join(' and ')
+   else
+      string = ''
+      array.each_with_index.map do |element, i|
+         string = if array.last == element
+                     string + ', and ' + element
+                  elsif i == 0
+                     element
+                  else
+                     string + ', ' + element
+                  end
+      end
+      string
+   end
 end
