9e6fb5ee3ed5b8ecebd46adb648486aefd48f119
Ingrid Wong <yw340@georgetown.edu>
2018-01-12 21:24:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 def oxford_comma(array)
 
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    output=""
+    for i in 0..array.length-2
+      output<<"#{array[i]}, "
+    end
+    output<<"and #{array[array.length-1]}"
+  end
 end
