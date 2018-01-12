708d76ae57382d4ec29f6d8081294bb303ea5caa
Brent  Ransom <sadumedusa@gmail.com>
2018-01-12 16:09:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    string = ''
+    array.each_with_index.map do |word, x|
+      string = if array.last == word
+        string + ', and ' + word
+      elsif x == 0
+        word
+      else
+        string + ', ' + word
+      end
+    end
+string
+  end
 end
