02facd83cb12f6103509490a4ea0cb0810ae4214
jpkim921 <jpkim921@gmail.com>
2018-01-15 16:25:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  return array[0] if array.length == 1
+  return "#{array[0]} and #{array[1]}" if array.length == 2
+  
+  
+  array[0...-1].join(", ") + ", and #{array[-1]}"
+  
 end
+
