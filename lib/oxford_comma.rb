8076444eed552278bfcdcc5d28afe758ec085956
Barbara Choy <barbchoy@gmail.com>
2018-01-19 07:48:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  str = ""
+  if array.size == 1
+    str << array[0]
+  elsif array.size == 2
+    str << array.join(" and ")
+  else
+    str = array[0..-2].join(", ")
+    str << ", and #{array.last}"
+  end
+  str
 end
