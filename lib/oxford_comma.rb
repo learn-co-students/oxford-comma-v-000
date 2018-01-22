f7335e3642a9b25b72d4ce0c9c047f2c8195b9c9
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-01-22 17:49:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 def oxford_comma(array)
-if array.length == 1
-"#{array[0]}"
-elsif array.length == 2
-  array.join(" and ")
-elsif array.length == 3
-fruit = array.pop
-   array.join(", ") + ", and #{fruit}"
- elsif array.length >= 4
-   fruit = array.pop
-      array.join(", ") + ", and #{fruit}"
-end
-end
+  if array.length == 1
+  "#{array[0]}"
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+  fruit = array.pop
+     array.join(", ") + ", and #{fruit}"
+   elsif array.length >= 4
+     fruit = array.pop
+        array.join(", ") + ", and #{fruit}"
+    end
+  end
