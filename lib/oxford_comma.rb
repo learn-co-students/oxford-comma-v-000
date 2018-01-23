d828c8cb01085b4a68a8e2adf087e527af79a630
Brennan Heisler <learn@heislercreative.com>
2018-01-23 13:13:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size == 3
-    array.join(", ")
+  elsif array.size >= 3
+    array_last = array.pop
+    new_array = array << "and #{array_last}"
+    new_array.join(", ")
   end
 end
