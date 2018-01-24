e6cdf007aadc2f1b0649cac47c3b6cc4d9e9ebf0
Kim Gonzales <kimgo589@gmail.com>
2018-01-24 21:47:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif 2 < array.size
+    last_word = array.pop
+    beginning_of_array=array.join(", ")
+    beginning_of_array <<", and #{last_word}"
+  else
+    array.join
+  end
 end
