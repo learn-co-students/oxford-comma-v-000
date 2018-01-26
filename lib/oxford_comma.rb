ec4c9afda1623a3cf7374565188db8060358afd2
Trac Nguyen <nguyendtrac@gmail.com>
2018-01-26 21:38:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    fruit = array.pop
+      array.join(", ") + ", and #{fruit}"
+  end
 end
