ba7d324da8426fbd6fdd78f44bafe77d3775d0db
Rollin Metzger <rollin.metzger@gmail.com>
2018-01-26 22:28:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array.join
+  when 2
+    return array.join(" and ")
+  end
+  if array.size >= 3
+    pop_element = array.pop
+    return array.join(", ") + ", and " + pop_element
+  end
 end
