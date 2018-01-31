20512792c828157176bd7abc66e026019465b5c7
cam <camsay@gmail.com>
2018-01-31 14:22:04 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.count == 1
+    array.join(" ")
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count > 2
+    last = array.pop
+    array.join(", ") + ", and #{last}"
 
+
+  end
 end
