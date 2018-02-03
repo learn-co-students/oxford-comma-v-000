9ce01abb891a58222c5cfc00afd451f9e687e492
Olivia Hanson <olivia.joy.hanson@gmail.com>
2018-02-03 19:31:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    last = array.pop
+    string = array.join(", ")
+    string << (", and ")
+    string << last
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
