9b9225d8e60b56056c03b8bb54d8dd2d279eb9af
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-04 16:08:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  string = ""
+  array_length = array.length
+  case array_length
+  when 1
+    string = array.first
+  when 2
+    string = array[0] + " and " +  array[1]
+  else
+    array.each_with_index do |element, index|
+      if index == (array.length - 1)
+        string += "and " + element
+      else
+        string += element + ", "
+      end
+    end
+  end
 
+  return string
 end
