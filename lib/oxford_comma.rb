db7fcaf59cd646cb05ba2a249177320ae0258bcf
Salma Elshahawy <salmaeng71@gmail.com>
2018-02-05 11:08:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+array = ["fiddleheads","okra","kohlrabi"]
 def oxford_comma(array)
-
+	case array.length
+	when 1
+		array[0]
+	when 2
+		array.join(" and ")
+	else
+		last_element = array.pop
+		array.join(", ") + ", and " + last_element
+	end
 end
+oxford_comma(array)
