8d329286b4910aa23a844ecdc01f6ae17239014b
J B <ironviralmedia@gmail.com>
2018-02-05 18:09:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    return a_s = "#{array[0..array.length - 3].join(", ")}, #{array[array.length - 2..array.length - 1].join(", and ")}"
+  end
 end
