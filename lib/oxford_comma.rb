211068172463b1cdf88982aa30ba3a691c987e57
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-02-10 09:59:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 1
+    return "#{array[0]}"
+  else
+    new_addition = "and " << array[-1]
+    array.pop
+    array.push(new_addition).join(", ")
+  end
 end
