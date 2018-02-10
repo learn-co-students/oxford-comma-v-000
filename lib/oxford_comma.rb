eacf8844c14006cb8427c22a02ccb481b8532b72
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-10 04:25:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1
-    array[0] #puts out array of 1
-  elsif array.length == 2  #insert takes the position and element as arguments
-    array.insert(-2, "and")
-    array.join (" ")
-  # at array[-2] put in an "and"
-  #if the aray is more than 2 count but in and then join with comma and space for delimeter
-elsif array.length > 2
-  array[-1].insert(0, "and ")
-  array.join(", ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
