4ce66cf82d8ba69d3a3bf2e11623d4a8d285530d
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 03:06:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+  array.join(" and ")
+elsif array.size > 2
+  last_fruit = array.pop
+    new_list = array.join(", ")
+  new_list << ", and #{last_fruit}"
+else
+    array.join
+  end
 end
