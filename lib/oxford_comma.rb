3444f201e290ed61d317e15ced077d7b959f03d3
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:08:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  last_one = ", and #{array.last}"
+  new_array = []
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    array.pop
+    string = array.join(", ")
+    string << "#{last_one}"
+  end
 end
