c4e1789c95e8f6b344aadceeb751c76d6cf62f2f
smithwebtek <brad@smithwebtek.com>
2018-02-12 19:15:55 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,12 +5,7 @@ def oxford_comma(array)
     return array.join
     elsif array.count == 2
       return array.join(" and ")
-    elsif array.count == 3
-      last = array.last
-      array.pop
-      asdf = array.join(", ")
-      return asdf + ", and #{last}"
-    elsif array.count > 3
+    elsif array.count >= 3
       last = array.last
       array.pop
       asdf = array.join(", ")
