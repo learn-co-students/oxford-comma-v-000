85b019d746a2633912345693b1000fdf0ef97d7b
emi kojima <emi8emi@gmail.com>
2018-02-13 20:17:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  new_array = []
+  if array.count <= 2
+  array.join(" and ")
+  
+  else
+    new_array << array.pop
+    return "#{array.join(", ")}, and #{new_array.join}"
+  end
 end
