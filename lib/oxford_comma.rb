be269a9be07317213435da0574759e6f5311dff4
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-16 03:19:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+if array.length == 1
+  return array.join
+elsif array.length == 2
+  array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+ array.join(', ')
 end
+end
+
+
+  #return array.join(", ") + " and " + array[-1] if array.length > 1
+#end
