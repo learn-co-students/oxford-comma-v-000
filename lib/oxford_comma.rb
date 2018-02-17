4723c72d698e358ffb3e4d655426a5200a1244c5
Lauren Pardee <lap5151@gmail.com>
2018-02-17 00:56:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
-    return array.join
+    result = array[0]
   elsif array.length == 2
     front = array[0...-1]
-    back = array[-1]
-    string = front.join(", ")
-    string.insert(-1," and ")
-    string.insert(-1, back)
-    return string
+    result = "#{front[0, front.length].join(', ')} and #{array.last}"
   else
-  front = array[0...-1]
-  back = array[-1]
-  string = front.join(", ")
-  string.insert(-1,", and ")
-  string.insert(-1, back)
-  return string
-end
+    front = array[0...-1]
+    result = "#{front[0, front.length].join(', ')}, and #{array.last}"
+  end
 end
