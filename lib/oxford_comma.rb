e44194b379b178473ad12084c0c4c881fc1fb115
Lauren Pardee <lap5151@gmail.com>
2018-02-17 00:47:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    front = array[0...-1]
+    back = array[-1]
+    string = front.join(", ")
+    string.insert(-1," and ")
+    string.insert(-1, back)
+    return string
+  else
+  front = array[0...-1]
+  back = array[-1]
+  string = front.join(", ")
+  string.insert(-1,", and ")
+  string.insert(-1, back)
+  return string
+end
 end
