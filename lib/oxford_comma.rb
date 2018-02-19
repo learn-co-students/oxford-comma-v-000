58516609d49556e9a76d3940dc86c879989d9d49
Cassandra DIckson <cassandradickson7@gmail.com>
2018-02-19 06:05:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  end
+  if array.length == 2
+    return array.join(" and ")
+  end
+  if array.length > 2
+    array[-1].insert(0, "and ")
+    return array.join(", ")
+  end
 end
