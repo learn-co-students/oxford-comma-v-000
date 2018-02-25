b6affcddea036c39ce9938b7bbb5d5dbca79a236
David McGrann <dpmcgrann@gmail.com>
2018-02-25 20:18:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array1 = "and #{array[-1]}"
+    array.pop
+    array.push(array1)
+    array.join(", ")
+  end
 end
