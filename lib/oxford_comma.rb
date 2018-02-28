04470243e626e41d896e154bbc93fd9dad9b0225
William Thompson <wmthmps1@gmail.com>
2018-02-28 19:34:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  if array.count ==2
+    array.join(" and ")
+  elsif array.count > 2
+    comma=array.count-2
+    last=array.count-1
+    array.each_with_index do |fruit,number|
+      if number == comma
+        fruit << ", and "
+      elsif number == last
+        fruit << ""
+      else
+        fruit << ", "
+      end
+    end
+    array.join("")
+  else
+    array.join(", ")
+  end
 end
