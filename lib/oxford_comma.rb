320ba66e86a903392577a3dc2cadb38839d95c34
Aspen James <aspenjames@tqca.org>
2018-03-02 18:59:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  array[-1] = "and #{array[-1]}"
-  array.join(", ")
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array[1] = "and #{array[1]}"
+    array.join(" ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
