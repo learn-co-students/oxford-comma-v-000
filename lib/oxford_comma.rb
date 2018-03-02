6ffc130a36341681b66c26b6ab30c8ed5ec0e82d
Maverick Tesini <mavericktesini@gmail.com>
2018-03-02 17:11:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join(", ")
+  when 2
+    array.join(" and ")
+  else
+    z = array.pop
+    # array << ","
+    y = array.join(", ")
+    y << ", and #{z}"
+  end
 end
