9cc6351641e7ba268f8824f60a0d445ab4462ab4
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-03-03 01:55:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
     elsif array.length == 2
       return array.join(" and ")
       elsif array.length >= 3
-      return array[0..-2].join(', ') + ", and " + array[-1]
+        return array[0..-2].join(', ') + ", and " + array[-1]
   end
 end
