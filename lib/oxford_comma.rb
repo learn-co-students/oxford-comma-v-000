d2c3004e9bb1a20f1e113dd779048ad451aaf8a5
Travis Williamson <travywill@gmail.com>
2018-03-06 17:19:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    string_with_commas = array.join(", and ")
+    back_to_array = string_with_commas.split("and ")
+    back_to_array.insert(-2, "and ")
+    final_string = back_to_array.join
+    final_string
+  end
 end
