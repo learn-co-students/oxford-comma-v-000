2c0f940c6341e137148eea12b902fc25c6905c95
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-07 23:10:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.length == 1 
+return array.join(",")
+elsif array.length == 2 
+return array.join(" and ")
+elsif array.length == 3
+slice_array = array.slice(0, array.length-1)
+string = slice_array.join(", ")
+last = array[-1]
+return "#{string}, and #{last}"
+elsif array.length > 3
+slice_array = array.slice(0, array.length-1)
+string = slice_array.join(", ")
+last = array[-1]
+return "#{string}, and #{last}"
 
+end
 end
