4db8afb7c0856a0b71b99874939eab60797eb4a0
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 17:38:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_el = array.pop
+    array.join(", ") << ", and #{last_el}"
+  end
 end
+
+oxford_comma(["kiwi"])
