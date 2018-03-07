8216c8cc2f950ead4f083197bc979598dd0f139b
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 06:31:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size > 2
      array[0...-1].join(", ") + ", and " + array.last
   elsif array.size == 2
-     array[0] + " and " + array[1]
+     "#{array[0]} + and + #{array[1]}"
   else
      array[0]
    end
