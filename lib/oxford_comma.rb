46241014e9247026140283d90dafb040db025255
David Markel <davidgmarkel@gmail.com>
2018-03-11 20:53:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    array[-1] = "and #{array[-1]}"
+    array.last = "and #{array[-1]}"
     array.join(", ")
   else
     array.join()
