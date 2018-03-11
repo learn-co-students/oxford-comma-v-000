6a73676a320ee25a35ad763b174b9e21a03adeda
David Markel <davidgmarkel@gmail.com>
2018-03-11 20:50:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  else
+    array.join()
+  end
 end
