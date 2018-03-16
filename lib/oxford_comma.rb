e22654185a1ec1bb7c74eb721fc411b982a6c97e
Amit Singh <amit_nyc@live.com>
2018-03-16 04:39:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,16 +6,12 @@ end
 
 
 def oxford_comma(array)
-  if array.size > 0 || array.size < 2
+  if array.size > 0 
   [array].join(" and ")
 end
 end
 
-def oxford_comma(array)
-    return array[0] if array.length == 1
-    return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
-    return array[0..-1].join(' and ') if array.length < 2
- end
+
 
 
 def oxford_comma(array)
