16b02c8c1903f75ceebd65c4ffa78016f3b15fa9
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-17 05:36:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
+  return array[0] if array.length == 1
+  return array[0] + " and " + array[1] if array.length == 2
+  array[0..-2].join(", ") << "," + " and " + array[-1]
 
 end
