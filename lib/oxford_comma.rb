d4e2357d82dfee5357d216922837581e7d61076e
Valencia Caldwell <vkcaldwell@gmail.com>
2018-03-22 02:21:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join(", ")
+  elsif array.length > 2
+    last_element = array.pop # take the last element and store it as a variable
+    and_element = "and" #take word "and" and store it
+    array.push(and_element) # add the word "and" at the end of the array
+    array.join(", ") + " #{last_element}"
+  else array.length == 2
+    array.join(" and ")
+  end
 end
