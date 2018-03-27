ae755376310a8c950b7e37b8f613c3748f19d5f5
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-27 20:45:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.each do |i|
+      return i
+    end
+  elsif array.length == 2
+    i_array = Array.new
+    array.each do |i|
+      i_array.push(i.join("and"))
+    end
+  elsif array.length == 3
+    array.each do |i|
+      i.join(", ")
+    end
+  else
+    array.split
+  end
 end
