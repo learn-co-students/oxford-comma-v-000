cff4d17da2972a2e08b90eccc5b0276cdde19d61
Diogo Ide <diogoide@gmail.com>
2018-04-01 20:45:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size < 3
+    array.join(" and ")
+  else
+    array[0..-2].join(", ") << ", and " << array[-1]
+    #missing_last_word = []
+    #missing_last_word = array.pop
+    #array << "and"
+    #array << missing_last_word
+    #array.insert(2, "and")
+    #array.join(", ")
+  #else
+    #missing_last_word = []
+    #missing_last_word = array.pop
+    #array << "and"
+    #array << missing_last_word
+    #array.sort!
+    #array.join(", ")
+  end
 end
