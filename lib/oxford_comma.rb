8fbb3faf48d58e0ac134b17b3c8069d148f6abd6
woollymammoth1 A <isaacatwood@outlook.com>
2018-04-02 18:33:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    last = array.pop
+    holder = array.join(", ")
+    holder << ", and #{last}"
+  else
+    array.join
+  end
 end
