9a01f86e9c024111385e7426b34cb3b2ee48bfed
Joanne Lee <vernapii@gmail.com>
2018-04-02 17:53:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+def oxford_comma(array = nil)
+  if array.nil? or array.length <= 1
+  return array[0]
+elsif array.size == 2
+  return array[0] + " and #{array[1]}"
+else array.length > 3
+  return array[0..-2].join(", ") + ", and #{array[-1]}"
 
+end
 end
