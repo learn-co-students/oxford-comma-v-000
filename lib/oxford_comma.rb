4b40f91e86a20f14c2152fcd5229e25604619075
Isaac Villicana <villicana.i@gmail.com>
2018-04-06 14:42:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    new_string = ""
+    array.each do |word|
+      if array.last == word
+        new_string << "and #{word}"
+      else
+        new_string << "#{word}, "
+      end
+    end
+    new_string
+  else
+    array.join
+  end
 
 end
