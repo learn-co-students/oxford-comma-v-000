56cadc19d766d4e45f5214fadefe8378562e6808
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 16:47:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size <= 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else array.size >= 3
+    last_element = Array.new
+    last_element << array[-1]
+    last_element.unshift (", and")
+    last_string = last_element.join (" ")
+    array.pop
+    new_string = array.join (", ")
+    new_string << last_string
+ end
 end
