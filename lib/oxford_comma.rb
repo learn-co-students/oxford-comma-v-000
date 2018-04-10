888a0bb4644ffb2eb4f1df10c5edfbcb089107d0
Nicholas Bharucha <nickbharucha@ou.edu>
2018-04-10 18:17:28 +0000

    Updated #oxford_comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1
-    new_string = array.join
-    return new_string
-  elsif array.length == 2
-    array[-1] = + " and " + array[-1]
-    new_string = array.join
-  else
-    array[-1] = + "and " + array[-1]
-    new_string = array.join(", ")
-    new_string
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
