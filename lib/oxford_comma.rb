7b9ad4d6453f92c040d56be3571148c3e503aedb
Nicholas Bharucha <nickbharucha@ou.edu>
2018-04-10 18:10:11 +0000

    Wrote my method #oxford_comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    new_string = array.join
+    return new_string
+  elsif array.length == 2
+    array[-1] = + " and " + array[-1]
+    new_string = array.join
+  else
+    array[-1] = + "and " + array[-1]
+    new_string = array.join(", ")
+    new_string
+  end
 end
