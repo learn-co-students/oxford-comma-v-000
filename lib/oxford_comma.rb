c248ca973ef0f3c03d228b47ec64b68b3bd3390e
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-04-13 01:37:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array[0]
+  when 2
+    array.join(" and ")
+  else
+    last = array.pop
+    str = array.join(", ")
+    "#{str}, and #{last}"
+  end
 end
