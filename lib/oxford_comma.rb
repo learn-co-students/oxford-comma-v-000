052ece85f350cee9361021165f207dd0b8c65f55
Eric Fiorani <efiorani305@gmail.com>
2018-04-14 18:17:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size == 3
-    array[0..-2].join(", ") << ", " + "and " + array[-1]
-  else array.size > 3
+  elsif array.size >= 3
     array[0..-2].join(", ") << ", " + "and " + array[-1]
   end
 end
