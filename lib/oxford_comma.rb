90b99822d69fb986f03e0e4f30b96d9cff838e7b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-19 02:37:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  num = array.size
+  if num == 1
+    array.join
+  elsif num == 2
+    array.join(" and ")
+  else num > 2
+    arr = Array.new(array)
+    last = arr.pop
+    arr.join(", ") + ", and " + last
+  end
 end
+
+  
