a916a8921b80ce183e12bd291ec36b1a10424b6a
Christiana Vandermale <christianavdm@gmail.com>
2018-04-19 01:42:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  last_element = "and #{array[-1]}"
+  if array.count == 1 
+    array.join(", ")
+  elsif array.count == 2 
+    array.join (" and ")
+else 
+  array.pop
+  array << last_element
+  array.join(", ")
+end
 end
