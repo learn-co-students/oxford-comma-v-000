5124bb1a20f1bd8c049b63487936ccf196a76776
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-20 16:23:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    array.join(", ")
+    array.insert(array.size - 1,"and ")
+  end
 end
