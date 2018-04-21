f895e62502d261bbeb4a5180387e5a83be85e9d2
George  Pianka <gpianka1@gmail.com>
2018-04-21 13:14:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  else
+    final_word = array.pop
+    new_string = array.join(", ")
+    new_string << (", and #{final_word}")
+  end
 end
