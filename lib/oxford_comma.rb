7bc4f28fc7b1ce1505b3fdf568882d03d9792308
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-04-24 01:15:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+  if array.length < 3
+     array.join(" and ")
+   else
+     last_word = array.pop
+     new_string = array.join(", ")
+     new_string << (", and #{last_word}")
+   end
+ end
