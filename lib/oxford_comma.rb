8a76d7b2056214d9bcd4006a0feb800bed088389
Suchitra Naik <susanaik@gmail.com>
2018-04-24 15:32:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
 
+len = array.length 
+
+case  
+    when len == 1  
+      return array.join
+    when len == 2
+      return array.join(" and ")
+    when len >= 3
+      last = array.slice(len-1, 1)
+      new_arr = array.pop()
+      new_arr = array.push("and ")
+      new_arr = array.join(", ")
+      puts new_arr
+      return new_arr + last.join  
+      
+    else 
+      puts "You gave me #{len} -- I have no idea what to do with that."
+    end
+
 end
