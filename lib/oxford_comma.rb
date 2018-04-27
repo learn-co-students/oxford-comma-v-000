54d92a33de4fd9af4ffe733cc56458a9affb824c
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 14:32:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  output = array.map {|element| element}
+  output[0..(output.length -2)].join(", ") + " and " + output.last
 end
+
+=begin
+def oxford_comma(array)
+  array.each do |arr_element|
+    if arr_element == 1
+      array.join
+    end
+  end
+end
+=end
