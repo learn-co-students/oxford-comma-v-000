2c2daca94c52c76f45b6a9853f1d834c4232c3ea
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 14:51:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
+=begin
 def oxford_comma(array)
   output = array.map {|element| element}
-  output[0..(output.length -2)].join(", ") + " and " + output.last
+  output[0] + output[1..output.length - 2].join(", ") + ",and " + output[output.length - 1]
 end
+=end
 
 =begin
 def oxford_comma(array)
@@ -12,3 +14,17 @@ def oxford_comma(array)
   end
 end
 =end
+
+
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array[0..1].join(", ") + ", and " + array.last
+  elsif array.length > 3
+    i = array.length
+    array[0..array.length - 2].join(", ") + ", and " + array[array.length - 1]
+  end
+end
