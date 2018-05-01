67c450e69b685b744ce02cc3a088c8ba78a303df
Dmitri Mendis <2dmendis@gmail.com>
2018-05-01 22:51:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    new_string = array.join
+    elsif array.size == 2 
+    last = array.last
+    array.pop
+    new_string = array << " and "
+    new_string = array << last
+    new_string.join
+  else 
+  last = array.last 
+  array.pop
+  
+  new_string = array.join(", ")
+  new_string << ", and #{last}"
+end 
 end
