42424c9c3a79dfd1b35bdf0133fd712b0657eb60
Dale Terrell <tragicid@gmail.com>
2018-05-04 02:33:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,15 +4,13 @@ def oxford_comma(array)
 if array.length == 1
   array [0]
 elsif array.length == 2
-  array.join("and")
-elsif array.length == 3
-  elsif array.count == 3
+  array.join(" and ")
+#elsif array.length == 3
+#  array.join = ", and "
+elsif array.count >= 3
       last = array.last
       array.pop
       asdf = array.join(", ")
       asdf + ", and #{last}"
-
-else
-  result = "#{array[0, array.length.join(', ')]} and #{array.last}"
 end
 end
