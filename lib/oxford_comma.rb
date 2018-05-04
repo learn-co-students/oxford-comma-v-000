75c6b96cf665b75288394a34078f2572f954248b
Dale Terrell <tragicid@gmail.com>
2018-05-04 01:13:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,7 @@ def oxford_comma(array)
   array.join(", and")
     if array length = 1
       result = array [0]
+    else
+      result = "#{array[0, array.length].join(', ')} and #{array.last}"
     end
 end
