9ed7c7d718c122b276abb2c66e6f069d993f8ceb
Nathaniel Shuster <nshu521992@gmail.com>
2018-05-07 23:55:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1 
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    new_ending = "and #{array[-1]}"
+    array.pop
+    array.push(new_ending)
+    array.join(", ")
+  end
 end
