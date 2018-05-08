1621ce61f489633794a5ef17696df40aed8ae5bf
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-08 00:03:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else array.size >= 3
+    last_array = "and #{array[-1]}"
+    array.pop
+    array.push(last_array).join(", ")
+  end
 end
