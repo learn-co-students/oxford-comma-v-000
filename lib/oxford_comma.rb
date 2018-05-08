fef57813fb5199cc870e3030c9b89382167f7f4c
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-08 21:20:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    oxford_end_of_array = "and #{array[-1]}"
+    array.pop
+    array.push(oxford_end_of_array)
+    array.join(", ")
+  end
 end
