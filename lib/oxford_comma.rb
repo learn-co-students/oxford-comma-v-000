9680a9a3ef99822f2374d99f80221e8ec0610f39
Conor Hamilton <conhamil19@gmail.com>
2018-05-14 10:44:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  elsif array.length > 2 
+  last_fruit = array.pop
+  array.join(", ") << ", and #{last_fruit}"
+  end
 end
