849b360c62779c84ff03e9dcc1e07a7fff5dbe98
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-15 17:53:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  sized = array.size()
+  step = 0
+  while step < sized
+    if step === 0
+      list = "#{array[step]}"
+    elsif step === 1 && sized === 2
+      list << " and #{array[step]}"
+    elsif step === sized - 1
+      list << ", and #{array[step]}"
+    else
+      list << ", #{array[step]}"
+    end
+    step += 1
+  end
+  list
 end
