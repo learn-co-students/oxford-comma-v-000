3691585378d4b1226fe100628579ec41436a4aa6
dmadust@gmail.com <dmadust@gmail.com>
2018-05-17 20:49:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0]
+  elsif array.count == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    array[-1] = "and " << array[-1]
+    array.join(", ")
+  end
 end
