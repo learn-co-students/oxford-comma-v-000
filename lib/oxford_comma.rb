85f716c8e1121a1ff63ad13b65710e19195b5b59
Elaine Anderson <eparie@bu.edu>
2018-05-21 17:54:04 +0000

    oxford commas added

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+  end_of_string = array.slice!(-2..-1)
+    array.join(", ") << ", " << end_of_string.join(", and ")
+  else
+    array.join
+  end
 end
