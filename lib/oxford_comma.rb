d4082a221f1c947ccd445535f664bc173a4f5ee7
Brandon Green <brandon@brandonleeproductions.com>
2018-05-24 02:43:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count < 3  #If the array has less than 3 indexes
+    array.join(" and ") #Put " and " in between the 2 indexes
+  else
+    array[0, array.length - 1].join(", ") + ", and " + array.last 
+    #Basically length method used on arrays in ruby returns number of elements in the array for which method is invoked.
+    # So I'm pulling the array length, using -1 to put "and" before the last array element.
+  end
 end
