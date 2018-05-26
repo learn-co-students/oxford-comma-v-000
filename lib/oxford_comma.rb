3feb65ae30582c05bc9d099419893840d7563ec5
Ariel Zhen <zhenpingping@live.com>
2018-05-26 19:55:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  result = ""
+  if array.size == 1
+    result = array.join()
+  elsif array.size == 2
+    arr = array.pop()
+    result = array.join(', ') + " and #{arr}"
+  else
+    arr = array.pop()
+    result = array.join(', ') + ", and #{arr}"
+  end
+  return result
 end
