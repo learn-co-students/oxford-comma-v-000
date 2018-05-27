60af8ad2209e7862416e8f77f50e7d845dbb26e3
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 05:00:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    string_end = array.pop
+    string_front = array.join(", ")
+    string_end2 = ", and #{string_end}"
+    return "#{string_front}#{string_end2}"
+  end
 end
