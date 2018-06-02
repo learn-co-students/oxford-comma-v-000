877a80334bcfb545003e153a3718d834164ae26c
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 20:30:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case (array.length)
+  when 1
+    return array[0];
+  when 2
+    return array.join(" and ")
+  else
+    last = array.pop();
+    final = "#{array.join(", ")}, and #{last}";
+    return final;
+  end
 end
