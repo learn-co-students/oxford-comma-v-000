cceba4497b3cad67c9699fdf108d1d239549f779
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 05:30:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
 def oxford_comma(array)
-
+  len = array.length
+  newString = ""
+  if len == 1
+    newString = array[0]
+    newString
+  elsif len == 2
+    newString << array[0]
+    newString << ' and '
+    newString << array[1]
+  else
+    index = 0
+    while len > 1 do
+      newString << array[index] 
+      newString << ', '
+      len -= 1
+      index += 1
+      puts "#{newString}"
+      puts "#{len}"
+  end
+  newString << 'and '
+  newString << array[index]
+  newString
 end
+end
+
+
+puts "#{oxford_comma(["kiwi", "durian"])}"
