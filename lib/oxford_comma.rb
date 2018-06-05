a778947df00aa8d2e460e635a1e7070bd65320a6
Daniel Etter <etter.dt@gmail.com>
2018-06-05 19:02:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    end_word=array.pop
+    return array.join(", ") + ", " + "and " + end_word
+end
 end
