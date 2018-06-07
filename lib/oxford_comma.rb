2749b4c44ee06e17165ea11d06ecc97679606c56
Will Lee <will.i.lee@outlook.com>
2018-06-07 17:03:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def oxford_comma(array)
+  new_string = ""
+  
+  case array.length
+  when 1
+    new_string = array.join
+    return new_string
+  when 2
+    new_string = array.join(" and ")
+    return new_string
+  else
+    i = 0
+    while i < array.length - 1
+      new_string << (array[i].to_s + ", ")
+      i += 1
+    end
+    new_string << ("and " + array[i].to_s)
+    return new_string
+  end
 
 end
