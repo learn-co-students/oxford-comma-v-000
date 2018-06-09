61242430a269c35970f2fb454b0d94bed107ed73
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 19:12:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+  array[0]
+  elsif array.length == 2
+  array[0] + " and " + array[1]
+  elsif array.length == 3
+    array[0] + ", " + array[1] + ", and " + array[2]
+  else
+    new_array = []
+    new_array << ", and " + array.last
+    array.pop
+    array.join(", ") + new_array.join
+  end
 end
