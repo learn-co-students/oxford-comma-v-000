f0c66dc1ee1df5a1eadf2746b0beb3872cb999aa
micahshute <micah.shute@gmail.com>
2018-06-09 20:59:26 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
 def oxford_comma(array)
-
+  list = array.size == 2 ? array.join(" and ") : array.join(", ")
+  reverse_index = list.reverse.index(",")
+  last_comma_index = !!reverse_index ? list.insert(list.size - reverse_index, " and") : list
 end
