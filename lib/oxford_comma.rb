a6af5d7f773bb9d408e5d7a1181d59242af0e2d1
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 22:53:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  combined_string = ""
+  new_array = []
+  new_array.push(array[array.size - 1])
+  if array.size == 1
+    combined_string = array[0]
+  elsif array.size == 2
+    array.pop
+    combined_string = array[0]
+    combined_string << " and "
+    combined_string << new_array[0]
+  else
+    array.pop
+    combined_string = array.join(", ")
+    combined_string << ", and "
+    combined_string << new_array[0]
+  end
+  return combined_string
 end
