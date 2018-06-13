14e4d75c64ce2d0ef1c9c4fdc0ae947840533f20
Trevor Russell <truss.42@gmail.com>
2018-06-13 16:55:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length === 1 
+    return array[0]
+  elsif array.length === 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    i = 0
+    while i < array.length - 1
+      array[i] = array[i] + ", "
+      i += 1
+    end
+    array[-1] = "and " + array[-1]
+  end
+  return array.join
 end
