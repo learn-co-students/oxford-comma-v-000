479d53759bd4532196c104c6618a367f386a2f0c
Jane Justiz <janejustiz@gmail.com>
2018-06-14 03:52:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join(",")
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size>2
+    x = array.pop
+    array.push("and #{x}")
+    array.join(", ")
+   end
 end
