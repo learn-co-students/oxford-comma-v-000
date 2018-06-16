0131ea02f1819e3776908facc81e36494879d9c9
Karyme Virginia <aveonyxcode@gmail.com>
2018-06-16 11:36:39 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.size == 1
+		array[0]
+	elsif array.size == 2
+		array.join(" and ")
+	else
+		last = array.pop
+		commas = array.join(", ")
+		commas << ", and " + last
+	end
 end
