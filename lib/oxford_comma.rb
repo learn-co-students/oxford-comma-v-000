7048cedc6579b58f1bbee319192be94c3b77d3de
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-22 18:50:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,11 +6,6 @@ def oxford_comma(array)
     return array.join
   when 2
     return array.join(" and ")
-  when 3
-    a = array.take(2)
-    b = a.join(", ")
-    c = array.pop
-    return "#{b}, and #{c}"
   else
     last = array.pop
     but_last = array.join(", ")
