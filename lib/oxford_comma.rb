1a3b9437ba7cfa8385cefb3ca6112490bb2e4d40
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-22 18:48:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  case array.size
+  when 1
+    return array.join
+  when 2
+    return array.join(" and ")
+  when 3
+    a = array.take(2)
+    b = a.join(", ")
+    c = array.pop
+    return "#{b}, and #{c}"
+  else
+    last = array.pop
+    but_last = array.join(", ")
+    return "#{but_last}, and #{last}"
+    binding.pry
+  end
 end
