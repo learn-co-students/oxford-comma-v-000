2e2f96d78bb7a99daa2f0a9c3359d1ceb336478d
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 17:43:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+if array.length <= 1
+  return array[0]
+elsif array.length == 2
+  return "#{array[0]} and #{array[1]}"
+else
+  i = 0
+  concat = ""
+  until i == array.length - 1
+     concat << "#{array[i]}, "
+     i += 1
+   end
+   concat << "and #{array.last}"
+ end
+ return concat
 end
