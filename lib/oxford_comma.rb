c9e10cb89fa482f35184c66e2cb1d8c5771a1aaa
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-06-24 00:55:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+    #array.join(", ").insert(-2, " and ")
+  end
 
 end
