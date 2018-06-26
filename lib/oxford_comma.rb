14bc9a1432d382b3b061d125ba55f8baa5333197
Jordan Harris <jharris@wework.com>
2018-06-26 14:26:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length <= 1
+    return array.join
+  else 
+    length = array.length
+    index = length - 1
+    oxford_array = array.first(length - 1)
+    oxford_array = oxford_array.join(", ")
+    new = [", and "]
+    new = new.push("#{array[index]}")
+    new = new.join
+    oxford = oxford_array.concat new
+      if oxford.split(' ').size <= 3
+         oxford = oxford.gsub(/[\,]/ ,"")
+      return oxford
+    else
+    return oxford
+    end
+  end
 end
