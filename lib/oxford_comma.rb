ffbd709b86785bd4ddf70c3b01080b3880dc8e47
Kristen Papke <klpapke215@gmail.com>
2018-07-01 19:45:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
 
+def oxford_comma(strings)
+  if strings.length == 1
+    strings.join
+  elsif strings.length == 2
+    strings.join(" and ")
+  elsif strings.length > 2
+    sentence = []
+    strings.each_with_index do |string, index|
+      if index < strings.length - 1
+        sentence << string + ", "
+      else
+        sentence << "and " + string
+      end
+    end
+    sentence.join
+  end
 end
