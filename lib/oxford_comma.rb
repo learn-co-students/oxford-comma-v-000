2c9da5cc704ee5e4a2841b4977e1e4397ae62598
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-10 19:08:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,9 @@ def oxford_comma(array)
    return array.join
  elsif array.length == 2 
    return array.join(" and ")
- elsif array.length == 3
-   array.join(", ")
-   last_value = array[-1]
-   comma_combo = array.pop
-   return comma_combo << "and" << last_value
+ elsif array.length >= 3
+   array.pop
+   return array.join(", ") + ", and " 
  end
 end 
 
