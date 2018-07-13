34a17b61161092b542c9ef2b3e10c4f02a6e0bc9
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-13 23:52:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    el = "and #{array[-1]}"
+    array.pop
+    array << el
+    array.join(", ")
+  end
 end
