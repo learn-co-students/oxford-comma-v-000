6777b76556592f5576dd60fb34adecc053e15a5e
Anna Dybas <a.m.dybas@gmail.com>
2018-07-15 16:37:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,11 @@ def oxford_comma(array)
   new_array = []
   if array.length <= 1
     array.join
+  elsif array.length == 2
+    new_array = array.pop
+    array.join<< " and #{new_array}"
   else
-    new_array = array.join(", ") << " and #{array.last}"
-    new_array
+    new_array = array.pop
+    array.join(", ") << ", and #{new_array}"
   end
 end
