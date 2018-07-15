c7d06f68909b8d60075d74161d3b6c0db7e8cf6a
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-15 04:57:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,10 @@
-require "pry"
-
 def oxford_comma(array)
   if array.size == 2 
     elements = array.join " and "
-
   elsif array.size >= 3
     elements = array.each { |w| w + ", " }
     array[-1].insert(0,"and ")
     array.join ", "
-    
   else  elements = array.join if array.empty? || array.size==1
-
   end
-
 end
