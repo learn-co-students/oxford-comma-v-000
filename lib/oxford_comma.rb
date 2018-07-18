9525995a46e4da74b168d17d166733c609fb30ed
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-18 18:09:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    new_last_word = "and #{array[-1]}"
+    array.pop
+    array << new_last_word
+    array.join(", ")
+  end
 end
