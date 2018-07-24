4ae44eb3db7d0511d14c493b36694cd89b6f46cb
Benjamin Jones <bljones36@gmail.com>
2018-07-24 23:15:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  else
+    last_index = array.pop
+    last_index = "and " + last_index
+    array << last_index
+    array.join(", ")
+  end
 end
