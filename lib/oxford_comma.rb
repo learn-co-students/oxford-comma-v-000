8d8a1afcc643dfa3ecd63a82274191f0e8963285
Jae Yong Kim <kim900180@gmail.com>
2018-07-28 01:58:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size > 2
+    first = []
+    counter = 0
+    while counter < array.size - 1
+      first << array [counter]
+      counter += 1
+    end
+    last = array.last
+    return first.join(", ") + ", and " + last
+  end
 end
