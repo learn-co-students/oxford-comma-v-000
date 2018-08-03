6705f87b91e458317e6f35db65df25be218ff77a
Achsah Abraham <achsahmp@gmail.com>
2018-08-03 21:27:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.size == 1
+      return array.join
+    elsif array.size == 2
+      return array.join(" and ")
+    elsif array.size > 2
+      last_name = array.pop
+      new_array = array.join(", ")
+      new_array = new_array + ", and #{last_name}"
+      return new_array
+    end
 end
