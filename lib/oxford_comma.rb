887f30e12f9c1d0aca095f840017bd6d29d926f1
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-06 00:53:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  elsif array.size == 3 
+    last_element = array[-1]
+    array.delete(last_element)
+    return array.join(", ") << "," << " and " << last_element
+  elsif array.size > 3
+    last_element = array[-1]
+    array.delete(last_element)
+    return array.join(", ") << "," << " and " << last_element
+  end
 end
