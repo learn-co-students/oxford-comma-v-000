eef1c18e63f69376e91442d8014b9f4c17a53fc9
Austin Powell <austinpowell58@gmail.com>
2018-08-08 23:09:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2 
+    array.join(" and ")
+  else if array.size >= 3
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  else 
+    array.join
+  end 
+end 
 end
