2539de4e817e2dc57849f8a887f6cf75f7982966
Jonathan Chan <johnkchan252@gmail.com>
2018-08-15 01:51:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join("")
+  else
+    array[-1] = "and " + array[-1]
+    if array.size == 2
+      array.join(" ")
+    else
+      array.join(", ")
+    end
+  end
 end
