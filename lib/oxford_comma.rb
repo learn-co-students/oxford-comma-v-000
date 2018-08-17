f4694336bc5b9f0c4763ccc6355dc80768a3864f
Laura Darlak <laura@polyporedesign.com>
2018-08-17 16:39:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,20 @@
+# def oxford_comma(array)
+#   if array.count <= 2
+#     array.join(" and ")
+#   else
+#     last_element = array.pop
+#     str = array.join(", ")
+#     str << ", and " << last_element
+#   end
+# end
+
+# refactor
+
 def oxford_comma(array)
-  if array.count <= 2
-    array.join(" and ")
-  else
-    last_element = array[-1]
-    array.pop
-    str = array.join(", ")
-    str << ", and " << last_element
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
