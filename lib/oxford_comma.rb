70c69bf6cd42d05be11a2cdec519dba5b30a6aa7
taisaoko <ttd3calvin@gmail.com>
2018-08-17 19:18:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+   case array.size
+    when 1
+    array[0]
+    when 2
+    array.join(" and ")
+    else
+    array.insert(-2,"and")
+    array.join(" , ")
+    end
 end
