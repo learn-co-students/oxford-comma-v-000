bf3e659f7184c21c4673f0bc4c35b34ca1ea1097
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-19 20:51:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
 def oxford_comma(array)
+  if array.first == array.last
+    array.join
+  
+  elsif array[2] != nil
+    last_i = array[-1]
+    array.pop
+    array << "and " + last_i
+    array.join(", ")
+  else
+    array.join(" and ")
+  end
+end  
 
+
+
+def oxford_comma2(array)
+  last_i = array[-1]
+  array.pop
+  if array != []  
+    array << "and " + last_i
+    array.join(",")
+  else
+    last_i
+  end
 end
