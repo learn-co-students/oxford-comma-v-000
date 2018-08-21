50d63ecf035bc5c32a87208e20905bb7d6a38bd3
Kevin W <kjoewill@gmail.com>
2018-08-21 17:03:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
-def oxford_comma(array)
+list_array_1 = ["one"]
+list_array_2 = ["one","two"]
+list_array_3 = ["one","two","three"]
+list_array_4 = ["one","two","three", "four"]
 
+def oxford_comma(strings)
+  
+  if strings.size == 1 
+    return strings[0] 
+  elsif strings.size == 2 
+    return "#{strings[0]} and #{strings[1]}"
+  else
+    last_element = strings.pop
+    strings.push("and #{last_element}")
+    strings.join(", ")
+  end
+  
 end
+
+
+puts oxford_comma(list_array_1).inspect
+puts oxford_comma(list_array_2).inspect
+puts oxford_comma(list_array_3).inspect
+puts oxford_comma(list_array_4).inspect
