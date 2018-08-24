c54a57e43e6f3f6fdef08b81d6ac92d9a7bbcd8f
alice T <alicewtien@gmail.com>
2018-08-24 19:36:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join(", ")
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = array.pop # .pop gives the last item of an array
+    array.join(", ") + (", and ") + last
+  end
 end
