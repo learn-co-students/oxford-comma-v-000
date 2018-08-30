69c2ed08a300697c202884a7c3faccff58d60497
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-30 17:32:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
-# def oxford_comma(array)
-# ["kiwi"].join
-# end
-
 def oxford_comma(array)
-  ["kiwi", "dorian"].join(" and ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
-
-def oxford_comma(array)
- ["kiwi", "dorian", "starfruit"].join(" , ")
