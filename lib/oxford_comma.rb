07617f7f64c346906b2fc8fc9e89f782706eb706
Rachel Wyatt <rw126@evansville.edu>
2018-09-01 20:12:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+if array.size <=2
+  return array.join(" and ")
+elsif array.size >=3
+  array[-1].insert(0, 'and ')
+  #array.insert(-2, 'and')
+  end
+ array.join (", ")
 end
