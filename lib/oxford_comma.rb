a95cb78c6f19fd65e5d3cb5ecd0fc0dfe3e6c2ef
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 13:02:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.count >= 3
+  last_word = array.pop
+  array.join(", ") << ", and #{last_word}"
+else
+  array.join(" and ")
+end
 end
