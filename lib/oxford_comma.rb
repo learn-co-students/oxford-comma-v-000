87a0659e0fd2cb325878dba2d68b91f6c75e7cbb
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-02 03:58:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length == 1
+      return array[0]
+    elsif array.length == 2
+      return array.join(" " + "and" + " ")
+    elsif array.length >= 3
+      array[-1].insert(0, "and ")
+      array.join(", ")
+    end
 end
