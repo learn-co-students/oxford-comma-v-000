4ec1523cc012e20a49ab44494051da34da6fce7b
Amil Masic <amil.masic@gmail.com>
2018-09-11 15:23:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  # array = ["fiddleheads", "okra", "kohlrabi"]
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array[0..-2].join(", ")+", and " + array[-1]
 
+
+  end
 end
