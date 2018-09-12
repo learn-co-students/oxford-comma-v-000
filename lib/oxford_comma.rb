8ab49443178fc43083401f678de23280f9539dde
veloblank <veloblank@gmail.com>
2018-09-12 07:06:11 -0400

    Further refactor. Reduce to 8 LOC.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,7 @@ def oxford_comma(array)
   if array.size > 2
     oxfordized = array.collect {|ele| "#{ele},"}
     oxfordized[-1] = "and #{array.last}"
-    oxfordized.join(" ")
-  else
-      array.join(" and ")
+    oxfordized = oxfordized.join(" ")
   end
+  oxfordized ||= array.join(" and ")
 end
