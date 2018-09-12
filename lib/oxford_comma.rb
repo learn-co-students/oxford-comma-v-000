9e63f62f39b33ed78fb2b20400fdbfab35e6b528
veloblank <veloblank@gmail.com>
2018-09-12 06:52:08 -0400

    Further refactor. Reduce LOC.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,10 @@
 def oxford_comma(array)
-  case array.size
-    when 1
-      array.first
-    when 2
-      "#{array[0]} and #{array[1]}"
-    else
-      oxfordized = []
-      array.each {|ele| oxfordized << "#{ele},"}
-      oxfordized[-1] = "and #{array.last}"
-      oxfordized.join(" ")
+  if array.size > 2
+    oxfordized = []
+    array.each {|ele| oxfordized << "#{ele},"}
+    oxfordized[-1] = "and #{array.last}"
+    oxfordized.join(" ")
+  else
+      array.join(" and ")
   end
 end
