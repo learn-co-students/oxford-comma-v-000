09f4b7b3bdebee07d18f1cc47fde404b44419b6f
Adam Taitano <adam.taitano@gmail.com>
2018-09-13 04:02:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   if array.length == 1
-    return array.join
+    array.join
   elsif array.length == 2
-    return array.join(" and ")
+    array.join(" and ")
   elsif array.length > 2
     last_element = array.pop
     sentence = array.join(", ")
     full_sentence = sentence + ", and " + last_element
-    return full_sentence
+    full_sentence
   end
 end
