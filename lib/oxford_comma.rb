237fabd68df8a8fe801225c2cad17cf3821d526e
Stef Claus <stef.claus@gmail.com>
2018-09-14 00:19:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1
+  array.join
+  elsif array.count == 2 
+  array.join(" and ")
+  else array.count >= 3
+    if index != [-2]
+      array.join(",")
+    else array.join(" and ")
+    end
+  end 
 end
