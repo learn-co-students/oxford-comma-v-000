779340e4ad77d8e3d9da08a5312597557a312ac2
Christina Delavar <c.delavar@gmail.com>
2018-09-14 18:32:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last_element = Array.new
+    last_element << array[-1]
+    array.delete_at(-1)
+    string = array.join(", ") << ", and "
+    string << last_element.join(" ")
+    string
+  end
 
 end
