29f324f39e2937b992617384b6b32c71d2d18dc0
Rachel Feldman <rachel.a.f.@Rachels-MacBook-Pro.local>
2018-09-16 18:55:40 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+    if array.length === 1
+        array.join
+    
+    elsif array.length === 2
+        array.join(" and ")
 
+    else new_array = []
+        array.each do |element|
+            new_array << element
+        end
+
+        new_array.pop
+
+        new_array.join(", ") << ", and #{array.last}"
+
+    end
 end
