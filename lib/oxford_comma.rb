9875dc7d12e44c4b1521ca35fd72bc6efa3741e2
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-20 01:29:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,3 +7,14 @@ def oxford_comma(array)
     array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
+
+
+
+# def oxford_comma(array)
+#   if array.length == 2
+#     return "#{array[0]} and #{array[1]}"
+#   elsif 2 < array.length
+#     array[-1].insert(0, "and ")
+#   end
+#   array.join(", ")    
+# end
