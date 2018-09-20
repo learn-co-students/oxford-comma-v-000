ea11d65a743142f3cbef605e0471437e4b93e065
Erin Green <erin.y.green@gmail.com>
2018-09-20 00:40:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ if array.length == 2 
+   array.join(" and ")
+   elsif array.length >= 3
+   array.insert(-2, "and")
+ return "#{array[0...-1].join(", ")} #{array.last}"
+else 
+  return array.join
+end 
+  
 end
