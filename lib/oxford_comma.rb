6c7237a2830eff15cb9395dd2e5d7008a7dcd126
Yolanda Fay <yolandafay@gmail.com>
2018-09-22 18:32:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  array_length = array.length  
+  last_entry = array[array_length - 1]
+  
+  if array_length == 1 
+    return array[0]
+  elsif
+    array_length == 2 
+    return "#{array[0]} and #{array[1]}"
+  else
+      array.pop
+      array << "and "
+      array_string = array.join(", ")
+      array_string << last_entry
+    end
 end
