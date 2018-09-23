37ce88dad6d0af831a2e88d8da9102d74dcd2ed8
Grace Choi <oiograceoio@gmail.com>
2018-09-23 04:20:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    return array.join
+  elsif array.size == 2
+    array.insert(-2, ' and ')
+    return array.join
+  elsif array.size > 2
+    array.insert(-2, 'and ')
+    last = array.pop
+    new_array = array.join(", ")
+    new_array << last
+    return new_array
+  end
 end
