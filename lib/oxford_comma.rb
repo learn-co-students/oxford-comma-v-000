c76ef39302906a8fb03276fff48e0c0130ed7436
Brian Birdwell <bbirdwell@gmail.com>
2018-09-30 16:40:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.each do |n|
+      return n
+    end
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    array.insert(2, "and")
+    array[0-2].join(", ")
+  end
 end
