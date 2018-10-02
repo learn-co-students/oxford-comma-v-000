eeb47453f947d2e014b5cd70c1a881bf0e0be70d
Christian V <viray.christianj@gmail.com>
2018-10-02 00:30:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
-end
+  if array.length == 1
+    return array[0]
+ 
+  else if array.length == 2
+    return array.join(" and ")
+  
+  else if array.length >= 3 
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end 
+end 
