ce3cd29a8e49eaf43da078c879adbcc07c1db74d
Tania Whiting <tanwhiting@aol.com>
2018-10-03 04:28:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1 
+      return "#{array[0]}"
+    when 2
+      return "#{array[0]} and #{array[1]}"
+     else
+         last_array_element = array.pop
+         oxford_string = array.join(", ")
+         return "#{oxford_string}, and #{last_array_element}"
+    end
 end
