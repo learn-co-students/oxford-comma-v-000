95cf9d44d8626f61cb6c225a19ecba9121798fa9
Lois Lee <lois.lee@me.com>
2018-10-19 23:56:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  elsif array.length ==  2
+  if array.length == 2
     array.join(" and ")
-  elsif array.length >=  3
-    fruit = array.pop
-    array.join(", ")  + ", and " + fruit
+  elsif array.length >= 3
+    last = array.pop
+    array.join(", ") + ", and "+ last
+  else
+    array.join
   end
 end
