3e5de20c6534493335e912e77b0d32e57808792c
matthew.serre <matthew.serre@gmail.com>
2018-10-27 01:16:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string = array.join
+    string
+  elsif array.length == 2
+    string = array.join(" and ")
+    string
+  else
+    pop = array.pop
+    string = array.join(", ")
+    string + ", and " + pop
+  end
 end
