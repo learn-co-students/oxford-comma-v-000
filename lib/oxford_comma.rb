be3e564ca1afbe64f5dfb6e00aa6652af19e9375
Brad Schrag <brad.schrag@gmail.com>
2018-11-04 17:29:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  final_value = array.pop
+  statement = array.join(', ')
+  if array.count > 1
+    statement = "#{statement}, and #{final_value}"
+  elsif array.count == 1
+    statement = "#{array[0]} and #{final_value}"
+  else
+    statement = final_value
+  end
 end
