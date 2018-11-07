ef2c154052529784ea376b6ec0bba266907a54a3
Angie Goza <angielg9888@gmail.com>
2018-11-07 20:25:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  counter = 0
+  array.each do |element|
+    counter += 1
+  end
+  if counter == 1
+    return array.join
+  elsif counter == 2
+    return array.join(" and ")
+  elsif counter >= 3
+    last_element = [ ]
+    element = array.pop
+    last_element << "and #{element}"
+    array << last_element
+    array.join(", ")
+  end
 end
