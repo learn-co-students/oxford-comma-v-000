8d060079792fc4354b775c5523d13b65bd96bf12
Kimberly Aviles <01010110.v@gmail.com>
2018-11-07 08:22:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,12 +7,8 @@ def oxford_comma(array)
   elsif array.count == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   else array.count > 3
-    array_new = []
-    array_pop = array[-1]
-    array.each do |element|
-      array_new << "#{element}, "
-    end
-    array_new.pop
-    return "#{array_new.to_s} and #{array_pop.to_s}"
+    array_new = array.pop
+    array_orig = array.join(", ").to_s
+    return "#{array_orig}, and #{array_new}"
   end
 end
