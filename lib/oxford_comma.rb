fdcb02d3622cbd557bdbe63599c0811ed5e6c37c
MargaritaKhodzka <mkhodzka@gmail.com>
2018-11-28 21:43:41 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    "#{array[0..-2].join(', ')}, and #{array.last}"
+  end
 end
