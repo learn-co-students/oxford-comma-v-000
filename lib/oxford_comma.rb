ad0d8e826c1c089637c319a01a3dcb23455df3b8
kitte anderson <kitteanderson@gmail.com>
2018-12-12 03:11:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+    #array.insert(-2, "and").join (", ")??? how to make this work also
+  case array.length
+  when 1
+    "#{array[0]}"
+  when 2
+    array [0..1].join(" and ")
+  else
+    array [0...-1].join(", ") << ", and #{array[-1]}"
 end
+end
+
+=begin
+  def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
+end
+=end
