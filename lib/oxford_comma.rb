39d522e89e144be335df6a902dd436c59386a4da
Toni-Ty <antonique2479@yahoo.com>
2019-01-04 07:13:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,12 +8,10 @@
 #     end
 #   end
 def oxford_comma(array)
-  case array.length
-when 1
-    "#{array[0]}"
-  when 2
-    array[0..1].join(" and ")
-  else
-    array[0...-1].join(", ") << ", and #{array[-1]}"
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
-  end
+  array.join(", ")
+end
