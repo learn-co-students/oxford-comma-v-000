a2db8f824cda8661e413b1c9be21c97dc5fb6f2d
Toni-Ty <antonique2479@yahoo.com>
2019-01-04 07:13:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+# def oxford_comma(array)
+#   array.join do |string|
+#     puts "#{string}"
+#   if array.length == 2
+#     array.join("and")
+#       puts "#{string}"
+#       end
+#     end
+#   end
 def oxford_comma(array)
-
-end
+  case array.length
+when 1
+    "#{array[0]}"
+  when 2
+    array[0..1].join(" and ")
+  else
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+  end
+  end
