4bd9f0e34e80d3123adca8ca83b7f92fd15877b3
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 21:00:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array[0]
+  when 2
+    return array[0]+" and "+array[1] if array.size==2
+  else
+    array.join(", ").tap{|s| s[s.rindex(', '), 2]=', and '} if array.size>2
+  end
 end
