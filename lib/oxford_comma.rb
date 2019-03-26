5e8820538ea1a5c20c33aae386b8b23d7df45352
Brian Arango <brianvictus@gmail.com>
2019-03-26 16:30:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
-  if array.size == 1
-    return array[0]
-  elsif array.size == 2
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
     return array.join(" and ")
-  else
-    return array[0..-2].join(', ') + ", and " + array[-1]
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
   end
-  end
+end
