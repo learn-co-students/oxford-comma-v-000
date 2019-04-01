2033dcb9f079e9a75d65d2858acf488ee216613e
HANA LEANDER <hana0070@gmail.com>
2019-04-01 16:48:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+    array.join(", ")
+    #so insert(index_to_add_to, thing_youre_adding)
+  elsif array.length == 1
+    array.join
+  end
 end
