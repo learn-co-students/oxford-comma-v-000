8b7c82a2ee8971249d1f54f24255eb643353154b
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 19:33:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+#string to an array, an array into a string, or a range into an array
+#that takes an argument array of string elements and converts it into a string using the Oxford comma.
 
+
+def oxford_comma(array)
+if array.count == 1 then array.join
+elsif array.count == 2 then array.join(" and ")
+elsif array.count >= 3
+   array[-1].insert(0, "and ")
+   array.join(', ')
+end
 end
