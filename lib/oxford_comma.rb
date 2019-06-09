d5ff3e2355978d990fafa8ab9e746fc2542d1792
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 20:29:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-  last_item = array.pop
-  if array.count == 0
-    return last_item
-  elsif array.count == 1
-    return "#{array[0]} and #{last_item}"
+  size = array.size
+  if size==1
+    array[0]
+  elsif size == 2
+    "#{array[0]} and #{array[1]}"
   else
-    return "#{array.join(", ")}, and #{last_item}"
+    array.join(", ")
   end
 end
