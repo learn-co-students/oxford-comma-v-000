3fc21b2493efd591a547e3ed74e3135254086fd3
Julie  McManus <julieannesays@gmail.com>
2020-01-23 18:45:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
- if array.count == 1
-   array.join(",")
-elsif array.count == 2
-   array.join(" " + "and" + " ")
-elsif array.count == 3
-  return "#{array.first}, #{array[1]}, and #{array.last}"
-else
-  return "#{array[0, array.length-1].join("," + " ")}, and #{array[-1]}"
-end
+  if array.length == 1
+    array.join(", ")
+  elsif array.length == 2
+    array.join(" and ")
+elsif 3 <= array.length
+  array[-1].insert(0, "and ")
+  array.join(", ")
+  end
 end
