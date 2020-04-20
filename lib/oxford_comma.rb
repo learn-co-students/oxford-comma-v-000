b187814cb0649f7221aa6802f787a08b42d82532
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-20 15:37:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size > 2
     last = array.pop
     string = array.join(", ")
-    string << (", and ")
+    string << ", and "
     string << last
   elsif array.size == 2
     array.join(" and ")
