87102e75936cfbb4d4a52a3fdf061fa7962f7470
idith almog <almog.idith@gmail.com>
2020-04-27 17:01:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last=array[-1]
+    array1 = array.pop
+    string1= array.join(", ")
+    string=string1 +", and "+ last
+  end
 
 end
