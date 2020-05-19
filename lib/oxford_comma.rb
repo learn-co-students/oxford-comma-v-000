66efc765efafb794375efdd93d0ba77303d265d1
Angela Bardysh <angelabardysh@gmail.com>
2020-05-19 01:56:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-    if array.length <2
+  if array.length <2
     array.join
   elsif array.length == 2
-    return array.join (' and ')
-  else
-    array[-1].insert(0,"and ")
+    return array.join( " and " )
+  else array[-1].insert(0,"and ")
     array.join(", ")
   end
 end
