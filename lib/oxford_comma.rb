0785a58d5322f13682cdc427ac922a5389625efe
Thomas <Thomas.c.viles.jr@gmail.com>
2021-05-03 22:08:22 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
-  if array.size==1
-    array[0]
-  elsif array.size == 2
-    "#{array[0]} and #{array[1]}"
-  else
-    last_item = array.pop
-    "#{array.join(", ")}, and #{last_item}"
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1] = "and #{array[-1]}"
   end
+  array.join(", ")
 end
